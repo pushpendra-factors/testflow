14a47f20baaae1d44df4d7b385daa011f43b8833
Aravind Murthy <aravind@autometa.ai>
2018-07-04 11:42:49 +0530

    Moved ranking of patterns to pattern service. Ability to add timing constraints

--- app_backend/src/pattern/pattern_service.go
+++ app_backend/src/pattern/pattern_service.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type PatternWrapper struct {
@@ -16,6 +18,19 @@ type PatternWrapper struct {
 type PatternService struct {
 	patternsMap map[uint64]*PatternWrapper
 }
+
+type result struct {
+	EventNames      []string  `json:"event_names"`
+	Timings         []float64 `json:"timings"`
+	Cardinalities   []float64 `json:"cardinalities"`
+	Repeats         []float64 `json:"repeats"`
+	Counts          []uint    `json:"counts"`
+	PerUserCounts   []uint    `json:"per_user_counts"`
+	TotalUserCount  uint      `json:"total_user_count"`
+	NormalizedCount uint      `json:"normalized_count"`
+}
+
+type PatternServiceResults []*result
 
 func NewPatternService(patternsMap map[uint64][]*Pattern) (*PatternService, error) {
 	patternService := PatternService{patternsMap: map[uint64]*PatternWrapper{}}
@@ -55,8 +70,69 @@ func (ps *PatternService) GetCount(projectId uint64, eventNames []string) (uint,
 	return c, ok
 }
 
-func (ps *PatternService) Query(projectId uint64, startEvent string, endEvent string) ([]*Pattern, error) {
+func (ps *PatternService) rankPatterns(projectId uint64, patterns []*Pattern, minStartInterval uint,
+	maxStartInterval uint, minInterEventInterval uint, maxInterEventInterval uint) PatternServiceResults {
+	results := PatternServiceResults{}
+	for _, p := range patterns {
+		r := result{
+			EventNames:     p.EventNames,
+			Timings:        []float64{},
+			Cardinalities:  []float64{},
+			Repeats:        []float64{},
+			Counts:         []uint{},
+			PerUserCounts:  []uint{},
+			TotalUserCount: p.UserCount,
+		}
+		var countMultiplier float64 = 1.0
+		for i := 0; i < len(p.EventNames); i++ {
+			r.Timings = append(r.Timings, p.Timings[i].Quantile(0.5))
+			r.Repeats = append(r.Repeats, p.Repeats[i].Quantile(0.5))
+			r.Cardinalities = append(r.Cardinalities, p.EventCardinalities[i].Quantile(0.5))
+			subsequenceCount, ok := ps.GetCount(projectId, p.EventNames[:i+1])
+			if !ok {
+				log.Errorf(fmt.Sprintf(
+					"Subsequence %s not as frequent as sequence %s",
+					strings.Join(p.EventNames[:i+1], ","), p.String()))
+				r.Counts = append(r.Counts, p.Count)
+			} else {
+				r.Counts = append(r.Counts, subsequenceCount)
+			}
+
+			subsequencePerUserCount, ok := ps.GetPerUserCount(projectId, p.EventNames[:i+1])
+			if !ok {
+				log.Errorf(fmt.Sprintf(
+					"Subsequence %s not as frequent as sequence %s",
+					strings.Join(p.EventNames[:i+1], ","), p.String()))
+				r.PerUserCounts = append(r.Counts, p.OncePerUserCount)
+			} else {
+				r.PerUserCounts = append(r.Counts, subsequencePerUserCount)
+			}
+			if i == 0 {
+				countMultiplier *= (p.Timings[i].CDF(float64(maxStartInterval)) - p.Timings[i].CDF(float64(minStartInterval)))
+			} else {
+				countMultiplier *= (p.Timings[i].CDF(float64(maxInterEventInterval)) - p.Timings[i].CDF(float64(minInterEventInterval)))
+			}
+		}
+		r.NormalizedCount = uint(float64(p.Count) * countMultiplier)
+		results = append(results, &r)
+	}
+
+	// Sort in decreasing order of counts.
+	sort.SliceStable(results,
+		func(i, j int) bool {
+			return (results[i].NormalizedCount > results[j].NormalizedCount)
+		})
 	maxPatterns := 50
+	if len(results) > maxPatterns {
+		results = results[:maxPatterns]
+	}
+	return results
+}
+
+func (ps *PatternService) Query(projectId uint64, startEvent string,
+	endEvent string, minStartInterval uint, maxStartInterval uint,
+	minInterEventInterval uint, maxInterEventInterval uint) (PatternServiceResults, error) {
+
 	pw, ok := ps.patternsMap[projectId]
 	if !ok {
 		return nil, fmt.Errorf(fmt.Sprintf("No patterns for projectId:%d", projectId))
@@ -64,20 +140,15 @@ func (ps *PatternService) Query(projectId uint64, startEvent string, endEvent st
 	if startEvent == "" && endEvent == "" {
 		return nil, fmt.Errorf("Invalid Query")
 	}
-	resPatterns := []*Pattern{}
+	matchPatterns := []*Pattern{}
 	for _, p := range pw.patterns {
 		if (startEvent == "" || strings.Compare(startEvent, p.EventNames[0]) == 0) &&
 			(endEvent == "" || strings.Compare(endEvent, p.EventNames[len(p.EventNames)-1]) == 0) {
-			resPatterns = append(resPatterns, p)
+			matchPatterns = append(matchPatterns, p)
 		}
 	}
-	// Sort in decreasing order of counts.
-	sort.SliceStable(resPatterns,
-		func(i, j int) bool {
-			return resPatterns[i].Count > resPatterns[j].Count
-		})
-	if len(resPatterns) > maxPatterns {
-		resPatterns = resPatterns[:maxPatterns]
-	}
-	return resPatterns, nil
+
+	results := ps.rankPatterns(projectId, matchPatterns, minStartInterval, maxStartInterval,
+		minInterEventInterval, maxInterEventInterval)
+	return results, nil
 }
