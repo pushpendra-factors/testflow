fef1e3885d30854e45c58ccdbc31bf9f7517f9b2
Dineshprabu S <dinesh@factors.ai>
2019-01-20 18:40:04 +0530

    Changed frontend to use origin for API.

--- frontend/src/actions/factorsActions.js
+++ frontend/src/actions/factorsActions.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import appConfig from "../config/appConfig"
 
+const ORIGIN = window.location.origin;
+
 export function fetchFactors(currentProjectId, query, queryParams) {
   return function(dispatch) {
     dispatch({type: "FETCH_FACTORS"});
 
-    axios.post(appConfig.API_PATH + "projects/" + currentProjectId + "/factor" + queryParams, query)
+    axios.post(ORIGIN + "projects/" + currentProjectId + "/factor" + queryParams, query)
       .then((response) => {
         dispatch({type: "FETCH_FACTORS_FULFILLED", payload: response.data})
       })
