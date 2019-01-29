e7011fc3980d4dbb19b9106b8dd441cf6be1003c
Ankit Deshpande <45165113+ankit-slashbit@users.noreply.github.com>
2019-01-29 17:13:52 +0530

    K8: Add postgres backup cron, update docker images (#68)
    * K8: Add postgres backup cron, update docker images
    
    * update pg_backup.yaml

---
+++ backend/src/pg_backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+touch /root/.pgpass
+# Creating .pgpass file
+# Postgres will pick password from this file
+# hostname:port:database:username:password
+echo $DB_HOST:$DB_PORT:$POSTGRES_DB:$POSTGRES_USER:$POSTGRES_PASSWORD > /root/.pgpass
+chmod 600 /root/.pgpass
+fname=$(date "+%Y-%b-%e-%H-%M-%S-backup.dump")
+# http://zevross.com/blog/2014/06/11/use-postgresqls-custom-format-to-efficiently-backup-and-restore-tables/
+# -Fc Indicates pg_dump to create backup in postgres custom format. It is compressed by defaut
+pg_dump -Fc -U $POSTGRES_USER -h $DB_HOST $POSTGRES_DB > /root/$fname
+gsutil cp /root/$fname gs://factors-staging/backups/postgres/$fname
