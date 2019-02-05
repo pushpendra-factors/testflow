dcd1c9dd2fa350804616212d026250c4fc7a8045
Dineshprabu S <dinesh@factors.ai>
2019-02-05 23:13:06 +0530

    Fixed sdk api host for staging

--- frontend/src/views/Settings/Settings.js
+++ frontend/src/views/Settings/Settings.js
@@ -16,7 +16,7 @@ import {
     InputGroupText,
 } from 'reactstrap';
 
-import { getSDKAssetURL, getHostURL, isStaging } from "../../util";
+import { getSDKAssetURL, isStaging } from "../../util";
 import { 
   fetchCurrentProjectSettings, 
   udpateCurrentProjectSettings,
@@ -151,7 +151,7 @@ class Settings extends Component {
   getSDKScript() {
     let token = this.getToken();
     let assetURL = getSDKAssetURL();
-    let init = isStaging() ? 'factors.init("'+token+'")' : 'factors.init("'+token+'", {host:"'+getHostURL()+'"})';
+    let init = isStaging() ? 'factors.init("'+token+'", {host:"'+window.origin+'"})' : 'factors.init("'+token+'")';
     return '(function(c){var s=document.createElement("script");s.type="text/javascript";if(s.readyState){s.onreadystatechange=function(){if(s.readyState=="loaded"||s.readyState=="complete"){s.onreadystatechange=null;c()}}}else{s.onload=function(){c()}}s.src="'+assetURL+'";d=!!document.body?document.body:document.head;d.appendChild(s)})(function(){'+init+'})';
   }
 
