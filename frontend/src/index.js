f6d8ca6c1ea830c862b3340bb0980ca823399c2b
Dineshprabu S <dinesh@factors.ai>
2019-02-13 21:41:26 +0530

    Removed service worker.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,9 @@ import './index.css';
 import App from './App';
 import store from "./store"
 
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Provider store={store}>
   <App />
 </Provider>, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
