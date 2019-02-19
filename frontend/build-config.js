5944af5152574177bf644905cf008a9ce127a4e0
Dineshprabu S <dinesh@factors.ai>
2019-02-19 14:03:43 +0530

    Fixes #80

--- frontend/build-config.js
+++ frontend/build-config.js
@@ -4,15 +4,16 @@ const CONFIG = {
     sdk_asset_url: 'http://localhost:8090/dist/factors.prod.js'
   },
   staging: {
-    backend_host: '', // Uses window.origin.
-    sdk_asset_url: '' // Use window.origin/assets/factors.js
+    backend_host: 'https://api.factors.ai',
+    // Uses window.origin/assets/factors.js. Todo: Replace with https://app.factors.ai/assets/factors.js
+    sdk_asset_url: ''
   },
   test: {
     backend_host: 'http://localhost:8080',
     sdk_asset_url: 'http://localhost:8090/dist/factors.prod.js'
   },
   production: {
-    backend_host: 'https://factors.ai',
+    backend_host: 'https://api.factors.ai',
     // Use factors.ai for production for rotating asset whenever required.
     sdk_asset_url: 'https://factors.ai/assets/factors.js'
   }
