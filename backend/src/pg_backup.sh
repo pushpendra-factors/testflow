57af7175dc0a67fdc7857a26a833122779a109f7
Ankit Deshpande <ankit@factors.ai>
2019-02-22 17:59:10 +0530

    pg backup fix env var name

--- backend/src/pg_backup.sh
+++ backend/src/pg_backup.sh
@@ -9,4 +9,4 @@ fname=$(date "+%Y-%b-%e-%H-%M-%S-backup.dump")
 # http://zevross.com/blog/2014/06/11/use-postgresqls-custom-format-to-efficiently-backup-and-restore-tables/
 # -Fc Indicates pg_dump to create backup in postgres custom format. It is compressed by defaut
 pg_dump -Fc -U $POSTGRES_USER -h $DB_HOST $POSTGRES_DB > /root/$fname
-gsutil cp /root/$fname gs://$(POSTGRES_BACKUP_BUCKET)/$fname
+gsutil cp /root/$fname gs://$POSTGRES_BACKUP_BUCKET/$fname
