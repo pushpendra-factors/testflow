f77d06e4436a5798e9804cdd17338d2c5b4f4ad7
Dineshprabu S <dinesh@factors.ai>
2019-04-04 09:51:12 +0530

    Added script tag to SDK.

--- frontend/src/views/Settings/JsSdk.js
+++ frontend/src/views/Settings/JsSdk.js
@@ -27,22 +27,33 @@ class JsSdk extends Component {
 
 	getSDKScript() {
     let token = this.getToken();
-    let assetURL = BUILD_CONFIG.sdk_asset_url;
+		let assetURL = BUILD_CONFIG.sdk_asset_url;
     return '(function(c){var s=document.createElement("script");s.type="text/javascript";if(s.readyState){s.onreadystatechange=function(){if(s.readyState=="loaded"||s.readyState=="complete"){s.onreadystatechange=null;c()}}}else{s.onload=function(){c()}}s.src="'+assetURL+'";d=!!document.body?document.body:document.head;d.appendChild(s)})(function(){factors.init("'+token+'")})';
-  }
+	}
+	
+	renderScriptCode() {
+		return (
+			<div className='fapp-code'>
+				<span style={{display: 'block'}}>{'<script>'}</span>
+				<div style={{marginLeft: '15px'}}><span>{this.getSDKScript()}</span></div>
+				<span style={{display: 'block'}}>{'</script>'}</span>
+			</div>
+		);
+	}
 
 	render() {
 		return (
 			<div class='fapp-content fapp-content-margin'>
 				<Card className='fapp-bordered-card'>
           <CardHeader>
-						<button className='btn btn-success' style={{float: 'right', padding: '2px 8px'}}> Copy  <i class='fa fa-copy' style={{marginLeft: '4px', fontWeight: 'inherit'}}></i> </button>
+						{
+							/* Todo(Dinesh): Add copy to clipboard, Use the button below. */
+							/* <button className='btn btn-success' style={{float: 'right', padding: '2px 8px'}}> Copy  <i class='fa fa-copy' style={{marginLeft: '4px', fontWeight: 'inherit'}}></i> </button> */
+						}
             <strong>Code Snippet</strong>
           </CardHeader>
-					<CardBody>
-						<div className='fapp-code'>
-							<span>{this.getSDKScript()}</span>
-						</div>
+					<CardBody style={{padding: '1.5rem 1.5rem'}}>
+						{ this.renderScriptCode() }
 					</CardBody>
         </Card>
 			</div>
