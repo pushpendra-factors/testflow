d30fab6fd3afecc9435bc6687ddc6f4bc70b71e3
Dineshprabu S <dinesh@factors.ai>
2019-04-23 13:53:39 +0530

    Reverted barchart reuse on factor.

--- frontend/src/views/Factor/BarChartCard.js
+++ frontend/src/views/Factor/BarChartCard.js
@@ -5,13 +5,59 @@ import {
   CardHeader,
   CardTitle,
 } from 'reactstrap';
+import { Bar } from 'react-chartjs-2';
+import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';	
 
-import BarChart from '../Query/BarChart';
+const barBackgroundColors = ['rgba(75,192,192,0.4)', 'rgba(255,99,132,0.2)'];	import BarChart from '../Query/BarChart';
+const barBorderColors = ['rgba(75,192,192,1)', 'rgba(255,99,132,1)'];	
+const barHoverBackgroundColors = ['rgba(75,192,192,1)', 'rgba(255,99,132,0.4)'];	
+const barHoverBorderColors = ['rgba(220,220,220,1)', 'rgba(255,99,132,0.4)'];
+
 
 class BarChartCard extends Component {
   render() {
     var chartData = this.props.chartData;
-    var chart = <BarChart bars={chartData} />;
+    var chartOptions = {
+      tooltips: {
+        enabled: false,	
+        custom: CustomTooltips	
+      },	
+      maintainAspectRatio: false,	
+      scales: {	
+        xAxes: [{	
+          scaleLabel: {	
+            display: false,	
+          }	
+        }],	
+        yAxes: [{	
+          scaleLabel: {	
+            display: false,	
+          }	
+        }],	
+      },	
+    };	
+    var bar = {	
+      labels: chartData.labels,	
+      datasets: chartData.datasets,	
+    };	
+    // Styling.	
+    for (var i = 0; i < bar.datasets.length; i++) {	
+      bar.datasets[i].backgroundColor = barBackgroundColors[i % barBackgroundColors.length];	
+      bar.datasets[i].borderColor = barBorderColors[i % barBorderColors.length];	
+      bar.datasets[i].borderWidth = 1;	
+      bar.datasets[i].hoverBackgroundColor = barHoverBackgroundColors[i % barHoverBackgroundColors.length];	
+      bar.datasets[i].hoverBorderColor = barHoverBorderColors[i % barHoverBorderColors.length];	
+    }	
+
+    if (chartData.x_label != "") {	
+      chartOptions.scales.xAxes[0].scaleLabel.display = true;	
+      chartOptions.scales.xAxes[0].scaleLabel.labelString = chartData.x_label;	
+    }	
+    if (chartData.y_label != "") {	
+      chartOptions.scales.yAxes[0].scaleLabel.display = true	
+      chartOptions.scales.yAxes[0].scaleLabel.labelString = chartData.y_label	
+    }	
+    var chart = <Bar data={bar} options={chartOptions} />
 
     const explanations = chartData.explanations.map((explainText) =>
       <CardTitle className="fapp-chart-card-title">{explainText}</CardTitle>
