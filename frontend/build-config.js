a5d76b705fec02598751ec3ef36fd3d791e42c85
Ankit Deshpande <45165113+ankit-slashbit@users.noreply.github.com>
2019-05-03 15:19:36 +0530

    Add Android, IOS SDK in settings Tab (#131)
    * Add download button for android sdk
    
    * Update instructions
    
    * Update logo for android sdk in settings tab
    
    * Add Ios Sdk in settings tab
    
    * Update ios Svg
    
    * Center request button, remove card

--- frontend/build-config.js
+++ frontend/build-config.js
@@ -2,21 +2,25 @@ const CONFIG = {
   development: {
     backend_host: 'http://factors-dev.com:8080',
     sdk_asset_url: 'http://localhost:8090/dist/factors.prod.js',
+    android_sdk_asset_url: 'https://storage.googleapis.com/factors-staging-v2/sdk/android/sdk-staging-v0.1.aar',
     factors_sdk_token: 'dummy'
   },
   staging: {
     backend_host: 'https://staging-api.factors.ai',
     sdk_asset_url: 'https://staging-app.factors.ai/assets/factors.js',
+    android_sdk_asset_url: 'https://storage.googleapis.com/factors-staging-v2/sdk/android/sdk-staging-v0.1.aar',
     factors_sdk_token: 'we0jyjxcs0ix4ggnkptymjh48ur8y7q7'
   },
   test: {
     backend_host: 'http://localhost:8080',
     sdk_asset_url: 'http://localhost:8090/dist/factors.prod.js',
+    android_sdk_asset_url: 'https://storage.googleapis.com/factors-staging-v2/sdk/android/sdk-staging-v0.1.aar',
     factors_sdk_token: 'dummy'
   },
   production: {
     backend_host: 'https://api.factors.ai',
     sdk_asset_url: 'https://app.factors.ai/assets/factors.js',
+    android_sdk_asset_url: 'https://storage.googleapis.com/factors-production-v2/sdk/android/sdk-production-v0.1.aar',
     factors_sdk_token: 'we0jyjxcs0ix4ggnkptymjh48ur8y7q7'
   }
 }
---
+++ frontend/src/views/Settings/IosSdk.js
@@ -0,0 +1,46 @@
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+		Col,
+		Row,		
+		Button,
+} from 'reactstrap';
+import Loading from '../../loading';
+import factorsai from '../../factorsaiObj';
+
+const mapStateToProps = store => {
+  return {
+		projects: store.projects.projects,
+		currentProjectId: store.projects.currentProjectId,
+  }
+}
+
+class IosSdk extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	getToken() {
+    return this.props.projects[this.props.currentProjectId].token;
+  }
+
+	trackIosSDKRequest = () => {
+		factorsai.track('ios_sdk_request', { project_id: this.props.currentProjectId });
+	}
+
+	render() {
+		return (
+			<div class='fapp-content fapp-content-margin'>
+				<Row style={{paddingTop: '12%'}}>
+						<div style={{width: '100%', textAlign: 'center', fontSize: '40px', fontWeight: '500', color: '#484848', marginBottom: '15px', letterSpacing: '0.1rem'}}>Coming Soon<span style={{marginLeft: '3px'}}>!</span></div>
+						<div style={{width: '100%', textAlign: 'center'}}>
+							<Button style={{ fontSize: '16px', padding: '8px 20px', fontWeight: '700 !important', letterSpacing: '0.05rem' }} color='success' onClick = {this.trackIosSDKRequest} >Request</Button>
+						</div>
+				</Row>
+			</div>
+		);
+	}
+}
+
+export default connect(mapStateToProps, null)(IosSdk);
