a9d74fc89fab67c24ee596d71189ca61e164cf48
Dineshprabu S <dinesh@factors.ai>
2019-05-14 12:35:17 +0530

    Corrected group by label.

--- frontend/src/views/Query/Query.js
+++ frontend/src/views/Query/Query.js
@@ -696,7 +696,7 @@ class Query extends Component {
           </Row>
           <Row style={{marginBottom: '15px'}}>
             <Col xs='12' md='12'>
-              <div style={{ marginBottom: '15px' }} hidden={this.state.groupBys.length == 0}><span style={LABEL_STYLE}> Group by </span></div>
+              <div style={{ marginBottom: '15px' }} hidden={this.state.groupBys.length == 0}><span style={LABEL_STYLE}> group by </span></div>
               {groupBys}
               <Button outline color='primary' onClick={this.addGroupBy} style={{ marginTop: '3px' }}>+ Group</Button>
             </Col>  
