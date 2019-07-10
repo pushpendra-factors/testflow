38b6675c22ced822fd5ef2782fbdbad322247d67
Dineshprabu S <dinesh@factors.ai>
2019-07-10 12:13:14 +0530

    Fixes #173 - Set cookie with org level domain.

--- sdk/javascript/src/utils/cookie.js
+++ sdk/javascript/src/utils/cookie.js
@@ -1,3 +1,26 @@
+// Gets org level domain.
+// e.g., www.example.co.uk -> example.co.uk 
+// This method tries to set cookies with hostname split by dot
+// from backwards to find the org level domain.
+function getOrgLevelDomain() {
+    var try_cookie='try_get_old_cookie=try_get_old_value';
+    var hostname = window.location.hostname.split('.');
+
+    var i, h;
+    for (i=hostname.length-1; i>=0; i--) {
+      h = hostname.slice(i).join('.');
+      document.cookie = try_cookie + ';domain=.' + h + ';';
+      if (document.cookie.indexOf(try_cookie) > -1) {
+        // we are able to store cookie.
+        document.cookie = try_cookie.split('=')[0] + '=;domain=.' + h + ';expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        return h;
+      }
+    }
+
+    // return complete domain name try fails.
+    return window.location.hostname;
+}
+
 // Sets the cookie. Replaces if exists already.
 function set(name, value, days) {
     let expires = "";
@@ -6,7 +29,9 @@ function set(name, value, days) {
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    
+    let orgDomain = getOrgLevelDomain();
+    document.cookie = name + "=" + (value || "")  + expires + "; domain=" + orgDomain + "; path=/";
 }
 
 // Gets cookie by its name.
