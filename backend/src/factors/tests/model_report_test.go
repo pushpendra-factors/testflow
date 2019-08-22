be9659afc471a5d358b5dbc7df18832aaa162b2e
Dineshprabu S <dinesh@factors.ai>
2019-08-22 09:25:25 +0530

    Reports test fixed.

--- backend/src/factors/tests/model_report_test.go
+++ backend/src/factors/tests/model_report_test.go
@@ -13,8 +13,8 @@ func TestTranslateReport(t *testing.T) {
 	r := make([][]interface{}, 0, 0)
 	r = append(r, []interface{}{float64(1)})
 
-	dashboardUnitIDToDashboardReports := make(map[uint64]M.DashboardUnitReport)
-	dashboardUnitIDToDashboardReports[1] = M.DashboardUnitReport{
+	units := make([]M.DashboardUnitReport, 0, 0)
+	unit := M.DashboardUnitReport{
 		Title:        "Test",
 		Presentation: M.PresentationCard,
 		Results: []M.ReportUnit{M.ReportUnit{
@@ -26,13 +26,12 @@ func TestTranslateReport(t *testing.T) {
 			},
 		}},
 	}
+	units = append(units, unit)
 
 	report := &M.Report{
-		ID:   uint64(1),
-		Type: M.ReportTypeWeekly,
-		Contents: M.ReportContent{
-			DashboardUnitIDToDashboardUnitReport: dashboardUnitIDToDashboardReports,
-		},
+		ID:    uint64(1),
+		Type:  M.ReportTypeWeekly,
+		Units: units,
 	}
 	dbReport, err := M.TranslateReportToDBReport(report)
 	assert.Nil(t, err)
