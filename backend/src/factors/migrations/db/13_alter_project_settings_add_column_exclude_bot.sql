ca3c7398d03fec6f75775e981f144c7cb96458eb
Dineshprabu S <dinesh@factors.ai>
2019-10-06 14:05:38 +0530

    Added not null and default values on migration for exclude_bot column.

--- backend/src/factors/migrations/db/13_alter_project_settings_add_column_exclude_bot.sql
+++ backend/src/factors/migrations/db/13_alter_project_settings_add_column_exclude_bot.sql
@@ -1,6 +1,10 @@
 -- UP
 ALTER TABLE project_settings ADD COLUMN exclude_bot boolean;
+
 UPDATE project_settings SET exclude_bot=true;
+-- ADDED NOT NULL AND DEFAULT
+ALTER TABLE project_settings ALTER COLUMN exclude_bot SET NOT NULL;
+ALTER TABLE project_settings ALTER COLUMN exclude_bot SET DEFAULT false;
 
 -- DOWN
 -- ALTER TABLE project_settings DROP COLUMN exclude_bot RESTRICT;
