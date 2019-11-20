db75feb1e605cec6a0d3f5b9f94824d3d79bbe20
Dineshprabu S <dinesh@factors.ai>
2019-11-20 12:02:35 +0530

    Fixed ad_performance_report on adwords sync.

--- integrations/adwords/scripts/sync.py
+++ integrations/adwords/scripts/sync.py
@@ -293,13 +293,13 @@ def get_ad_performance_report(adwords_client, timestamp):
     during = str_timestamp + "," + str_timestamp
     downloader = adwords_client.GetReportDownloader(version='v201809')
     
-    seg_fields = ['account_currency_code', 'account_descriptive_name', 'active_view_impressions', 
+    seg_fields = ['id', 'account_currency_code', 'account_descriptive_name', 'active_view_impressions', 
         'active_view_measurability', 'active_view_measurable_cost', 'active_view_measurable_impressions',
-        'active_view_viewability', 'ad_group_id', 'ad_id', 'adwords_customer_id', 'all_conversion_rate', 
+        'active_view_viewability', 'ad_group_id', 'all_conversion_rate', 
         'all_conversion_value', 'all_conversions', 'average_cost', 'average_position', 'average_time_on_site',
         'bounce_rate', 'click_assisted_conversion_value', 'click_assisted_conversions', 
         'click_assisted_conversions_over_last_click_conversions',
-        'clicks', 'conversion_value', 'conversions', 'cost', 'date_start', 'date_stop', 'engagements', 'gmail_forwards',
+        'clicks', 'conversion_value', 'conversions', 'cost', 'date', 'engagements', 'gmail_forwards',
         'gmail_saves', 'gmail_secondary_clicks', 'impression_assisted_conversions', 'impressions', 'interaction_types',
         'interactions', 'value_per_all_conversion', 'video_quartile_100_rate', 'video_quartile_25_rate', 
         'video_quartile_50_rate', 'video_quartile_75_rate', 'video_view_rate', 'video_views', 'view_through_conversions']
