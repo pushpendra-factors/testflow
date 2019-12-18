fa9f4b3675c4cbc1f7dc383f526fae7015ec8311
Dineshprabu S <dinesh@factors.ai>
2019-12-18 14:17:17 +0530

    Update auto_form_capture as false for existing records.

--- backend/src/factors/migrations/db/18_alter_project_settings_add_auto_form_capture.sql
+++ backend/src/factors/migrations/db/18_alter_project_settings_add_auto_form_capture.sql
@@ -1,6 +1,7 @@
 -- UP
 ALTER TABLE project_settings ADD COLUMN auto_form_capture boolean;
 ALTER TABLE project_settings ALTER COLUMN auto_form_capture SET DEFAULT false;
+UPDATE project_settings SET auto_form_capture='false';
 
 -- DOWN
 -- ALTER TABLE project_settings DROP COLUMN auto_form_capture RESTRICT;
