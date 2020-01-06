da8a6d5bbfb4693b452934f2a4d6bc3264e40780
Dineshprabu S <dinesh@factors.ai>
2020-01-06 13:41:30 +0530

    Fix for decimal points for zero metric values channel metrics.

--- frontend/src/views/ChannelQuery/common.js
+++ frontend/src/views/ChannelQuery/common.js
@@ -4,7 +4,8 @@ export function getReadableChannelMetricValue(key, value, meta) {
 
   let rValue = value;
   let isFloat = (value % 1) > 0
-  if (isFloat) rValue = value.toFixed(0);
+  // no decimal points for value >= 1 and 2 decimal points < 1.
+  if (isFloat) rValue = value >= 1 ? value.toFixed(0) : value.toFixed(2);
 
   if (meta && meta.currency && key.toLowerCase().indexOf('cost') > -1)
     rValue = rValue + ' ' + meta.currency;
