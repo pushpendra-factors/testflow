bfa0f514f81801f24e8ba8332749b41749a55987
Dineshprabu S <dinesh@factors.ai>
2020-01-06 13:30:20 +0530

    Show 2 decimal points for zero metric values.

--- frontend/src/views/ChannelQuery/common.js
+++ frontend/src/views/ChannelQuery/common.js
@@ -4,7 +4,9 @@ export function getReadableChannelMetricValue(key, value, meta) {
 
   let rValue = value;
   let isFloat = (value % 1) > 0
-  if (isFloat) rValue = value.toFixed(0);
+  if (isFloat) roundedValue = value.toFixed(0);
+  // no decimal points for value >= 1 and 2 decimal points < 1.
+  rValue = roundedValue == 0 ? value.toFixed(2) : roundedValue;
 
   if (meta && meta.currency && key.toLowerCase().indexOf('cost') > -1)
     rValue = rValue + ' ' + meta.currency;
