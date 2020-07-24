8ca09ef0f624d2d15235f2c8127e3ed2d1984870
Jitesh Kriplani <jiteshkriplani@Jiteshs-MacBook-Pro.local>
2020-07-24 19:00:44 +0530

    testing tailwind

--- frontend-new/src/components/Sidebar/index.js
+++ frontend-new/src/components/Sidebar/index.js
@@ -8,23 +8,23 @@ function Sidebar() {
       <div className={styles.logo}></div>
       <ul>
         <li className={styles.active}>
-          <i className="fa fa-share-alt"></i>
+          <i className="text-gray-600	fa fa-share-alt"></i>
           <span className={styles.tooltip}>Connections</span>
         </li>
         <li>
-          <i className="fa fa-hdd-o"></i>
+          <i className="text-gray-600	fa fa-hdd-o"></i>
           <span className={styles.tooltip}>Devices</span>
         </li>
         <li>
-          <i className="fa fa-newspaper-o"></i>
+          <i className="text-gray-600	fa fa-newspaper-o"></i>
           <span className={styles.tooltip}>Contacts</span>
         </li>
         <li>
-          <i className="fa fa-print"></i>
+          <i className="text-gray-600	fa fa-print"></i>
           <span className={styles.tooltip}>Fax</span>
         </li>
         <li>
-          <i className="fa fa-sliders"></i>
+          <i className="text-gray-600	fa fa-sliders"></i>
           <span className={styles.tooltip}>Settings</span>
         </li>
       </ul>
