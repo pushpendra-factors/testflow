87c4496c1259b24585d1c1e28aefc47995c97e5d
Akhil Nair <akhilnair@192.168.0.104>
2020-08-06 08:09:51 +0530

    search bar styles

--- frontend-new/src/components/SearchBar/index.js
+++ frontend-new/src/components/SearchBar/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Input } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
+import styles from './index.module.scss';
 
 function SearchBar() {
     return (
-        <Input size="large" placeholder="large size" prefix={<SearchOutlined />} />
+        <div>
+            <Input size="large" className={styles.app_input} placeholder="large size" prefix={<SearchOutlined />} />
+        </div>
     );
 }
 
