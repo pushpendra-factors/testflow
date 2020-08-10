48cc368d720da632639b36430bf2818f40357f82
Akhil Nair <akhilnair@192.168.0.108>
2020-08-10 11:10:29 +0530

    move to webpack build

--- frontend-new/src/Views/CoreQuery/index.js
+++ frontend-new/src/Views/CoreQuery/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './header';
+import QueryComposer from '../../components/QueryComposer';
 
 function CoreQuery() {
     return (
         <>
             <Header />
+            <div>
+                <QueryComposer visible={true}></QueryComposer>
+            </div>
         </>
     )
 }
--- frontend-new/src/index.js
+++ frontend-new/src/index.js
@@ -6,8 +6,6 @@ import './assets/index.scss';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
