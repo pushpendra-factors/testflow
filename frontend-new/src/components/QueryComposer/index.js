59d95442e52b74bc2dae9a1ce4ba858c80c14a7b
Akhil Nair <akhil@factors.ai>
2020-08-13 10:47:15 +0530

    move fetch projects to app.js

--- frontend-new/src/components/QueryComposer/index.js
+++ frontend-new/src/components/QueryComposer/index.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Drawer, Button } from 'antd';
 
-import {get} from '../../request';
-
 import styles from './index.module.scss';
 
 function QueryComposer() {
@@ -19,24 +17,6 @@ function QueryComposer() {
             <span>Event Analysis</span>
         </div>)
     }
-
-    function getHostURL() {
-        let host = BUILD_CONFIG.backend_host;
-        return (host[host.length-1] === "/") ? host : host+"/";
-    }
-
-    var host = getHostURL();
-
-    function fetchProjects() {
-            get(host + "projects")
-              .then((response)=>{      
-                console.log(response)
-              }).catch((err)=>{        
-                console.log(err);
-              });
-    }
-
-    fetchProjects();
 
     return(
         <Drawer
