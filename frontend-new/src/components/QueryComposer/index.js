c6a3a59026c456b1703e4d06526294359e95b734
Akhil Nair <akhil@factors.ai>
2020-08-13 10:44:45 +0530

    connect to backend, fetch projects call verification

--- frontend-new/src/components/QueryComposer/index.js
+++ frontend-new/src/components/QueryComposer/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Drawer, Button } from 'antd';
 
+import {get} from '../../request';
+
 import styles from './index.module.scss';
 
 function QueryComposer() {
@@ -17,6 +19,24 @@ function QueryComposer() {
             <span>Event Analysis</span>
         </div>)
     }
+
+    function getHostURL() {
+        let host = BUILD_CONFIG.backend_host;
+        return (host[host.length-1] === "/") ? host : host+"/";
+    }
+
+    var host = getHostURL();
+
+    function fetchProjects() {
+            get(host + "projects")
+              .then((response)=>{      
+                console.log(response)
+              }).catch((err)=>{        
+                console.log(err);
+              });
+    }
+
+    fetchProjects();
 
     return(
         <Drawer
