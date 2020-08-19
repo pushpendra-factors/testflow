159d7a60153f969945c17e5e30451548f9883fc6
Akhil Nair <akhil@factors.ai>
2020-08-19 08:21:25 +0530

    import c3 styles

--- frontend-new/src/index.js
+++ frontend-new/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './assets/tailwind.output.css';
 import './assets/index.scss';
+import 'c3/c3.css';
 import App from './App';
 
 ReactDOM.render(
