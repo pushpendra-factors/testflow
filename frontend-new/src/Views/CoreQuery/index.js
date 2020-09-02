6413cae1bd08458d1536d29ff5a7e23cc5f398eb
Vishnu Baliga <baliga-factors@users.noreply.github.com>
2020-09-02 12:24:50 +0530

    UI minor changes wrt drawer

--- frontend-new/src/Views/CoreQuery/index.js
+++ frontend-new/src/Views/CoreQuery/index.js
@@ -3,6 +3,9 @@ import Header from './header';
 import ResultsPage from './ResultsPage';
 import QueryComposer from '../../components/QueryComposer';
 import CoreQueryHome from '../CoreQueryHome';
+import { Drawer, Button, Collapse, Select, Popover } from 'antd';
+import {SVG, Text} from 'factorsComponents';
+import styles from './index.module.scss';
 
 function CoreQuery() {
     const [drawerVisible, setDrawerVisible] = useState(false);
@@ -33,18 +36,39 @@ function CoreQuery() {
         setDrawerVisible(false);
     }
 
+    const title = () => {
+        return (<div className={styles.composer_title}>
+            <div>
+                <SVG name="teamfeed"></SVG>
+                <Text type={'title'} level={4} weight={`bold`} extraClass={`ml-2`}>Find event funnel for</Text> 
+            </div>
+            <span className={styles.composer_title__help}>
+                <SVG name="play"></SVG>
+                    Help
+            </span>
+            
+        </div>)
+    }
+
     return (
         <>
+             <Drawer
+        title={title()}
+        placement="left"
+        closable={true}
+        visible={drawerVisible}
+        onClose={closeDrawer} 
+        getContainer={false}
+        width={"600px"}
+        className={`fa-drawer`}
+      >
 
-            <QueryComposer
-                drawerVisible={drawerVisible}
-                queries={queries}
-                onClose={closeDrawer}
+            <QueryComposer  
+                queries={queries} 
                 runQuery={runQuery}
                 eventChange={queryChange}
-            >
-
-            </QueryComposer>
+            /> 
+      </Drawer>
 
             {
                 showResult ? (<ResultsPage setDrawerVisible={setDrawerVisible} queries={queries.map(elem => elem.label)} />) : (<CoreQueryHome setDrawerVisible={setDrawerVisible} />)
