8f76c7a340cfb0d0caffd38e294e9eb485ae5fa3
Maisa <maisa@factors.ai>
2020-09-07 06:35:22 +0530

    changed comment

--- backend/src/factors/migrations/db/30_add_salesforce_setting_columns.sql
+++ backend/src/factors/migrations/db/30_add_salesforce_setting_columns.sql
@@ -5,6 +5,6 @@ ALTER TABLE agents ADD int_salesforce_instance_url text;
 
 -- DOWN
 -- ALTER TABLE project_settings DROP int_salesforce_enabled_agent_uuid;
--- ALTER TABLE agents ADD int_salesforce_refresh_token text;
--- ALTER TABLE agents ADD int_salesforce_instance_url text;
+-- ALTER TABLE agents DROP int_salesforce_refresh_token text;
+-- ALTER TABLE agents DROP int_salesforce_instance_url text;
 
