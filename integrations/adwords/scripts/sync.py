dec0130ff8c7399c6872d02983856cbd909edaab
ansari-ashhar <60336936+ansari-ashhar@users.noreply.github.com>
2020-09-17 19:25:34 +0530

    Fix field combinations for adgroup report

--- integrations/adwords/scripts/sync.py
+++ integrations/adwords/scripts/sync.py
@@ -351,14 +351,13 @@ def get_ad_group_performance_report(adwords_client, timestamp):
     query_fields = ['active_view_impressions', 'active_view_measurability', 'active_view_measurable_cost', 
     'active_view_measurable_impressions', 'active_view_viewability', 'all_conversion_rate', 'conversion_rate',
     'cost_per_conversion', 'all_conversion_value', 'all_conversions', 'average_cost', 'average_position',
-    'average_pageviews', 'average_time_on_site', 'ad_group_id', 'ad_group_name', 'ad_group_status',
-    'base_ad_group_id','base_campaign_id', 'bounce_rate', 'campaign_id', 'campaign_name', 'campaign_status',
+    'average_time_on_site', 'ad_group_id', 'ad_group_name', 'ad_group_status','base_ad_group_id','base_campaign_id', 
+    'bounce_rate', 'campaign_id', 'campaign_name', 'campaign_status','click_type','final_url_suffix', 'device',
     'click_assisted_conversion_value', 'click_assisted_conversions', 'click_assisted_conversions_over_last_click_conversions', 
     'clicks', 'conversion_value', 'conversions', 'cost', 'engagements', 'gmail_forwards', 'gmail_saves', 
     'gmail_secondary_clicks', 'impression_assisted_conversions', 'impression_assisted_conversion_value', 'impressions', 
     'interaction_types', 'interactions', 'value_per_all_conversion', 'video_quartile_100_rate', 'video_quartile_25_rate', 
-    'video_quartile_50_rate', 'video_quartile_75_rate', 'video_view_rate', 'video_views', 'view_through_conversions','click_type',
-    'final_url_suffix', 'device']
+    'video_quartile_50_rate', 'video_quartile_75_rate', 'video_view_rate', 'video_views', 'view_through_conversions']
     fields = snake_to_pascal_case(query_fields)
 
     # Create report query.
