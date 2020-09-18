d1e57bd2972013dc8eecde9eaad8eabab1dcf2dd
Janani Somaskandan <janani@factors.ai>
2020-09-18 19:37:14 +0530

    Fixing "/ issue"

--- backend/src/factors/handler/event_names.go
+++ backend/src/factors/handler/event_names.go
@@ -42,7 +42,7 @@ func GetEventNamesHandler(c *gin.Context) {
 	if helpers.IsProjectWhitelistedForEventUserCache(projectId) {
 		// RedisGet is the only call. In case of Cache crash, job will be manually triggered to repopulate cache
 		// No fallback for now.
-		eventNames, err := M.GetEventNamesOrderedByOccurenceAndRecency(projectId, 2500, 30)
+		eventNames, err := M.GetEventNamesOrderedByOccurenceAndRecency(projectId, 2500, 10)
 		if err != nil {
 			logCtx.WithError(err).Error("get event names ordered by occurence and recency")
 			c.AbortWithStatus(http.StatusInternalServerError)
@@ -119,7 +119,7 @@ func GetEventPropertiesHandler(c *gin.Context) {
 	logCtx.WithField("decodedEventName", eventName).Debug("Decoded event name on properties request.")
 
 	if helpers.IsProjectWhitelistedForEventUserCache(projectId) {
-		properties, err = M.GetPropertiesByEvent(projectId, eventName, 2500, 30)
+		properties, err = M.GetPropertiesByEvent(projectId, eventName, 2500, 10)
 		if err != nil {
 			logCtx.WithError(err).Error("get properties by event")
 			c.AbortWithStatus(http.StatusInternalServerError)
@@ -208,7 +208,7 @@ func GetEventPropertyValuesHandler(c *gin.Context) {
 	log.WithField("decodedEventName", eventName).Debug("Decoded event name on properties value request.")
 
 	if helpers.IsProjectWhitelistedForEventUserCache(projectId) {
-		propertyValues, err = M.GetPropertyValuesByEventProperty(projectId, eventName, propertyName, 2500, 30)
+		propertyValues, err = M.GetPropertyValuesByEventProperty(projectId, eventName, propertyName, 2500, 10)
 		if err != nil {
 			logCtx.WithError(err).Error("get properties values by event property")
 			c.AbortWithStatus(http.StatusInternalServerError)
