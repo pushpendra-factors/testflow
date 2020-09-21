a3083c95dd33de625cebdd8b256137ed93d9b9ee
Vishnu Baliga <baliga-factors@users.noreply.github.com>
2020-09-21 22:46:06 +0530

    sdk settings ui contents

--- frontend-new/src/Views/Settings/ProjectSettings/JavascriptSDK.js
+++ frontend-new/src/Views/Settings/ProjectSettings/JavascriptSDK.js
@@ -5,16 +5,15 @@ import {
 import { Text } from 'factorsComponents';
 const { TabPane } = Tabs;
 
-
 const ViewSetup = () => {
   return (
     <Row>
           <Col span={24}>
             <Text type={'title'} level={5} weight={'bold'} color={'grey'} extraClass={'m-0 mt-2'}>Setup 1</Text>
-            <Text type={'paragraph'}  extraClass={'m-0'}>Add the below javascript code on every page between the {`<head>`} and {`</head>`} tags.</Text>
+            <Text type={'paragraph'} extraClass={'m-0'}>Add the below javascript code on every page between the {'<head>'} and {'</head>'} tags.</Text>
           </Col>
           <Col span={24}>
-            <pre className={`fa-code-block my-4`}>
+            <pre className={'fa-code-block my-4'}>
             <code>
 {`<script>
 (function(c){var s=document.createElement("script");s.type="text/javascript";if(s.readyState){s.onreadystatechange=function(){if(s.readyState=="loaded"||s.readyState=="complete"){s.onreadystatechange=null;c()}}}else{s.onload=function(){c()}}s.src="https://app.factors.ai/assets/factors.js";s.async=true;d=!!document.body?document.body:document.head;d.appendChild(s)})(function(){factors.init("h0tfn8hfvw66gdhbu4ixnwr7evt3h42k")})
@@ -24,22 +23,21 @@ const ViewSetup = () => {
           </Col>
           <Col span={24}>
             <Text type={'title'} level={5} weight={'bold'} color={'grey'} extraClass={'m-0 mt-4'}>Setup 2</Text>
-            <Text type={'paragraph'}  extraClass={'m-0'}>Send us an event (Enable Auto-track for capturing user visits automatically).</Text>
+            <Text type={'paragraph'} extraClass={'m-0'}>Send us an event (Enable Auto-track for capturing user visits automatically).</Text>
           </Col>
           <Col span={24}>
-            <pre className={`fa-code-block my-4`}>
+            <pre className={'fa-code-block my-4'}>
             <code>
-{`factors.track("YOUR_EVENT");`}
+{'factors.track("YOUR_EVENT");'}
             </code>
             </pre>
           </Col>
-          <Col span={24}> 
-            <Text type={'paragraph'}  extraClass={'m-0 mt-2'}>For detailed instructions on how to install and initialize the JavaScript SDK please refer to our <a href="#!">JavaScript developer documentation.</a></Text>
+          <Col span={24}>
+            <Text type={'paragraph'} extraClass={'m-0 mt-2'}>For detailed instructions on how to install and initialize the JavaScript SDK please refer to our <a href="#!">JavaScript developer documentation.</a></Text>
           </Col>
     </Row>
-  )
-}
-
+  );
+};
 
 function EditUserDetails() {
   const [dataLoading, setDataLoading] = useState(true);
