8ff8518aedd107c3aa3f9bcc38d12d9ba3c7779b
Jitesh Kriplani <jiteshkriplani@Jiteshs-MacBook-Pro.local>
2020-09-24 22:20:13 +0530

    spark and line chart componentised

--- frontend-new/src/Views/EventsAnalytics/TotalEvents/index.js
+++ frontend-new/src/Views/EventsAnalytics/TotalEvents/index.js
@@ -1,18 +1,15 @@
 import React, { useState } from 'react';
-import { Menu, Dropdown, Button } from 'antd';
-import SparkChart from './SparkChart';
 import TotalEventsTable from './TotalEventsTable';
 import { getSingleEventAnalyticsData, getDataInLineChartFormat, getMultiEventsAnalyticsData } from '../utils';
-import EventHeader from '../EventHeader';
-import styles from '../index.module.scss';
-import { SVG } from '../../../components/factorsComponents';
 import { generateColors } from '../../CoreQuery/FunnelsResultPage/utils';
-import LineChart from '../TotalUsers/LineChart';
+import ChartTypeDropdown from '../../../components/ChartTypeDropdown';
+import SparkLineChart from '../../../components/SparkLineChart';
+import LineChart from '../../../components/LineChart';
 
 
 function TotalEvents({ queries }) {
   const appliedColors = generateColors(queries.length);
-  const [chartType, setChartType] = useState('sparklines');
+  const [chartType, setChartType] = useState('linechart');
 
   const eventsMapper = {};
   const reverseEventsMapper = {};
@@ -45,73 +42,15 @@ function TotalEvents({ queries }) {
     }
   ]
 
-  const menu = (
-    <Menu className={styles.dropdownMenu}>
-      {menuItems.map(item => {
-        return (
-          <Menu.Item key={item.key} onClick={setChartType.bind(this, item.key)} className={`${styles.dropdownMenuItem} ${chartType === item.key ? styles.active : ''}`}>
-            <div className={`flex items-center`}>
-              <SVG extraClass="mr-1" name={item.key} size={25} color={chartType === item.key ? '#8692A3' : '#3E516C'} />
-              <span className="mr-3">{item.name}</span>
-              {chartType === item.key ? (
-                <SVG name="checkmark" size={17} color="#8692A3" />
-              ) : null}
-            </div>
-          </Menu.Item>
-        )
-      })}
-    </Menu>
-  );
-
-  let sparkLinesJsx;
-
-  if (queries.length === 1) {
-
-    let total = 0;
-
-    chartsData.forEach(elem => {
-      total += elem[eventsMapper[queries[0]]];
-    });
-
-    sparkLinesJsx = (
-      <div className="flex justify-center items-center mt-8">
-        <div className="w-1/4">
-          <EventHeader bgColor="#4D7DB4" query={queries[0]} total={total} />
-        </div>
-        <div className="w-3/4">
-          <SparkChart event={eventsMapper[queries[0]]} page="totalEvents" chartData={chartsData} chartColor="#4D7DB4" />
-        </div>
-      </div>
-    )
-  } else {
-    sparkLinesJsx = (
-      <div className="flex flex-wrap mt-8">
-        {queries.map((q, index) => {
-          let total = 0;
-          const data = chartsData.map(elem => {
-            return {
-              date: elem.date,
-              [eventsMapper[q]]: elem[eventsMapper[q]]
-            };
-          });
-          data.forEach(elem => {
-            total += elem[eventsMapper[q]];
-          });
-
-          return (
-            <div key={q + index} className="w-1/3 mt-4 px-1">
-              <div className="flex flex-col">
-                <EventHeader total={total} query={q} bgColor={appliedColors[index]} />
-                <div className="mt-8">
-                  <SparkChart event={eventsMapper[q]} page="totalEvents" chartData={data} chartColor={appliedColors[index]} />
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    )
-  }
+  const sparkLinesJsx = (
+    <SparkLineChart
+      queries={queries}
+      chartsData={chartsData}
+      parentClass="flex justify-center items-center flex-wrap mt-8"
+      appliedColors={appliedColors}
+      eventsMapper={eventsMapper}
+    />
+  )
 
   return (
     <div className="total-events">
@@ -120,17 +59,17 @@ function TotalEvents({ queries }) {
 
         </div>
         <div className="user-actions">
-          <Dropdown overlay={menu}>
-            <Button className={`ant-dropdown-link flex items-center ${styles.dropdownBtn}`}>
-              <SVG name={chartType} size={25} color="#0E2647" />
-              <SVG name={'dropdown'} size={25} color="#3E516C" />
-            </Button>
-          </Dropdown>
+          <ChartTypeDropdown
+            chartType={chartType}
+            menuItems={menuItems}
+            onClick={(item) => {
+              setChartType(item.key);
+            }}
+          />
         </div>
       </div>
       {chartType === 'sparklines' ? (
         <div>{sparkLinesJsx}</div>
-
       ) : (
           <div className="flex mt-8">
             <LineChart chartData={getDataInLineChartFormat(chartsData, queries, eventsMapper)} appliedColors={appliedColors} queries={queries} />
--- frontend-new/src/Views/EventsAnalytics/index.js
+++ frontend-new/src/Views/EventsAnalytics/index.js
@@ -6,7 +6,6 @@ import EventsInfo from '../CoreQuery/FunnelsResultPage/EventsInfo';
 import ContentTabs from '../../components/ContentTabs';
 import TotalEvents from './TotalEvents';
 import { SVG } from '../../components/factorsComponents';
-import TotalUsers from './TotalUsers';
 
 function EventsAnalytics({ queries }) {
   const [activeKey, setActiveKey] = useState('1');
@@ -26,35 +25,35 @@ function EventsAnalytics({ queries }) {
       key: '2',
       title: 'Total Users',
       titleIcon: <SVG name={'totalusers'} size={24} color={activeKey === '2' ? '#3E516C' : '#8692A3'} />,
-      content: <TotalUsers queries={queries} />
+      content: <TotalEvents queries={queries} />
     },
     {
       key: '3',
       title: 'Active Users',
       titleIcon: <SVG name={'activeusers'} size={24} color={activeKey === '3' ? '#3E516C' : '#8692A3'} />,
-      content: <div>Coming Soon</div>
+      content: <TotalEvents queries={queries} />
     },
     {
       key: '4',
       title: 'Frequency',
       titleIcon: <SVG name={'frequency'} size={24} color={activeKey === '4' ? '#3E516C' : '#8692A3'} />,
-      content: <div>Coming Soon</div>
+      content: <TotalEvents queries={queries} />
     }
   ];
 
   return (
     <>
-            <Header>
-                <div className="flex py-4 justify-end">
-                    <Button type="primary" icon={<PoweroffOutlined />} >Save query as</Button>
-                </div>
-                <div className="py-4">
-                    <EventsInfo queries={queries} />
-                </div>
-            </Header>
-            <div className="mt-40 mb-8 fa-container">
-                <ContentTabs onChange={handleTabChange} activeKey={activeKey} tabItems={tabItems} />
-            </div>
+      <Header>
+        <div className="flex py-4 justify-end">
+          <Button type="primary" icon={<PoweroffOutlined />} >Save query as</Button>
+        </div>
+        <div className="py-4">
+          <EventsInfo queries={queries} />
+        </div>
+      </Header>
+      <div className="mt-40 mb-8 fa-container">
+        <ContentTabs onChange={handleTabChange} activeKey={activeKey} tabItems={tabItems} />
+      </div>
     </>
   );
 }
