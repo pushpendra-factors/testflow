d57a8bf9d06d288e07ae2283d4914a598f412995
Jitesh Kriplani <jiteshkriplani@Jiteshs-MacBook-Pro.local>
2020-10-01 20:12:22 +0530

    minor indentation changes

--- frontend-new/src/Views/EventsAnalytics/BreakdownCharts/index.js
+++ frontend-new/src/Views/EventsAnalytics/BreakdownCharts/index.js
@@ -1,77 +1,80 @@
 import React, { useState } from 'react';
 import ChartTypeDropdown from '../../../components/ChartTypeDropdown';
-import TotalEventsTable from '../TotalEvents/TotalEventsTable';
+// import TotalEventsTable from '../TotalEvents/TotalEventsTable';
 import { singleEventSinglePropertyDateTimeResponse } from '../SampleResponse';
 import { formatSingleEventSinglePropertyData } from '../utils';
 import BarChart from '../../../components/BarChart';
 
-function BreakdownCharts({ queries, eventsMapper, reverseEventsMapper, breakdown }) {
-    const [hiddenEvents, setHiddenEvents] = useState([]);
-    const [chartType, setChartType] = useState('barchart');
+function BreakdownCharts({
+  queries, eventsMapper, reverseEventsMapper, breakdown
+}) {
+  console.log(queries, eventsMapper, reverseEventsMapper);
+  // const [hiddenEvents, setHiddenEvents] = useState([]);
+  const [chartType, setChartType] = useState('barchart');
 
-    let chartsData = [];
-    if (breakdown.length === 1) {
-        chartsData = formatSingleEventSinglePropertyData(singleEventSinglePropertyDateTimeResponse);
+  let chartsData = [];
+  if (breakdown.length === 1) {
+    chartsData = formatSingleEventSinglePropertyData(singleEventSinglePropertyDateTimeResponse);
+  }
+
+  if (!chartsData.length) {
+    return null;
+  }
+
+  const menuItems = [
+    {
+      key: 'barchart',
+      onClick: setChartType,
+      name: 'Barchart'
+    },
+    {
+      key: 'linechart',
+      onClick: setChartType,
+      name: 'Line Chart'
     }
+  ];
 
-    if (!chartsData.length) {
-        return null;
-    }
+  let chartContent = null;
 
-    const menuItems = [
-        {
-            key: 'barchart',
-            onClick: setChartType,
-            name: 'Barchart'
-        },
-        {
-            key: 'linechart',
-            onClick: setChartType,
-            name: 'Line Chart'
-        }
-    ];
+  if (chartType === 'barchart') {
+    chartContent = (
+      <div className="flex mt-8">
+        <BarChart
+          chartData={[...chartsData.slice(0, 7)]}
+        />
+      </div>
+    );
+  }
 
-    let chartContent = null;
+  return (
+    <div className="total-events">
+      <div className="flex items-center justify-between">
+        <div className="filters-info">
 
-    if (chartType === 'barchart') {
-        chartContent = (
-            <div className="flex mt-8">
-                <BarChart
-                    chartData={[...chartsData.slice(0, 7)]}
-                />
-            </div>
-        )
-    }
-
-    return (
-        <div className="total-events">
-            <div className="flex items-center justify-between">
-                <div className="filters-info">
-
-                </div>
-                <div className="user-actions">
-                    <ChartTypeDropdown
-                        chartType={chartType}
-                        menuItems={menuItems}
-                        onClick={(item) => {
-                            setChartType(item.key);
-                        }}
-                    />
-                </div>
-            </div>
-            {chartContent}
-            {/* <div className="mt-8">
-                <TotalEventsTable
-                    data={chartsData}
-                    events={queries}
-                    reverseEventsMapper={reverseEventsMapper}
-                    chartType={chartType}
-                    setHiddenEvents={setHiddenEvents}
-                    hiddenEvents={hiddenEvents}
-                />
-            </div> */}
         </div>
-    )
+        <div className="user-actions">
+          <ChartTypeDropdown
+            chartType={chartType}
+            menuItems={menuItems}
+            onClick={(item) => {
+              setChartType(item.key);
+            }}
+          />
+        </div>
+      </div>
+      {chartContent}
+      {/* <div className="mt-8">
+        <TotalEventsTable
+          data={chartsData}
+          events={queries}
+          reverseEventsMapper={reverseEventsMapper}
+          chartType={chartType}
+          setHiddenEvents={setHiddenEvents}
+          hiddenEvents={hiddenEvents}
+        />
+      </div> */}
+    </div>
+  );
 }
 
 export default BreakdownCharts;
