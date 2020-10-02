dd9e23b0b0e078949ac78274d00adda8b8bc33d7
Maisa <maisa@factors.ai>
2020-10-02 15:49:47 +0530

    added foreign key in salesforce document

--- backend/src/factors/migrations/db/31_add_salesforce_documents_table.sql
+++ backend/src/factors/migrations/db/31_add_salesforce_documents_table.sql
@@ -1,5 +1,4 @@
 -- UP
-
 CREATE TABLE public.salesforce_documents (
     id text NOT NULL,
     project_id bigint NOT NULL,
@@ -12,13 +11,15 @@ CREATE TABLE public.salesforce_documents (
     created_at timestamp with time zone,
     updated_at timestamp with time zone,
     CONSTRAINT salesforce_documents_pkey PRIMARY KEY (project_id, id, type, timestamp)
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
+) WITH (OIDS = FALSE) TABLESPACE pg_default;
 
-ALTER TABLE public.salesforce_documents OWNER to postgres;
+ALTER TABLE
+    public.salesforce_documents OWNER to postgres;
+
+ALTER TABLE
+    salesforce_documents
+ADD
+    CONSTRAINT salesforce_documents_foreign FOREIGN KEY (project_id) REFERENCES public.projects (id) MATCH SIMPLE ON UPDATE RESTRICT ON DELETE RESTRICT
 
 -- DOWN
 -- DROP TABLE salesforce_documents;
