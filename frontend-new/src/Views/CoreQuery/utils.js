a620c630dfb1d2ec3aee34b496b446168793f0dd
Jitesh Kriplani <jiteshkriplani@Jiteshs-MacBook-Pro-2.local>
2020-10-09 14:26:15 +0530

    lint issues fixed

--- frontend-new/src/Views/CoreQuery/utils.js
+++ frontend-new/src/Views/CoreQuery/utils.js
@@ -1,4 +1,4 @@
-export const initialResultState = [1, 2, 3, 4].map(elem => {
+export const initialResultState = [1, 2, 3, 4].map(_ => {
   return { loading: false, error: false, data: null };
 });
 
@@ -6,8 +6,8 @@ export const calculateFrequencyData = (eventData, userData) => {
   const rows = eventData.result_group[0].rows.map((elem, index) => {
     const eventVals = elem.slice(1).map((e, idx) => {
       if (!e) return e;
-      const e_val = e / userData.result_group[0].rows[index][idx + 1];
-      return e_val % 1 !== 0 ? parseFloat(e_val.toFixed(2)) : e_val;
+      const eVal = e / userData.result_group[0].rows[index][idx + 1];
+      return eVal % 1 !== 0 ? parseFloat(eVal.toFixed(2)) : eVal;
     });
     return [elem[0], ...eventVals];
   });
@@ -19,11 +19,19 @@ export const calculateActiveUsersData = (userData, sessionData) => {
   const rows = userData.result_group[0].rows.map((elem) => {
     const eventVals = elem.slice(1).map((e) => {
       if (!e) return e;
-      const e_val = sessionData.result_group[0].rows[0] / e;
-      return e_val % 1 !== 0 ? parseFloat(e_val.toFixed(2)) : e_val;
+      const eVal = sessionData.result_group[0].rows[0] / e;
+      return eVal % 1 !== 0 ? parseFloat(eVal.toFixed(2)) : eVal;
     });
     return [elem[0], ...eventVals];
   });
   const result = { result_group: [{ ...userData.result_group[0], rows }] };
   return result;
 };
+
+
+export const hasApiFailed = (res) => {
+  if(res.result_group && res.result_group[0] && res.result_group[0].headers && (res.result_group[0].headers.indexOf('error') > -1)) {
+    return true;
+  }
+  return false;
+}
