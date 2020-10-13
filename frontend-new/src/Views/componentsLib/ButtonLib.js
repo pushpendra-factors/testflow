b81d9ae84674e5caf555a0acc1c4f5592c89bc6d
Vishnu Baliga <baliga-factors@users.noreply.github.com>
2020-10-13 10:35:23 +0530

    buttons sizes init

--- frontend-new/src/Views/componentsLib/ButtonLib.js
+++ frontend-new/src/Views/componentsLib/ButtonLib.js
@@ -20,6 +20,23 @@ function ButtonLib() {
 
       <Row>
         <Col span={4}>
+          <Button size="large" type="primary">Primary Button</Button>
+        </Col>
+        <Col span={4}>
+          <Button size="large" >Default Button</Button>
+        </Col>
+        {/* <Col span={4}>
+          <Button size="large" type="dashed">Dashed Button</Button>
+        </Col> */}
+        <Col span={4}>
+          <Button size="large" type="text">Text Button</Button>
+        </Col>
+        <Col span={4}>
+          <Button size="large" type="link">Secondary Button</Button>
+        </Col>
+      </Row>
+      <Row className={'my-6'}>
+        <Col span={4}>
           <Button type="primary">Primary Button</Button>
         </Col>
         <Col span={4}>
@@ -56,19 +73,19 @@ function ButtonLib() {
 
       <Row className={'my-6'}>
         <Col span={4}>
-          <Button type="primary" icon={<PoweroffOutlined />} />
+          <Button size="medium" shape="circle"  type="primary" icon={<PoweroffOutlined />} />
         </Col>
         <Col span={4}>
-          <Button icon={<PoweroffOutlined />} />
+          <Button size="medium" shape="circle"  icon={<PoweroffOutlined />} />
         </Col>
         {/* <Col span={4}>
           <Button type="dashed" icon={<PoweroffOutlined />}/>
         </Col> */}
         <Col span={4}>
-          <Button type="text" icon={<PoweroffOutlined />}/>
+          <Button size="medium"  shape="circle" type="text"icon={<PoweroffOutlined />}/>
         </Col>
         <Col span={4}>
-          <Button type="link" icon={<PoweroffOutlined />}/>
+          <Button  size="medium" shape="circle" type="link" icon={<PoweroffOutlined />}/>
         </Col>
       </Row>
 
