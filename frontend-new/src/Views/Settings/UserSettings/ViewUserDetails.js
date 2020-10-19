eda3b4596c8d5e69890460ecd746dee1d3dc58a6
Jitesh Kriplani <jiteshkriplani@Jiteshs-MacBook-Pro-2.local>
2020-10-20 03:27:48 +0530

    fixed dependencies

--- frontend-new/src/Views/Settings/UserSettings/ViewUserDetails.js
+++ frontend-new/src/Views/Settings/UserSettings/ViewUserDetails.js
@@ -2,21 +2,21 @@ import React, { useState, useEffect } from 'react';
 import {
   Row, Col, Button, Avatar, Skeleton
 } from 'antd';
-import { Text } from 'factorsComponents';
+import { Text } from '../../../components/factorsComponents';
 import { UserOutlined } from '@ant-design/icons';
 import { fetchAgentInfo } from '../../../reducers/agentActions';
 import { connect } from 'react-redux';
 
-function ViewUserDetails(props) {
+function ViewUserDetails({ fetchAgentInfo, editDetails, editPassword }) {
   const [dataLoading, setDataLoading] = useState(true);
   const [userData, setuserData] = useState(null);
 
   useEffect(() => {
-    props.fetchAgentInfo().then((response) => {
+    fetchAgentInfo().then((response) => {
       setuserData(response.data);
       setDataLoading(false);
     });
-  }, []);
+  }, [fetchAgentInfo]);
 
   return (
     <>
@@ -28,7 +28,7 @@ function ViewUserDetails(props) {
         </Row>
         <Row className={'mt-2'}>
           <Col>
-            { dataLoading ? <Skeleton.Avatar active={true} size={104} shape={'square'} />
+            {dataLoading ? <Skeleton.Avatar active={true} size={104} shape={'square'} />
               : <Avatar size={104} shape={'square'} icon={<UserOutlined />} />
             }
             <Text type={'paragraph'} mini extraClass={'m-0 mt-1'} color={'grey'} >A photo helps personalise your account</Text>
@@ -37,7 +37,7 @@ function ViewUserDetails(props) {
         <Row className={'mt-6'}>
           <Col>
             <Text type={'title'} level={7} extraClass={'m-0'}>Name</Text>
-            { dataLoading ? <Skeleton.Input style={{ width: 200 }} active={true} size={'small'} />
+            {dataLoading ? <Skeleton.Input style={{ width: 200 }} active={true} size={'small'} />
               : <Text type={'title'} level={6} extraClass={'m-0'} weight={'bold'}>{`${userData.first_name} ${userData.last_name}`}</Text>
             }
           </Col>
@@ -45,7 +45,7 @@ function ViewUserDetails(props) {
         <Row className={'mt-6'}>
           <Col>
             <Text type={'title'} level={7} extraClass={'m-0'}>Email</Text>
-            { dataLoading ? <Skeleton.Input style={{ width: 200 }} active={true} size={'small'} />
+            {dataLoading ? <Skeleton.Input style={{ width: 200 }} active={true} size={'small'} />
               : <Text type={'title'} level={6} extraClass={'m-0'} weight={'bold'}>{userData.email}</Text>
             }
           </Col>
@@ -53,7 +53,7 @@ function ViewUserDetails(props) {
         <Row className={'mt-6'}>
           <Col>
             <Text type={'title'} level={7} extraClass={'m-0'}>Mobile</Text>
-            { dataLoading ? <Skeleton.Input style={{ width: 200 }} active={true} size={'small'} />
+            {dataLoading ? <Skeleton.Input style={{ width: 200 }} active={true} size={'small'} />
               : <Text type={'title'} level={6} extraClass={'m-0'} weight={'bold'}>+91-8123456789</Text>
             }
           </Col>
@@ -61,15 +61,15 @@ function ViewUserDetails(props) {
         <Row className={'mt-6'}>
           <Col>
             <Text type={'title'} level={7} extraClass={'m-0'}>Password</Text>
-            { dataLoading ? <Skeleton.Input style={{ width: 200 }} active={true} size={'small'} />
+            {dataLoading ? <Skeleton.Input style={{ width: 200 }} active={true} size={'small'} />
               : <Text type={'title'} level={6} extraClass={'m-0'} weight={'bold'}>&#8226; &#8226; &#8226; &#8226; &#8226; &#8226;</Text>
             }
           </Col>
         </Row>
         <Row className={'mt-6'}>
           <Col className={'flex justify-start items-center'}>
-            <Button disabled={dataLoading} onClick={props.editDetails}>Edit Details</Button>
-            <Button disabled={dataLoading} className={'ml-4'} onClick={props.editPassword} >Change Password</Button>
+            <Button disabled={dataLoading} onClick={editDetails}>Edit Details</Button>
+            <Button disabled={dataLoading} className={'ml-4'} onClick={editPassword} >Change Password</Button>
           </Col>
         </Row>
       </div>
