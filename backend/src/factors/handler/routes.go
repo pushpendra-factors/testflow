1b232bfc9a05bdbadcd73a03b5137ba317964262
Maisa <maisa@factors.ai>
2020-10-19 21:00:21 +0530

    amp identify to POST

--- backend/src/factors/handler/routes.go
+++ backend/src/factors/handler/routes.go
@@ -117,7 +117,7 @@ func InitSDKServiceRoutes(r *gin.Engine) {
 	ampSdkRouteGroup := r.Group(ROUTE_SDK_AMP_ROOT)
 	ampSdkRouteGroup.POST("/event/track", SDKAMPTrackHandler)
 	ampSdkRouteGroup.POST("/event/update_properties", SDKAMPUpdateEventPropertiesHandler)
-	ampSdkRouteGroup.GET("/user/identify", SDKAMPIdentifyHandler)
+	ampSdkRouteGroup.POST("/user/identify", SDKAMPIdentifyHandler)
 
 	intRouteGroup := r.Group(ROUTE_INTEGRATIONS_ROOT)
 	intRouteGroup.POST("/segment", mid.SetScopeProjectPrivateToken(), IntSegmentHandler)
