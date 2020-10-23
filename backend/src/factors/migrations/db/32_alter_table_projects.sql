f55810d20f7a46439051c2fdb67e2dfc1ada1751
janani-somaskanda <janani@factors.ai>
2020-10-23 11:50:45 +0530

    fixing typo

--- backend/src/factors/migrations/db/32_alter_table_projects.sql
+++ backend/src/factors/migrations/db/32_alter_table_projects.sql
@@ -1,5 +1,5 @@
 ALTER TABLE projects 
-ADD COLUMN project_uri VARCHAR
+ADD COLUMN project_uri VARCHAR,
 ADD COLUMN time_format VARCHAR, 
 ADD COLUMN date_format VARCHAR, 
 ADD COLUMN time_zone VARCHAR
