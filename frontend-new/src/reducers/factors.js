b3dc1c8e486c541ba58f4e35001899288b4514a7
Vishnu Baliga <baliga-factors@users.noreply.github.com>
2020-11-21 16:17:19 +0530

    fetch and wiring user-properties and events in configure-data-points

--- frontend-new/src/reducers/factors.js
+++ frontend-new/src/reducers/factors.js
@@ -18,6 +18,18 @@
           case 'FETCH_GOALS_FULFILLED': {
             return { ...state, error: action.payload };
           } 
+          case 'FETCH_TRACKED_EVENTS_FULFILLED': {
+            return { ...state, tracked_events: action.payload };
+          } 
+          case 'FETCH_TRACKED_EVENTS_REJECTED': {
+            return { ...state, error: action.payload };
+          } 
+          case 'FETCH_TRACKED_USER_PROPERTIES_FULFILLED': {
+            return { ...state, tracked_user_property: action.payload };
+          } 
+          case 'FETCH_TRACKED_USER_PROPERTIES_REJECTED': {
+            return { ...state, error: action.payload };
+          } 
         }
         return state;
       }
@@ -25,17 +37,46 @@
       
       
 
-      export function fetchFactorsGoals(projectID) {
-        return function(dispatch) {
-          return new Promise((resolve,reject) => {
-            get(dispatch, host + "projects/"+projectID+"/v1/factors/goals")
-              .then((response)=>{        
-                dispatch({type:"FETCH_GOALS_FULFILLED", payload: response.data});
-                resolve(response)
-              }).catch((err)=>{        
-                dispatch({type:"FETCH_GOALS_REJECTED", payload: err});
-                reject(err);
-              });
+  export function fetchFactorsGoals(projectID) {
+    return function(dispatch) {
+      return new Promise((resolve,reject) => {
+        get(dispatch, host + "projects/"+projectID+"/v1/factors/goals")
+          .then((response)=>{        
+            dispatch({type:"FETCH_GOALS_FULFILLED", payload: response.data});
+            resolve(response)
+          }).catch((err)=>{        
+            dispatch({type:"FETCH_GOALS_REJECTED", payload: err});
+            reject(err);
           });
-        }
-      }
+      });
+    }
+  }
+  
+  export function fetchFactorsTrackedEvents(projectID) {
+    return function(dispatch) {
+      return new Promise((resolve,reject) => {
+        get(dispatch, host + "projects/"+projectID+"/v1/factors/tracked_event")
+          .then((response)=>{        
+            dispatch({type:"FETCH_TRACKED_EVENTS_FULFILLED", payload: response.data});
+            resolve(response)
+          }).catch((err)=>{        
+            dispatch({type:"FETCH_TRACKED_EVENTS_REJECTED", payload: err});
+            reject(err);
+          });
+      });
+    }
+  }
+  export function fetchFactorsTrackedUserProperties(projectID) {
+    return function(dispatch) {
+      return new Promise((resolve,reject) => {
+        get(dispatch, host + "projects/"+projectID+"/v1/factors/tracked_user_property")
+          .then((response)=>{        
+            dispatch({type:"FETCH_TRACKED_USER_PROPERTIES_FULFILLED", payload: response.data});
+            resolve(response)
+          }).catch((err)=>{        
+            dispatch({type:"FETCH_TRACKED_USER_PROPERTIES_REJECTED", payload: err});
+            reject(err);
+          });
+      });
+    }
+  }
