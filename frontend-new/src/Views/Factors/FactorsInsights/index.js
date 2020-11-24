29f6635f9927c90fd41e86cb196845201ea25688
Vishnu Baliga <baliga-factors@users.noreply.github.com>
2020-11-24 18:48:00 +0530

    insights re-wired with updated data-set

--- frontend-new/src/Views/Factors/FactorsInsights/index.js
+++ frontend-new/src/Views/Factors/FactorsInsights/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
-  Tabs, Row, Col, Progress, Modal
+  Tabs, Row, Col, Progress, Modal, Button
 } from 'antd';
 import { Text, SVG } from 'factorsComponents';
 import { fetchGoalInsights } from 'Reducers/factors';
@@ -16,13 +16,13 @@ const InsightHighlightItem = ({ data }) => {
                     <Col span={16}>
                         <div className={'relative border-left--thin-2 m-0 pl-16 py-2'}>
                             <div className={'w-full'}>
-                            <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0'} >All visitors</Text>
+                            <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0'} >{data.goal?.st_en}</Text>
                             <Progress percent={100} strokeColor={'#5949BC'} showInfo={false} />
 
                             <Text type={'title'} level={2} weight={'bold'} extraClass={'m-0'} >{data.overall_percentage_text}</Text>
 
                             <Progress percent={data.overall_percentage} strokeColor={'#F9C06E'} showInfo={false} />
-                            <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0'} >Deal won</Text>
+                            <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0'} >{data.goal?.en_en}</Text>
                             </div>
 
                             <div className={'fa-insights-box--highlight'}>
@@ -32,7 +32,7 @@ const InsightHighlightItem = ({ data }) => {
                                         <Text type={'title'} level={4} color={'grey'} weight={'bold'} extraClass={'m-0'} >1x</Text>
                                         <Text type={'title'} level={7} color={'grey'} extraClass={'m-0'} >Impact</Text>
                                     </div>
-                                    <Text type={'title'} level={5} color={'yellow'} weight={'bold'} extraClass={'m-0'} >{data.insights_user_count}</Text>
+                                    <Text type={'title'} level={5} color={'yellow'} weight={'bold'} extraClass={'m-0'} >{data.goal_user_count}</Text>
                                 </div>
                             </div>
                         </div>
@@ -67,10 +67,10 @@ const SubInsightItem = ({ SubInsightsData, showModal, handleClose }) => {
                                 <Text type={'title'} level={3} weight={'bold'} extraClass={'m-0'} >{`${dataItem.factors_insights_multiplier}x`}</Text>
 
                                 <div className={'mt-8 w-9/12'}>
-                                <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0'} >{dataItem.factors_users_count}</Text>
+                                <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0'} >{dataItem.factors_insights_users_count}</Text>
                                 <Progress percent={100} strokeColor={'#5949BC'} showInfo={false} />
 
-                                <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0 mt-2'} >{`${dataItem.factors_insights_percentage} (${dataItem.factors_insights_percentage}% goal completion)`}</Text>
+                                <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0 mt-2'} >{`${dataItem.factors_goal_users_count} (${dataItem.factors_insights_percentage}% goal completion)`}</Text>
                                 <Progress percent={dataItem.factors_insights_percentage} strokeColor={'#F9C06E'} showInfo={false} />
                                 </div>
 
@@ -94,7 +94,10 @@ const SubInsightItem = ({ SubInsightsData, showModal, handleClose }) => {
     );
   } else return null;
 };
-const InsightItem = ({ data, type, showSubInsightsData }) => {
+
+const InsightItem = ({
+  data, type, showSubInsightsData, displayType = false
+}) => {
   if (data) {
     return data.insights.map((dataItem, index) => {
       if (dataItem.factors_insights_type === type) {
@@ -102,19 +105,29 @@ const InsightItem = ({ data, type, showSubInsightsData }) => {
                   <div key={index} className={'relative border-bottom--thin'}>
                       <Row gutter={[0, 0]} justify={'center'}>
                           <Col span={16}>
-                              <div className={'relative border-left--thin-2 m-0 pl-16 py-8'} onClick={() => { showSubInsightsData(dataItem.factors_sub_insights); }}>
-                                  {/* <Text type={'title'} level={4} weight={'bold'} extraClass={'m-0'} >of which visitors coming via <a>.../product</a> shows 2x higher goal completion.</Text> */}
+                              <div className={'relative border-left--thin-2 m-0 pl-16 py-8 cursor-pointer fa-insight-item'} onClick={() => { showSubInsightsData(dataItem.factors_sub_insights); }}>
+                                 {displayType && <Text type={'paragraph'} mini color={'grey'} weight={'bold'} extraClass={'uppercase fa-insights-box--type'} >{type}</Text>}
                                   <Text type={'title'} level={4} weight={'bold'} extraClass={'m-0'} >{dataItem.factors_insights_text}</Text>
                                   <Text type={'title'} level={6} color={'grey'} extraClass={'mt-4'} >{'Higher completions for time spent on page <= 1min +3 factors'} </Text>
                                   <Text type={'title'} level={6} color={'grey'} extraClass={'mt-2'} >{'Lower completions for Time-Spent <= 10sec +2 factors'} </Text>
 
                                   <div className={'mt-8 w-9/12'}>
-                                  <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0'} >{dataItem.factors_users_count}</Text>
+                                  <div className={'flex items-center'}>
+                                    <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0'} >{dataItem.factors_insights_users_count}</Text>
+                                    <div className={'flex items-center ml-4 fa-insights-box--animate'}>  <SVG name={'corequery'} size={12} color={'grey'} /> <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0 ml-1'} >{dataItem.factors_insights_key}</Text></div>
+                                  </div>
                                   <Progress percent={100} strokeColor={'#5949BC'} showInfo={false} />
 
-                                  <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0 mt-2'} >{`${dataItem.factors_insights_percentage} (${dataItem.factors_insights_percentage}% goal completion)`}</Text>
+                                  <Text type={'title'} weight={'thin'} level={7} extraClass={'m-0 mt-2'} >{`${dataItem.factors_goal_users_count} (${dataItem.factors_insights_percentage}% goal completion)`}</Text>
                                   <Progress percent={dataItem.factors_insights_percentage} strokeColor={'#F9C06E'} showInfo={false} />
                                   </div>
+
+                                  {dataItem?.factors_sub_insights?.length > 0 && <div className={'fa-insights-box--actions'}>
+                                    <Button size={'large'}>
+                                        <SVG name={'corequery'} size={24} color={'grey'} />
+                                    </Button>
+                                  </div>
+                                  }
 
                                   <div className={'fa-insights-box--spike'}>
                                       <div className={'flex justify-end items-center'}>
@@ -122,6 +135,7 @@ const InsightItem = ({ data, type, showSubInsightsData }) => {
                                         {dataItem.factors_multiplier_increase_flag ? <SVG name={'spikeup'} size={42} /> : <SVG name={'spikedown'} size={42} />}
                                       </div>
                                   </div>
+
                               </div>
                           </Col>
                       </Row>
@@ -166,9 +180,9 @@ const FactorsInsights = ({ fetchGoalInsights, activeProject, goal_insights }) =>
                     <div className={'fa-insights--tab'}>
                     <Tabs defaultActiveKey="1" >
                         <TabPane tab="All Insights" key="1">
-                                    <InsightItem data={goal_insights} showSubInsightsData={showSubInsightsData} type={'journey'} />
-                                    <InsightItem data={goal_insights} showSubInsightsData={showSubInsightsData} type={'attribute'} />
-                                    <InsightItem data={goal_insights} showSubInsightsData={showSubInsightsData} type={'campaign'} />
+                                    <InsightItem data={goal_insights} showSubInsightsData={showSubInsightsData} displayType={true} type={'journey'} />
+                                    <InsightItem data={goal_insights} showSubInsightsData={showSubInsightsData} displayType={true} type={'attribute'} />
+                                    <InsightItem data={goal_insights} showSubInsightsData={showSubInsightsData} displayType={true} type={'campaign'} />
                         </TabPane>
                         <TabPane tab="Attributes" key="2">
                             <div className={'w-full p-4 background-color--brand-color-1'}>
