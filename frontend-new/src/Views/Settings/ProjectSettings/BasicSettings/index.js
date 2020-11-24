25470f19e2c3fa414a4c2b83f8474b4f2eb60355
Vishnu Baliga <baliga-factors@users.noreply.github.com>
2020-11-24 13:14:16 +0530

    fixes #914 no19

--- frontend-new/src/Views/Settings/ProjectSettings/BasicSettings/index.js
+++ frontend-new/src/Views/Settings/ProjectSettings/BasicSettings/index.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ViewBasicSettings from './ViewBasicSettings';
 import EditBasicSettings from './EditBasicSettings';
+import { connect } from 'react-redux';
+import { fetchProjectAgents, fetchAgentInfo, fetchProjects } from 'Reducers/agentActions';
 
-function BasicSettings() {
+function BasicSettings({
+  fetchProjectAgents, fetchAgentInfo, fetchProjects, activeProject
+}) {
   const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      await fetchAgentInfo();
+      await fetchProjects();
+      await fetchProjectAgents(activeProject.id);
+    };
+    getData();
+  });
   return (
     <>
     {editMode ? <EditBasicSettings setEditMode={setEditMode} /> : <ViewBasicSettings setEditMode={setEditMode} /> }
@@ -11,5 +24,7 @@ function BasicSettings() {
 
   );
 }
-
-export default BasicSettings;
+const mapStateToProps = (state) => ({
+  activeProject: state.global.active_project
+});
+export default connect(mapStateToProps, { fetchProjectAgents, fetchAgentInfo, fetchProjects })(BasicSettings);
