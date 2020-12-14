cf1133b232ef269bca51447babec1ee8b7aed3ae
Vishnu Baliga <baliga-factors@users.noreply.github.com>
2020-12-14 16:41:07 +0530

    intercom init

--- frontend-new/src/App.js
+++ frontend-new/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLayout from './Views/AppLayout';
 import Login from './Views/Pages/Login';
 import ForgotPassword from './Views/Pages/ForgotPassword';
@@ -11,7 +11,21 @@ import {
   BrowserRouter as Router, Route, Switch, Redirect
 } from 'react-router-dom';
 
-function App({ isAgentLoggedIn }) {
+function App({ isAgentLoggedIn, agent_details }) {
+
+  useEffect(()=>{
+  
+  var APP_ID = "rvffkuu7";
+  window.intercomSettings = {
+      app_id: APP_ID,
+      name: agent_details?.first_name,
+      email: agent_details?.email,
+      user_id: agent_details?.uuid
+    };
+  (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/' + APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+
+  });
+
   return (
     <div className="App">
       <Router>
@@ -30,7 +44,8 @@ function App({ isAgentLoggedIn }) {
 }
 
 const mapStateToProps = (state) => ({
-  isAgentLoggedIn: state.agent.isLoggedIn
+  isAgentLoggedIn: state.agent.isLoggedIn,
+  agent_details: state.agent.agent_details
 });
 
 export default connect(mapStateToProps)(App);
