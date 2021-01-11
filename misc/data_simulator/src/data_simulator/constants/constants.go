1b47e4b6fdf5ee4fe779ccb4f948936dd18b1de0
anil <anil@factors.ai>
2021-01-11 22:13:27 +0530

    added date fix

--- misc/data_simulator/src/data_simulator/constants/constants.go
+++ misc/data_simulator/src/data_simulator/constants/constants.go
@@ -11,6 +11,7 @@ const (
 	BUCKETNAME            = "factors-staging-datagenerator"
 	TIMEFORMAT            = "2006-01-02T15-04-05.000"
 	DATEFORMAT            = "2006-01-02"
+	DATEFORMATJOIN        = "20060102"
 	LOCALOUTPUTFOLDER     = "output"
 	UNPROCESSEDFILESCLOUD = "datagen/unprocessed"
 	PROCESSEDFILESCLOUD   = "datagen/processed"
