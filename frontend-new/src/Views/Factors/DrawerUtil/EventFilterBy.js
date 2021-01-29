f63f1b815e61a33ecc41a7b5f0747b82f8f869a8
Vishnu Baliga <68986923+baliga-factors@users.noreply.github.com>
2021-01-29 15:11:57 +0530

    != support for filters - explain (#1283)
    * suporting != for explain global categorical
    
    * suporting != for explain event level categorical
    
    Co-authored-by: Vishnu Baliga <baliga-factors@users.noreply.github.com>
    Co-authored-by: Akhil Nair <akhil@factors.ai>

--- frontend-new/src/Views/Factors/DrawerUtil/EventFilterBy.js
+++ frontend-new/src/Views/Factors/DrawerUtil/EventFilterBy.js
@@ -135,6 +135,7 @@ const renderFilterBlock = () => {
                       operatorProps={{
                         "categorical": [
                           '=',
+                          '!='
                         ],
                         "numerical": [
                           '=',
