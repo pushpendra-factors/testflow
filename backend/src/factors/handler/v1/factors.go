4d97e79e6542742c07472d4d3949470bdff4b7eb
janani-somaskanda <68629545+janani-somaskanda@users.noreply.github.com>
2021-02-04 10:13:48 +0530

    Janani/poc (#1352)
    * Distribution - insights
    
    * factors comparison - POC

--- backend/src/factors/handler/v1/factors.go
+++ backend/src/factors/handler/v1/factors.go
@@ -161,7 +161,9 @@ func GetFactorsHandler(c *gin.Context) {
 	}
 	if patternMode == "EventDistributionWithProperties" {
 		distributionProperties := make(map[string]string)
-		distributionProperties[distributionProperty] = "categorical"
+		if distributionProperty != "" {
+			distributionProperties[distributionProperty] = "categorical"
+		}
 		baseProperty := P.EventConstraints{}
 		if basePropertyType == "numerical" {
 
@@ -177,6 +179,222 @@ func GetFactorsHandler(c *gin.Context) {
 		c.JSON(http.StatusOK, res)
 		return
 	}
+}
+
+type CompareFactorsGoalParams struct {
+	Name string                  `json:"name"`
+	Rule []CreateGoalInputParams `json:"rule"`
+}
+
+func GetcompareFactorsGoalParams(c *gin.Context) (*CompareFactorsGoalParams, error) {
+	params := CompareFactorsGoalParams{}
+	err := c.BindJSON(&params)
+	if err != nil {
+		return nil, err
+	}
+	return &params, nil
+}
+
+func PostFactorsCompareHandler(c *gin.Context) {
+	projectId := U.GetScopeByKeyAsUint64(c, mid.SCOPE_PROJECT_ID)
+	if projectId == 0 {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	logCtx := log.WithFields(log.Fields{
+		"projectId": projectId,
+	})
+	comparisonMode := c.Query("comparison_mode")
+	if comparisonMode == "Week" {
+		modelId1 := uint64(0)
+		modelId2 := uint64(0)
+		modelIdParam1 := c.Query("model_id1")
+		modelIdParam2 := c.Query("model_id2")
+		var err error
+		if modelIdParam1 != "" && modelIdParam2 != "" {
+			modelId1, err = strconv.ParseUint(modelIdParam1, 10, 64)
+			modelId2, err = strconv.ParseUint(modelIdParam2, 10, 64)
+			if err != nil {
+				c.AbortWithStatus(http.StatusBadRequest)
+				return
+			}
+		}
+		inputType := c.Query("type")
+		ipParams, err := GetcreateFactorsGoalParams(c)
+		if err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+		params := MapRule(ipParams.Rule)
+		ps1, err := PW.NewPatternServiceWrapper("", projectId, modelId1)
+		if err != nil {
+			logCtx.WithError(err).Error("Pattern Service initialization failed.")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":  err.Error(),
+				"status": http.StatusBadRequest,
+			})
+			return
+		}
+		startConstraints, endConstraints := parseConstraints(params.Rule)
+		var results1, results2 PW.Factors
+		if results1, err, _ = PW.FactorV1("",
+			projectId, params.StartEvent, startConstraints,
+			params.EndEvent, endConstraints, P.COUNT_TYPE_PER_USER, ps1, "", nil); err != nil {
+			logCtx.WithError(err).Error("Factors failed.")
+			if err.Error() == "Root node not found or frequency 0" {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "No Insights Found"})
+			}
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		} else {
+			results1.Type = inputType
+			results1.GoalRule = params
+
+		}
+		ps2, err := PW.NewPatternServiceWrapper("", projectId, modelId2)
+		if err != nil {
+			logCtx.WithError(err).Error("Pattern Service initialization failed.")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":  err.Error(),
+				"status": http.StatusBadRequest,
+			})
+			return
+		}
+		if results2, err, _ = PW.FactorV1("",
+			projectId, params.StartEvent, startConstraints,
+			params.EndEvent, endConstraints, P.COUNT_TYPE_PER_USER, ps2, "", nil); err != nil {
+			logCtx.WithError(err).Error("Factors failed.")
+			if err.Error() == "Root node not found or frequency 0" {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "No Insights Found"})
+			}
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		} else {
+			results2.Type = inputType
+			results2.GoalRule = params
+
+		}
+		res := formatComparisonResults(results1, results2)
+		c.JSON(http.StatusOK, res)
+	}
+	if comparisonMode == "Property" {
+		modelId := uint64(0)
+		modelIdParam := c.Query("model_id")
+		var err error
+		if modelIdParam != "" {
+			modelId, err = strconv.ParseUint(modelIdParam, 10, 64)
+			if err != nil {
+				c.AbortWithStatus(http.StatusBadRequest)
+				return
+			}
+		}
+		inputType := c.Query("type")
+		ipParams, err := GetcompareFactorsGoalParams(c)
+		if err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+		params1 := MapRule(ipParams.Rule[0])
+		params2 := MapRule(ipParams.Rule[1])
+		ps, err := PW.NewPatternServiceWrapper("", projectId, modelId)
+		if err != nil {
+			logCtx.WithError(err).Error("Pattern Service initialization failed.")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":  err.Error(),
+				"status": http.StatusBadRequest,
+			})
+			return
+		}
+		startConstraints, endConstraints := parseConstraints(params1.Rule)
+		var results1, results2 PW.Factors
+		if results1, err, _ = PW.FactorV1("",
+			projectId, params1.StartEvent, startConstraints,
+			params1.EndEvent, endConstraints, P.COUNT_TYPE_PER_USER, ps, "", nil); err != nil {
+			logCtx.WithError(err).Error("Factors failed.")
+			if err.Error() == "Root node not found or frequency 0" {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "No Insights Found"})
+			}
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		} else {
+			results1.Type = inputType
+			results1.GoalRule = params1
+		}
+		startConstraints, endConstraints = parseConstraints(params2.Rule)
+		if results2, err, _ = PW.FactorV1("",
+			projectId, params2.StartEvent, startConstraints,
+			params2.EndEvent, endConstraints, P.COUNT_TYPE_PER_USER, ps, "", nil); err != nil {
+			logCtx.WithError(err).Error("Factors failed.")
+			if err.Error() == "Root node not found or frequency 0" {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "No Insights Found"})
+			}
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		} else {
+			results2.Type = inputType
+			results2.GoalRule = params2
+		}
+		res := formatComparisonResults(results1, results2)
+		c.JSON(http.StatusOK, res)
+	}
+}
+
+type ComparisonResult struct {
+	FirstWeek        []float64
+	SecondWeek       []float64
+	PercentageChange float64
+	Type             string
+}
+
+func formatComparisonResults(result1 PW.Factors, result2 PW.Factors) map[string]ComparisonResult {
+	result := make(map[string]ComparisonResult)
+	var key string
+	for _, insight := range result1.Insights {
+		if insight.FactorsInsightsType == PW.JOURNEYTYPE || insight.FactorsInsightsType == PW.CAMPAIGNTYPE {
+			key = insight.FactorsInsightsKey
+		} else if insight.FactorsInsightsType == PW.ATTRIBUTETYPE {
+			if insight.FactorsInsightsAttribute[0].FactorsAttributeValue != "" {
+				key = insight.FactorsInsightsAttribute[0].FactorsAttributeKey + ":" + insight.FactorsInsightsAttribute[0].FactorsAttributeValue
+			} else {
+				key = insight.FactorsInsightsAttribute[0].FactorsAttributeKey + ":" + fmt.Sprintf("%v", insight.FactorsInsightsAttribute[0].FactorsAttributeLowerBound) + "-" + fmt.Sprintf("%v", insight.FactorsInsightsAttribute[0].FactorsAttributeUpperBound)
+			}
+		}
+		result[key] = ComparisonResult{
+			FirstWeek: []float64{insight.FactorsInsightsMultiplier, insight.FactorsInsightsPercentage, insight.FactorsInsightsUsersCount, insight.FactorsGoalUsersCount},
+			Type:      insight.FactorsInsightsType,
+		}
+	}
+	for _, insight := range result2.Insights {
+		var key string
+		if insight.FactorsInsightsType == PW.JOURNEYTYPE || insight.FactorsInsightsType == PW.CAMPAIGNTYPE {
+			key = insight.FactorsInsightsKey
+		} else if insight.FactorsInsightsType == PW.ATTRIBUTETYPE {
+			if insight.FactorsInsightsAttribute[0].FactorsAttributeValue != "" {
+				key = insight.FactorsInsightsAttribute[0].FactorsAttributeKey + ":" + insight.FactorsInsightsAttribute[0].FactorsAttributeValue
+			} else {
+				key = insight.FactorsInsightsAttribute[0].FactorsAttributeKey + ":" + fmt.Sprintf("%v", insight.FactorsInsightsAttribute[0].FactorsAttributeLowerBound) + "-" + fmt.Sprintf("%v", insight.FactorsInsightsAttribute[0].FactorsAttributeUpperBound)
+			}
+		}
+		comparisonObj := ComparisonResult{}
+		var percentagebase float64
+		if val, ok := result[key]; ok {
+			comparisonObj = val
+			percentagebase = comparisonObj.FirstWeek[1]
+		}
+		comparisonObj.SecondWeek = []float64{insight.FactorsInsightsMultiplier, insight.FactorsInsightsPercentage, insight.FactorsInsightsUsersCount, insight.FactorsGoalUsersCount}
+		if percentagebase != 0.0 {
+			comparisonObj.PercentageChange = insight.FactorsInsightsPercentage / percentagebase
+		}
+		result[key] = comparisonObj
+	}
+	overallPercentageChange := result2.OverallPercentage / result1.OverallPercentage
+	result["base"] = ComparisonResult{
+		FirstWeek:        []float64{result1.OverallMultiplier, result1.OverallPercentage, result1.TotalUsersCount, result1.GoalUserCount},
+		SecondWeek:       []float64{result2.OverallMultiplier, result2.OverallPercentage, result2.TotalUsersCount, result2.GoalUserCount},
+		PercentageChange: overallPercentageChange,
+	}
+	return result
 }
 
 func extractOperator(isEqual bool) string {
