079cceb88bf0450debfef4da6602eeed1d3840e3
Mohammad Ashhar Ansari <60336936+ansari-ashhar@users.noreply.github.com>
2021-02-05 19:41:53 +0530

    Revert "Added logging for facebookDocument failure (#1368)" (#1369)
    This reverts commit 1663d9a20554b1336cf1857b06547bf03154f824.

--- backend/src/factors/handler/internal/data_service_facebook.go
+++ backend/src/factors/handler/internal/data_service_facebook.go
@@ -30,7 +30,6 @@ func DataServiceFacebookAddDocumentHandler(c *gin.Context) {
 
 	errCode := M.CreateFacebookDocument(facebookDocument.ProjectID, &facebookDocument)
 	if errCode == http.StatusInternalServerError || errCode == http.StatusBadRequest || errCode == http.StatusConflict {
-		log.WithField("document", facebookDocument).Error("Failed to decode Json request on facebook upsert document handler.")
 		c.AbortWithStatusJSON(errCode,
 			gin.H{"error": "Failed to upsert facebook document."})
 		return
