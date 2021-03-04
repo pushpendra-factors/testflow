6da78a80c36e6603884e3498b75164c13f5a4140
Anil Tiwari <67252920+anilfactors@users.noreply.github.com>
2021-03-04 16:53:18 +0530

    Facebook, Linkedin data into attribution report (#1553)
    * added facebook, linkedin impression clicks cost data
    
    * update method sign
    
    * added query for adwords, fb, linkdn
    
    * adding up the spend, clicks data for matching campaigns/keys
    
    * Id -> ID change

--- backend/src/factors/model/model.go
+++ backend/src/factors/model/model.go
@@ -64,9 +64,13 @@ type Model interface {
 		goalEventProperties []model.QueryProperty, from, to int64,
 		eventNameToIdList map[string][]interface{}) (map[string]model.UserInfo,
 		map[string][]model.UserIDPropID, map[string]int64, error)
-	AddPerformanceReportInfo(projectId uint64, attributionData map[string]*model.AttributionData,
+	AddAdwordsPerformanceReportInfo(projectId uint64, attributionData map[string]*model.AttributionData,
 		from, to int64, customerAccountId string, attributionKey string, timeZone string) (string, error)
 	GetAdwordsCurrency(projectId uint64, customerAccountId string, from, to int64) (string, error)
+	AddFacebookPerformanceReportInfo(projectId uint64, attributionData map[string]*model.AttributionData,
+		from, to int64, customerAccountId string, attributionKey string, timeZone string) error
+	AddLinkedinPerformanceReportInfo(projectId uint64, attributionData map[string]*model.AttributionData,
+		from, to int64, customerAccountId string, attributionKey string, timeZone string) error
 
 	// bigquery_setting
 	CreateBigquerySetting(setting *model.BigquerySetting) (*model.BigquerySetting, int)
