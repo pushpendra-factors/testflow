3f4a85f2bb561e0bb3e4122eccc19890e1df6a23
Dineshprabu S <44255030+dineshprabu-slashbit@users.noreply.github.com>
2021-03-23 15:54:46 +0530

    Fix - URL parse failure because of invalid URL escape (#1719)
    * Fixes for issue recreation.
    
    * Unescape invalid escape sequences.
    
    * Added comment.
    
    * Added additiona log and test with valid and invalid escape.

--- backend/src/factors/handler/integrations.go
+++ backend/src/factors/handler/integrations.go
@@ -74,10 +74,12 @@ func IntSegmentHandler(c *gin.Context) {
 	status, response := IntSegment.ReceiveEventWithQueue(token, &event,
 		C.GetSegmentRequestQueueAllowedTokens())
 
-	// send error on StatusInternalServerError (db unavailability and redis error),
-	// for segment to retry.
-	if status == http.StatusInternalServerError {
+	// send error on StatusInternalServerError.
+	// Possible when error from redis while queuing, if queue is enabled.
+	// Possible on multiple conditions when processing directly.
+	if status == http.StatusInternalServerError || status == http.StatusBadRequest {
 		c.AbortWithStatus(status)
+		return
 	}
 
 	// Always send StatusOK for failure on direct processing.
