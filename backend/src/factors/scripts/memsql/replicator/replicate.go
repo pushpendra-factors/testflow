39bb1a29dcc77281b2752e3a9404750c65043245
Dineshprabu S <44255030+dineshprabu-slashbit@users.noreply.github.com>
2021-05-06 12:44:04 +0530

    MemSQL Connection SSL Support for app-server and replicator (#2041)

--- backend/src/factors/scripts/memsql/replicator/replicate.go
+++ backend/src/factors/scripts/memsql/replicator/replicate.go
@@ -117,12 +117,7 @@ func main() {
 	memSQLUser := flag.String("memsql_user", C.MemSQLDefaultDBParams.User, "")
 	memSQLName := flag.String("memsql_name", C.MemSQLDefaultDBParams.Name, "")
 	memSQLPass := flag.String("memsql_pass", C.MemSQLDefaultDBParams.Password, "")
-	memSQLDSN := flag.String(
-		"memsql_dsn",
-		fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=30s",
-			*memSQLUser, *memSQLPass, *memSQLHost, *memSQLPort, *memSQLName),
-		"",
-	)
+	memSQLCertificate := flag.String("memsql_cert", "", "")
 	sentryDSN := flag.String("sentry_dsn", "", "Sentry DSN")
 
 	projectIDStringList := flag.String("project_ids", "", "")
@@ -194,7 +189,15 @@ func main() {
 	}
 
 	maxOpenConns := (30 * migrationRoutines) + 10
-	initMemSQLDB(*env, *memSQLDSN, maxOpenConns)
+	memSQLDBConf := C.DBConf{
+		Host:        *memSQLHost,
+		Port:        *memSQLPort,
+		User:        *memSQLUser,
+		Name:        *memSQLName,
+		Password:    *memSQLPass,
+		Certiifcate: *memSQLCertificate,
+	}
+	initMemSQLDB(*env, &memSQLDBConf, maxOpenConns)
 
 	allProjects, projectIDsMap, _ := C.GetProjectsFromListWithAllProjectSupport(*projectIDStringList, "")
 	projectIDs := C.ProjectIdsFromProjectIdBoolMap(projectIDsMap)
@@ -223,10 +226,11 @@ func main() {
 	migrateAllTables(projectIDs)
 }
 
-func initMemSQLDB(env, dsn string, maxOpenConns int) {
+func initMemSQLDB(env string, dbConf *C.DBConf, maxOpenConns int) {
+	dbConf.UseSSL = C.IsStaging() || C.IsProduction()
+
 	var err error
-	// dsn sample admin:LpAHQyAMyI@tcp(svc-2b9e36ee-d5d0-4082-9779-2027e39fcbab-ddl.gcp-virginia-1.db.memsql.com:3306)/factors?charset=utf8mb4&parseTime=True&loc=Local
-	memSQLDB, err = gorm.Open("mysql", dsn)
+	memSQLDB, err = gorm.Open("mysql", C.GetMemSQLDSNString(dbConf))
 	if err != nil {
 		log.WithError(err).Fatal("Failed connecting to memsql.")
 	}
