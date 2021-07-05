1b6e7732bdd67738431373a72c9a1760e847ccba
vikasatfactors <85861073+vikasatfactors@users.noreply.github.com>
2021-07-05 11:19:57 +0530

    Added confirmation model for Remove Property

--- frontend-new/src/Views/Settings/ProjectSettings/PropertySettings/index.js
+++ frontend-new/src/Views/Settings/ProjectSettings/PropertySettings/index.js
@@ -1,35 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { connect } from 'react-redux';
 
-import { Text, SVG } from 'factorsComponents'; 
+import { Text, SVG } from 'factorsComponents';
 import {
     Row, Col, Button, Tabs, Table, Dropdown, Menu, notification
-  } from 'antd';
+} from 'antd';
 import { MoreOutlined } from "@ant-design/icons";
 
 import { fetchSmartProperties, deleteSmartProperty } from 'Reducers/settings/middleware';
 import SmartProperties from './SmartProperties';
 
+import ConfirmationModal from "../../../../components/ConfirmationModal";
+
 const { TabPane } = Tabs;
 
-function Properties ({activeProject, smartProperties, fetchSmartProperties, deleteSmartProperty}) {
+function Properties({ activeProject, smartProperties, fetchSmartProperties, deleteSmartProperty }) {
 
     const [selectedProperty, setSelectedProperty] = useState(null);
     const [showPropertyForm, setShowPropertyForm] = useState(false);
     const [smartPropData, setSmartPropData] = useState([]);
+    const [deleteWidgetModal, showDeleteWidgetModal] = useState(false);
+    const [deleteApiCalled, setDeleteApiCalled] = useState(false);
 
     useEffect(() => {
-        if(activeProject?.id) {
+        if (activeProject?.id) {
             fetchSmartProperties(activeProject.id);
         }
-        
+
     }, [activeProject]);
 
     useEffect(() => {
         const smrtProperties = [];
         smartProperties.forEach((prop) => {
             //harcoded type
-            smrtProperties.push({name: prop.name, type: prop.type_alias, actions: prop})
+            smrtProperties.push({ name: prop.name, type: prop.type_alias, actions: prop })
         })
         setSmartPropData(smrtProperties);
     }, [smartProperties])
@@ -37,41 +41,41 @@ function Properties ({activeProject, smartProperties, fetchSmartProperties, dele
     const columns = [
 
         {
-          title: 'Diplay name',
-          dataIndex: 'name',
-          key: 'name', 
-          render: (text) => <span className={'capitalize'}>{text}</span>
+            title: 'Diplay name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text) => <span className={'capitalize'}>{text}</span>
         },
         {
-          title: 'Type',
-          dataIndex: 'type',
-          key: 'type', 
-          render: (text) => <span className={'capitalize'}>{text.replace('_', ' ')}</span>
+            title: 'Type',
+            dataIndex: 'type',
+            key: 'type',
+            render: (text) => <span className={'capitalize'}>{text.replace('_', ' ')}</span>
         },
         {
             title: '',
             dataIndex: 'actions',
-            key: 'actions', 
+            key: 'actions',
             render: (obj) => (
                 <div className={`flex justify-end`}>
                     <Dropdown overlay={() => menu(obj)} trigger={['click']}>
-                    <Button size={'large'} type="text" icon={<MoreOutlined />} />
+                        <Button size={'large'} type="text" icon={<MoreOutlined />} />
                     </Dropdown>
                 </div>
-              )
-          }
+            )
+        }
     ];
 
     const menu = (obj) => {
         return (
-        <Menu>
-          <Menu.Item key="0" onClick={() => confirmRemove(obj.id)}>
-            <a>Remove Property</a>
-          </Menu.Item>
-          <Menu.Item key="0" onClick={() => editProp(obj)}>
-            <a>Edit Property</a>
-          </Menu.Item>
-        </Menu>
+            <Menu>
+                <Menu.Item key="0" onClick={() => showDeleteWidgetModal(obj.id)}>
+                    <a>Remove Property</a>
+                </Menu.Item>
+                <Menu.Item key="0" onClick={() => editProp(obj)}>
+                    <a>Edit Property</a>
+                </Menu.Item>
+            </Menu>
         );
     };
 
@@ -81,7 +85,7 @@ function Properties ({activeProject, smartProperties, fetchSmartProperties, dele
     }
 
     const confirmRemove = (id) => {
-        deleteSmartProperty(activeProject.id, id).then(res => {
+        return deleteSmartProperty(activeProject.id, id).then(res => {
             fetchSmartProperties(activeProject.id);
             notification.success({
                 message: "Success",
@@ -96,57 +100,79 @@ function Properties ({activeProject, smartProperties, fetchSmartProperties, dele
             })
         });
     }
-
+    const confirmDelete = useCallback(async () => {
+        try {
+            setDeleteApiCalled(true);
+            await confirmRemove(deleteWidgetModal);
+            setDeleteApiCalled(false);
+            showDeleteWidgetModal(false);
+        } catch (err) {
+            console.log(err);
+            console.log(err.response);
+        }
+    }, [
+        deleteWidgetModal
+    ]);
     const renderSmartPropertyTable = () => {
         return (
             <>
-            <Row>
-          <Col span={12}>
-            <Text type={'title'} level={3} weight={'bold'} extraClass={'m-0'}>Properties</Text>
-          </Col>
-          <Col span={12}>
-            <div className={'flex justify-end'}>
-              <Button size={'large'} onClick={() => setShowPropertyForm(true)}><SVG name={'plus'} extraClass={'mr-2'} size={16} />Add New</Button>
-            </div>
-          </Col>
-        </Row>
-        <Row className={'mt-4'}>
-            <Col span={24}>  
-            <div className={'mt-6'}>
-                <Tabs defaultActiveKey="1" >
-                            <TabPane tab="Custom Dimensions" key="1">
-                                    <Table className="fa-table--basic mt-4" 
-                                    columns={columns} 
-                                    dataSource={smartPropData} 
-                                    pagination={false}
+                <Row>
+                    <Col span={12}>
+                        <Text type={'title'} level={3} weight={'bold'} extraClass={'m-0'}>Properties</Text>
+                    </Col>
+                    <Col span={12}>
+                        <div className={'flex justify-end'}>
+                            <Button size={'large'} onClick={() => setShowPropertyForm(true)}><SVG name={'plus'} extraClass={'mr-2'} size={16} />Add New</Button>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className={'mt-4'}>
+                    <Col span={24}>
+                        <div className={'mt-6'}>
+                            <Tabs defaultActiveKey="1" >
+                                <TabPane tab="Custom Dimensions" key="1">
+                                    <Table className="fa-table--basic mt-4"
+                                        columns={columns}
+                                        dataSource={smartPropData}
+                                        pagination={false}
                                     />
-                            </TabPane>
-                </Tabs> 
-            </div>  
-        </Col> 
-        </Row>
-        </>
+                                </TabPane>
+                            </Tabs>
+                        </div>
+                    </Col>
+                </Row>
+            </>
         )
     }
 
     const renderSmartPropertyDetail = () => {
         return (
-            <SmartProperties smartProperty={selectedProperty}  setShowSmartProperty={(showVal) => {
+            <SmartProperties smartProperty={selectedProperty} setShowSmartProperty={(showVal) => {
                 setShowPropertyForm(showVal);
                 setSelectedProperty(null);
                 fetchSmartProperties(activeProject.id);
             }}></SmartProperties>
         )
     }
-    
+
     return (<div className={'mb-10 pl-4'}>
         {!showPropertyForm ? renderSmartPropertyTable() : renderSmartPropertyDetail()}
+        <ConfirmationModal
+            visible={deleteWidgetModal ? true : false}
+            confirmationText="Do you really want to remove this property?"
+            onOk={confirmDelete}
+            onCancel={showDeleteWidgetModal.bind(this, false)}
+            title="Remove Property"
+            okText="Confirm"
+            cancelText="Cancel"
+            confirmLoading={deleteApiCalled}
+        />
     </div>)
 }
 
 const mapStateToProps = (state) => ({
     activeProject: state.global.active_project,
     smartProperties: state.settings.smartProperties
-  });
+});
 
-export default connect(mapStateToProps, {fetchSmartProperties, deleteSmartProperty})(Properties);
+export default connect(mapStateToProps, { fetchSmartProperties, deleteSmartProperty })(Properties);
