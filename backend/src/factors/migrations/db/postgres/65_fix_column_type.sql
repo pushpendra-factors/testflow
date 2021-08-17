6ad23f68231711d643a71d7516ff6a62575944e6
Dineshprabu S <44255030+dineshprabu-slashbit@users.noreply.github.com>
2021-08-17 14:56:48 +0530

    Fix db_create script. (#2741)

--- backend/src/factors/migrations/db/postgres/65_fix_column_type.sql
+++
@@ -1,7 +0,0 @@
--- UP
-ALTER TABLE event_names ALTER COLUMN id TYPE bigint USING id::bigint;
-ALTER TABLE events ALTER COLUMN event_name_id TYPE bigint USING event_name_id::bigint;
-
--- DOWN
--- ALTER TABLE event_names ALTER COLUMN id TYPE text USING id::text;
--- ALTER TABLE events ALTER COLUMN event_name_id TYPE text USING event_name_id::text;
