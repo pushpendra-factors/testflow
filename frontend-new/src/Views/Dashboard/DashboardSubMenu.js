8b737e27e6279ad905b7db6e32324dadb9bda451
vikasatfactors <85861073+vikasatfactors@users.noreply.github.com>
2021-09-09 09:46:35 +0530

    Tooltip for Private and Public Dashboard

--- frontend-new/src/Views/Dashboard/DashboardSubMenu.js
+++ frontend-new/src/Views/Dashboard/DashboardSubMenu.js
@@ -21,23 +21,35 @@ function DashboardSubMenu({
 
   if (dashboard.type === 'pr') {
     btn = (
-      <Button
-        style={{ display: 'flex' }}
-        type={'text'}
-        className={'m-0 fa-button-ghost items-center p-0 py-2'}
+      <Tooltip
+        placement='bottom'
+        title={'This dashboard is only visible to you.'}
+        mouseEnterDelay={0.2}
       >
-        <LockOutlined /> Private.
-      </Button>
+        <Button
+          style={{ cursor: 'default' }}
+          type={'text'}
+          className={'m-0 fa-button-ghost items-center p-0 py-2'}
+        >
+          <LockOutlined /> Private.
+        </Button>
+      </Tooltip>
     );
   } else {
     btn = (
-      <Button
-        style={{ display: 'flex' }}
-        type={'text'}
-        className={'m-0 fa-button-ghost items-center p-0 py-2'}
+      <Tooltip
+        placement='bottom'
+        title={'This dashboard is visible to everyone.'}
+        mouseEnterDelay={0.2}
       >
-        <UnlockOutlined /> Public.
-      </Button>
+        <Button
+          style={{ cursor: 'default' }}
+          type={'text'}
+          className={'m-0 fa-button-ghost items-center p-0 py-2'}
+        >
+          <UnlockOutlined /> Public.
+        </Button>
+      </Tooltip>
     );
   }
   useEffect(() => {
