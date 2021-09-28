f67c26f2c4b2c1591b6599bad596d3ed8d5016dc
kartheekb-factors <70333266+kartheekb-factors@users.noreply.github.com>
2021-09-28 15:06:48 +0530

    Memsql 2678 (#2969)
    * Run enrich smart properties on memsql - duplicate job.
    
    * Small fix.
    
    * Adding uptime.
    
    * Adding duplicate queue support.
    
    * Fixing for memsql resource pool provisioning.

--- backend/src/factors/scripts/run_enrich_smart_properties/run_enrich_smart_properties.go
+++ backend/src/factors/scripts/run_enrich_smart_properties/run_enrich_smart_properties.go
@@ -29,6 +29,7 @@ func main() {
 	memSQLName := flag.String("memsql_name", C.MemSQLDefaultDBParams.Name, "")
 	memSQLPass := flag.String("memsql_pass", C.MemSQLDefaultDBParams.Password, "")
 	memSQLCertificate := flag.String("memsql_cert", "", "")
+	memSQLResourcePool := flag.String("memsql_resource_pool", "", "If provided, all the queries will run under the given resource pool")
 	primaryDatastore := flag.String("primary_datastore", C.DatastoreTypePostgres, "Primary datastore type as memsql or postgres")
 
 	overrideHealthcheckPingID := flag.String("healthcheck_ping_id", "", "Override default healthcheck ping id.")
@@ -61,13 +62,14 @@ func main() {
 			AppName:  appName,
 		},
 		MemSQLInfo: C.DBConf{
-			Host:        *memSQLHost,
-			Port:        *memSQLPort,
-			User:        *memSQLUser,
-			Name:        *memSQLName,
-			Password:    *memSQLPass,
-			Certificate: *memSQLCertificate,
-			AppName:     appName,
+			Host:         *memSQLHost,
+			Port:         *memSQLPort,
+			User:         *memSQLUser,
+			Name:         *memSQLName,
+			Password:     *memSQLPass,
+			Certificate:  *memSQLCertificate,
+			ResourcePool: *memSQLResourcePool,
+			AppName:      appName,
 		},
 		PrimaryDatastore:      *primaryDatastore,
 		DryRunSmartProperties: *dryRunSmartProperties,
