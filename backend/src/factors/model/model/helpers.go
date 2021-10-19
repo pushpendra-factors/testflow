0f4cfbabe38c336d639d22dff48d9a1bb22d8662
Dineshprabu S <44255030+dineshprabu-slashbit@users.noreply.github.com>
2021-10-19 12:26:40 +0530

    Reverted temporary properties_ro column used on users table. (#3105)

--- backend/src/factors/model/model/helpers.go
+++ backend/src/factors/model/model/helpers.go
@@ -2,13 +2,10 @@ package model
 
 import (
 	C "factors/config"
-	"factors/util"
-	"reflect"
 	"runtime"
 	"strings"
 	"time"
 
-	"github.com/jinzhu/gorm"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -56,32 +53,4 @@ func LogOnSlowExecutionWithParams(starttime time.Time, params *log.Fields) {
 	if timeTakenInMillsecs > 50 {
 		logCtx.Info("Slow query or method execution.")
 	}
-}
-
-func BuildSelectColumns(tableStruct interface{}, excludedColumns []string) string {
-	if len(excludedColumns) == 0 {
-		return "*"
-	}
-
-	columns := make([]string, 0, 0)
-	val := reflect.ValueOf(tableStruct)
-	for i := 0; i < val.NumField(); i++ {
-		colName := gorm.ToColumnName(val.Type().Field(i).Name)
-
-		// Override with column_name provided as tag.
-		gormTag := val.Type().Field(i).Tag.Get("gorm")
-		for _, k := range strings.Split(gormTag, ";") {
-			subTag := strings.Split(k, ":")
-			if len(subTag) == 2 && subTag[0] == "column" && subTag[1] != "" {
-				colName = subTag[1]
-			}
-		}
-
-		if util.StringValueIn(colName, excludedColumns) {
-			continue
-		}
-		columns = append(columns, colName)
-	}
-
-	return strings.Join(columns, ",")
 }
