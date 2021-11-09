ca6b67b05022140d928067a5067eacf99665fd37
Junaid Khan <junaid.khan.17@mountblue.tech>
2021-11-09 18:07:09 +0530

    add styles to tour

--- frontend-new/src/steps.js
+++ frontend-new/src/steps.js
@@ -1,7 +1,32 @@
+const style = {
+  popover: base => ({
+    ...base,
+    boxShadow: '0 0 3em rgba(0, 0, 0, 0.5)',
+    color: '#007aff',
+    borderRadius: 10,
+    top: 10,
+  }),
+  button: base => ({ ...base, transform: `scale(1.3)`}),
+  navigation: base => ({ ...base, margin: 10}),
+  maskWrapper: base => ({
+    ...base,
+    opacity: 0.5,
+  }),
+  // highlightedArea: (base, { x, y }) => ({
+  //   ...base,
+  //   x: x + 10,
+  //   y: y + 10,
+  // }),
+  // badge: base => ({ ...base, color: 'red' }),
+  close: base => ({ ...base, color: 'blue', width: 11, height: 11}),
+}
+
 const steps = [
   {
     selector: '[data-tour="step-1"]',
     content: 'Vivamus sed dui nisi',
+    position: 'right',
+    styles: style,
   },
   {
     selector: '[data-tour="step-2"]',
@@ -10,27 +35,18 @@ const steps = [
     action: node => {
       node.focus()
     },
+    styles: style,
   },
   {
     selector: '[data-tour="step-2.1"]',
     content: 'Vivamus sed dui nisi',
     position: 'center',
-    styles: {
-      maskWrapper: base => ({
-        ...base,
-        color: 'red',
-      }),
-      highlightedArea: (base, { x, y }) => ({
-        ...base,
-        x: x + 10,
-        y: y + 10,
-      }),
-      badge: base => ({ ...base, color: 'red' }),
-    },
+    styles: style,
   },
   {
     selector: '[data-tour="step-3"]',
     content: 'Vivamus sed dui nisi',
+    styles: style,
   },
   {
     selector: '[data-tour="step-4"]',
@@ -39,32 +55,40 @@ const steps = [
     action: node => {
       node.click()
     },
+    styles: style,
   },
   {
     selector: '[data-tour="step-4.1"]',
     content: 'Vivamus sed dui nisi',
     position: 'right',
+    styles: style,
   },
   {
     selector: '[data-tour="step-5"]',
     content: 'Vivamus sed dui nisi',
+    position: 'right',
     action: node => {
       node.click()
     },
+    styles: style,
   },
   {
     selector: '[data-tour="step-6"]',
     content: 'Vivamus sed dui nisi',
+    position: 'right',
     action: node => {
       node.click()
     },
+    styles: style,
   },
   {
     selector: '[data-tour="step-7"]',
     content: 'Vivamus sed dui nisi',
+    position: 'right',
     action: node => {
       node.click()
     },
+    styles: style,
   },
   {
     selector: '[data-tour="step-8"]',
@@ -72,14 +96,17 @@ const steps = [
     action: node => {
       node.click()
     },
+    styles: style,
   },
   {
     selector: '[data-tour="step-9"]',
     content: 'Vivamus sed dui nisi',
+    styles: style,
   },
     {
     selector: '[data-tour="step-9.1"]',
     content: 'Vivamus sed dui nisi',
+    styles: style,
   },
 ];
 
