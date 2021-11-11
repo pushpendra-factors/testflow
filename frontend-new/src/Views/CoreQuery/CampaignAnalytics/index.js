3aaa0109c38adc09c8a5beca1ed3f4076c9ef580
janani-somaskanda <68629545+janani-somaskanda@users.noreply.github.com>
2021-11-11 12:36:41 +0530

    campaigns hot fix (#3250)
    Co-authored-by: jk0693 <jitesh@factors.ai>

--- frontend-new/src/Views/CoreQuery/CampaignAnalytics/index.js
+++ frontend-new/src/Views/CoreQuery/CampaignAnalytics/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import NoBreakdownCharts from "./NoBreakdownCharts";
-import BreakdownCharts from "./BreakdownCharts";
+import React from 'react';
+import NoBreakdownCharts from './NoBreakdownCharts';
+import BreakdownCharts from './BreakdownCharts';
 
 function CampaignAnalytics({
   resultState,
@@ -9,28 +9,18 @@ function CampaignAnalytics({
   chartType,
   currMetricsValue,
   section,
-  durationObj
+  durationObj,
 }) {
   const { group_by: breakdown } = campaignState;
-    
 
-  const APIResponse = {"result":[{"headers":["aggregate"],"rows":[[3896]],"meta":{"query":{"cl":"","ty":"","ec":"","ewp":null,"gbp":null,"gup":null,"gbt":null,"tz":"","fr":0,"to":0,"ovp":false,"sse":0,"see":0,"agFn":"","agPr":"","agEn":""},"currency":"","metrics":null}},{"headers":["aggregate"],"rows":[[3893]],"meta":{"query":{"cl":"","ty":"","ec":"","ewp":null,"gbp":null,"gup":null,"gbt":null,"tz":"","fr":0,"to":0,"ovp":false,"sse":0,"see":0,"agFn":"","agPr":"","agEn":""},"currency":"","metrics":null}}]}
-  const DuractionObjNew = {
-  "from": "2021-10-09T18:30:00.000Z",
-  "to": "2021-10-13T18:29:59.999Z",
-  "frequency": "date",
-  "dateType": "this_week"
-} 
-  
   let content = null;
-  
+
   if (breakdown.length) {
     content = (
       <BreakdownCharts
         arrayMapper={arrayMapper}
         chartType={chartType}
-        // data={resultState.data}
-        data={APIResponse}
+        data={resultState.data}
         breakdown={breakdown}
         currentEventIndex={currMetricsValue}
         section={section}
@@ -41,9 +31,9 @@ function CampaignAnalytics({
       <NoBreakdownCharts
         arrayMapper={arrayMapper}
         chartType={chartType}
-        data={APIResponse}
+        data={resultState.data}
         section={section}
-        durationObj={DuractionObjNew}
+        durationObj={durationObj}
       />
     );
   }
