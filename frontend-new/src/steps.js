e727bb41b4a0c8b47c8f1f00c8a81b699ef17e8d
Junaid Khan <junaid.khan.17@mountblue.tech>
2021-11-12 17:51:45 +0530

    change p tag to factors text component

--- frontend-new/src/steps.js
+++ frontend-new/src/steps.js
@@ -1,5 +1,6 @@
-import { keyframes } from '@emotion/core';
 import React from 'react';
+import { keyframes } from '@emotion/core';
+import { Text } from 'factorsComponents';
 
 const keyframesRotate = keyframes`
   50% {
@@ -11,10 +12,9 @@ const style = {
   popover: base => ({
     ...base,
     boxShadow: '0 0 3em rgba(0, 0, 0, 0.5)',
-    color: '#007aff',
     borderRadius: 10,
-    'p' : {
-      fontSize: 15,
+    '.fai-text' : {
+      color: '#007aff',
     }
   }),
   button: base => ({ 
@@ -44,7 +44,6 @@ const style = {
     background: 'var(--reactour-accent,#007aff)',
     height: '2.1em',
     width: '2.6em',
-    lineHeight: 2,
     paddingLeft: '0.8125em',
     paddingRight: '0.8125em',
     borderRadius: '1.625em',
@@ -73,14 +72,14 @@ const steps = [
     selector: '[data-tour="step-1"]',
     content: 
       <div>
-        <p><strong>Let’s start — here is your dashboard.</strong></p>
-        <p>All previously built and saved reports can be added to this view for quick and easy access.</p>
+        <Text type={'title'} level={6} weight={'bold'}>Let’s start — here is your dashboard.</Text>
+        <Text type={'title'} level={7}>All previously built and saved reports can be added to this view for quick and easy access.</Text>
       </div>,
     styles: style,
   },
   {
     selector: '[data-tour="step-2"]',
-    content: <p>The search bar on top gives you easy access to recently saved reports from anywhere.</p>,
+    content: <Text type={'title'} level={7}>The search bar on top gives you easy access to recently saved reports from anywhere.</Text>,
     action: node => {
       node.focus()
     },
@@ -88,20 +87,20 @@ const steps = [
   },
   {
     selector: '[data-tour="step-3"]',
-    content: <p>You can also run new queries from here, if you’re in a rush!</p>,
+    content: <Text type={'title'} level={7}>You can also run new queries from here, if you’re in a rush!</Text>,
     styles: style,
   },
   {
     selector: '[data-tour="step-4"]',
-    content: <p>You can add new dashboard views, to represent different categories of reports you need to look at</p>,
+    content: <Text type={'title'} level={7}>You can add new dashboard views, to represent different categories of reports you need to look at</Text>,
     styles: style,
   },
   {
     selector: '[data-tour="step-5"]',
     content: 
       <div>
-        <p><strong>The heart of your work lies here — the Analyse engine.</strong></p>
-        <p>Here you can run deep analyses and charts for events, funnels, and campaigns, as well as model attribution analyses across all your marketing touchpoints</p>
+        <Text type={'title'} level={6} weight={'bold'}>The heart of your work lies here — the Analyse engine.</Text>
+        <Text type={'title'} level={7}>Here you can run deep analyses and charts for events, funnels, and campaigns, as well as model attribution analyses across all your marketing touchpoints</Text>
       </div>,
     action: node => {
       node.click()
@@ -112,9 +111,9 @@ const steps = [
     selector: '[data-tour="step-6"]',
     content: 
       <div>
-        <p><strong>Ah, the Explain engine!</strong></p>
-        <p>Here, we periodically track conversion goals and journeys you define, to help you understand what factors are impacting them the most.</p>
-        <p>You’ll have periodic and actionable insights sent to you.</p>
+        <Text type={'title'} level={6} weight={'bold'}>Ah, the Explain engine!</Text>
+        <Text type={'title'} level={7}>Here, we periodically track conversion goals and journeys you define, to help you understand what factors are impacting them the most.</Text>
+        <Text type={'title'} level={7}>You’ll have periodic and actionable insights sent to you.</Text>
       </div>,
     action: node => {
       node.click()
@@ -125,8 +124,8 @@ const steps = [
     selector: '[data-tour="step-7"]',
     content: 
       <div>
-        <p>Set up your custom events and properties,</p>
-        <p>as well as configure your UTM parameters to match the standards used across the platform.</p>
+        <Text type={'title'} level={6} weight={'bold'}>Set up your custom events and properties,</Text>
+        <Text type={'title'} level={7}>as well as configure your UTM parameters to match the standards used across the platform.</Text>
       </div>,
     action: node => {
       node.click()
@@ -135,7 +134,7 @@ const steps = [
   },
   {
     selector: '[data-tour="step-8"]',
-    content: <p>All projects you have access to appear here.</p>,
+    content: <Text type={'title'} level={7}>All projects you have access to appear here.</Text>,
     action: node => {
       node.click()
     },
@@ -143,13 +142,13 @@ const steps = [
   },
   {
     selector: '[data-tour="step-9"]',
-    content: <p>You can also view and edit account settings form this panel</p>,
+    content: <Text type={'title'} level={7}>You can also view and edit account settings form this panel</Text>,
     position: 'right',
     styles: style,
   },
   {
     selector: '[data-tour="step-10"]',
-    content: <p>Connect to all your data sources here!</p>,
+    content: <Text type={'title'} level={7}>Connect to all your data sources here!</Text>,
     action: node => {
       node.click()
     },
@@ -159,8 +158,8 @@ const steps = [
     selector: '[data-tour="step-11"]',
     content: 
       <div>
-        <p><strong>For each integration you need,</strong></p>
-        <p>use the primary blue button for an easy SSO (Single Sign-On) process, or view documentation from the adjacent button.</p>
+        <Text type={'title'} level={6} weight={'bold'}>For each integration you need,</Text>
+        <Text type={'title'} level={7}>use the primary blue button for an easy SSO (Single Sign-On) process, or view documentation from the adjacent button.</Text>
       </div>,
     position: 'right',
     styles: style,
