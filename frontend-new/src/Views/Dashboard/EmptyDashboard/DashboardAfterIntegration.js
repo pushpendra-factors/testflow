8d34ea1020e79f6fd1abe3986b977bd7dfce3054
Junaid Khan <junaid.khan.17@mountblue.tech>
2021-12-08 13:41:02 +0530

    dashboard after integration

--- frontend-new/src/Views/Dashboard/EmptyDashboard/DashboardAfterIntegration.js
+++ frontend-new/src/Views/Dashboard/EmptyDashboard/DashboardAfterIntegration.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-// import NoDataChart from '../../../components/NoDataChart';
 import { SVG, Text } from '../../../components/factorsComponents';
 import { FaErrorComp, FaErrorLog } from '../../../components/factorsComponents';
 import { ErrorBoundary } from 'react-error-boundary';
+import { Button } from 'antd';
+import Header from '../../AppLayout/Header';
+import AddDashboard from '../AddDashboard';
 
 function DashboardAfterIntegration() {
+    // const [addDashboardModal, setaddDashboardModal] = useState(false);
+    const [dataLoading, setdataLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setdataLoading(false)
+        },3000);
+    }, []);
+
+    // const addDashboard = () => {
+    //     setaddDashboardModal(true);
+    // }
 
     return (
         <>
@@ -21,8 +35,27 @@ function DashboardAfterIntegration() {
                 }
                 onError={FaErrorLog}
             >
+                <Header>
+                    <div className={'rounded-lg border-2 border-gray-200 w-full h-24 mt-8'}>
+                            <div className='w-20 float-left mt-2 ml-4 mr-4 mb-1'>
+                                <img src='assets/images/NoData.png'/>
+                            </div>
+                            <div className={'mt-4 mb-4'}>
+                                <Text type={'title'} level={4} color={'grey-2'} weight={'bold'} extraClass={'m-0 mt-2 mb-1'}>
+                                    Complete project setup
+                                </Text>
+                                <Text type={'title'} level={7} color={'grey'} extraClass={'m-0 mb-1'}>
+                                    A few things pending under your project setup
+                                </Text>
+                            </div>
+                            <div className={'float-right -mt-20 pt-2 mr-8'}>
+                                <Link to='/project-setup' className={'text-base font-semibold'}>Setup Assist<span style={{fontSize:'30px'}}>→</span> </Link>
+                            </div>
+                    </div>
+                </Header>
+
                 <div
-                    style={{marginTop:'20em'}}
+                    style={{marginTop:'24em'}}
                     className={
                     'flex justify-center flex-col items-center fa-dashboard--no-data-container'
                     }
@@ -32,10 +65,10 @@ function DashboardAfterIntegration() {
                         Create a dashboard to moniter your metrics in one place.
                     </Text>
                     <Text type={'title'} level={7} color={'grey'} weight={'bold'} extraClass={'m-0'}>
-                        Learn  <Link to='/project-setup' className={'text-sm font-semibold'}>Dashboard Basics </Link>
+                        Learn  <Link to='#' className={'text-sm font-semibold'}>Dashboard Basics {dataLoading? <span style={{fontSize:'20px'}}>→</span>:null} </Link>
                     </Text>
-
-                    <div className={'rounded-lg border-2 border-gray-400 w-11/12 mt-4'}>
+                    { dataLoading ? 
+                    <div className={'rounded-lg border-2 border-gray-400 w-11/12 mt-6'}>
                         <Text type={'title'} level={6} color={'grey'} extraClass={'m-0 mt-2 -mb-4'}>
                            We don’t have any data yet. While we fetching your metrics,
                         </Text>
@@ -43,7 +76,22 @@ function DashboardAfterIntegration() {
                            Explore our Demo Project <span style={{fontSize:'30px'}}>→</span>
                         </Text>
                     </div>
+                    :
+                    <div className={'mt-6'}>
+                        <Button type={'primary'} size={'large'} className={'w-full'}>Create your first dashboard</Button>
+                        <Text type={'title'} level={7} weight={'bold'} color={'grey'} extraClass={'m-0 mt-2 mb-2'}>
+                            or
+                        </Text>
+                        <Button type={'default'} size={'large'} className={'w-full'}>Choose from templates</Button>
+                    </div>
+                    }
                 </div>
+
+                {/* Add dashboard modal */}
+                {/* <AddDashboard
+                    addDashboardModal={addDashboardModal}
+                    setaddDashboardModal={setaddDashboardModal}
+                /> */}
                 
             </ErrorBoundary>
         </>
