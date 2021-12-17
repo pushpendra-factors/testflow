13b8ca6f4733036d70a6cc81a3b3e884e49e7e80
Aashish <87294809+Taashish@users.noreply.github.com>
2021-12-17 15:16:34 +0530

    Added query to inculde Source column in Users table. (#3580)
    * Added query to inculde Source column in Users table.
    
    * Moved alter table query in a separate file.
    
    * Added the query inside Users table schema as well.
    
    * Added column inside create users table query itself.

--- backend/src/factors/migrations/db/memsql/1_create_schema.sql
+++ backend/src/factors/migrations/db/memsql/1_create_schema.sql
@@ -59,6 +59,7 @@ CREATE TABLE IF NOT EXISTS users (
     group_4_user_id text,
     created_at timestamp(6) NOT NULL,
     updated_at timestamp(6) NOT NULL,
+    source int,
     -- COLUMNSTORE key is sort key, can we add an incremental numerical column to the end?
     -- Initial parts of the indices are still useful when don't use the last column which is an incremental value.
     KEY (project_id, id) USING CLUSTERED COLUMNSTORE,
