8e07f67d7325bfa23b5287865b1e52376d7ac010
Dineshprabu S <44255030+dineshprabu-slashbit@users.noreply.github.com>
2022-01-04 12:00:28 +0530

    Remove resource_pool configuration and move to user level pool (#3273)

--- backend/src/factors/scripts/run_enrich_smart_properties/run_enrich_smart_properties.go
+++ backend/src/factors/scripts/run_enrich_smart_properties/run_enrich_smart_properties.go
@@ -29,7 +29,6 @@ func main() {
 	memSQLName := flag.String("memsql_name", C.MemSQLDefaultDBParams.Name, "")
 	memSQLPass := flag.String("memsql_pass", C.MemSQLDefaultDBParams.Password, "")
 	memSQLCertificate := flag.String("memsql_cert", "", "")
-	memSQLResourcePool := flag.String("memsql_resource_pool", "", "If provided, all the queries will run under the given resource pool")
 	primaryDatastore := flag.String("primary_datastore", C.DatastoreTypePostgres, "Primary datastore type as memsql or postgres")
 
 	overrideHealthcheckPingID := flag.String("healthcheck_ping_id", "", "Override default healthcheck ping id.")
@@ -62,14 +61,13 @@ func main() {
 			AppName:  appName,
 		},
 		MemSQLInfo: C.DBConf{
-			Host:         *memSQLHost,
-			Port:         *memSQLPort,
-			User:         *memSQLUser,
-			Name:         *memSQLName,
-			Password:     *memSQLPass,
-			Certificate:  *memSQLCertificate,
-			ResourcePool: *memSQLResourcePool,
-			AppName:      appName,
+			Host:        *memSQLHost,
+			Port:        *memSQLPort,
+			User:        *memSQLUser,
+			Name:        *memSQLName,
+			Password:    *memSQLPass,
+			Certificate: *memSQLCertificate,
+			AppName:     appName,
 		},
 		PrimaryDatastore:      *primaryDatastore,
 		DryRunSmartProperties: *dryRunSmartProperties,
