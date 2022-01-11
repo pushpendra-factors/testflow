8ccad47446f1f9e0079844812bd4530aac262d9f
Aashish <87294809+Taashish@users.noreply.github.com>
2022-01-11 11:29:53 +0530

    Hubspot API for onboarding. (#3714)
    * Hubspot API for onboarding.
    
    * Code review changes.
    
    * Code review changes.
    
    * Added email validation, changed response value, and other code review changes.
    
    * Added flag protection.
    
    * Minor code changes.
    
    * bug fixes
    
    Co-authored-by: Maisa <maisa@factors.ai>

--- backend/src/factors/app/app.go
+++ backend/src/factors/app/app.go
@@ -126,6 +126,7 @@ func main() {
 	allowSupportForSourceColumnInUsers := flag.String("allow_support_for_source_column_in_users", "", "")
 	useOLAPPoolForAnalytics := flag.Bool("use_olap_pool_for_analytics", false,
 		"Will use the pool_olap if set, else use user-level default pool.")
+	hubspotAPIOnboardingHAPIKey := flag.String("hubspot_API_onboarding_HAPI_key", "4ae4fe41-1921-4fb7-a7e5-67d0f492dc0c", "")
 
 	flag.Parse()
 
@@ -211,6 +212,7 @@ func main() {
 		EnableEventLevelEventProperties:         *enableEventLevelEventProperties,
 		AllowSupportForSourceColumnInUsers:      *allowSupportForSourceColumnInUsers,
 		UseOLAPPoolForAnalytics:                 *useOLAPPoolForAnalytics,
+		HubspotAPIOnboardingHAPIKey:             *hubspotAPIOnboardingHAPIKey,
 	}
 	C.InitConf(config)
 
--- backend/src/factors/handler/routes.go
+++ backend/src/factors/handler/routes.go
@@ -65,6 +65,8 @@ func InitAppRoutes(r *gin.Engine) {
 	r.GET(routePrefix+"/IsDependentTaskDone", mid.SetLoggedInAgentInternalOnly(), responseWrapper(V1.IsDependentTaskDoneHandler))
 	r.POST(routePrefix+"/InsertTaskBeginRecord", mid.SetLoggedInAgentInternalOnly(), responseWrapper(V1.InsertTaskBeginRecordHandler))
 	r.POST(routePrefix+"/InsertTaskEndRecord", mid.SetLoggedInAgentInternalOnly(), responseWrapper(V1.InsertTaskEndRecordHandler))
+	r.POST("/hubspot/createcontact", V1.HubspotCreateContact)
+	r.GET("/hubspot/getcontact", V1.GetHubspotContactByEmail)
 	// Auth route group with authentication an authorization middleware.
 	authRouteGroup := r.Group(routePrefix + ROUTE_PROJECTS_ROOT)
 	authRouteGroup.Use(mid.SetLoggedInAgent())
