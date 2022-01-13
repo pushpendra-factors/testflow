547966b3674844d04c2065781afbee2910a527bd
Dineshprabu S <44255030+dineshprabu-slashbit@users.noreply.github.com>
2022-01-13 18:21:20 +0530

    Hot Fix - Explicit emoji cleanup on Overwrite User Properties on MemSQL #3792

--- backend/src/factors/tests/util_database_test.go
+++ backend/src/factors/tests/util_database_test.go
@@ -18,3 +18,11 @@ func TestIsPostgresIntegrityViolationError(t *testing.T) {
 	assert.True(t, U.IsPostgresIntegrityViolationError(
 		errors.New("pq: duplicate key value violates unique constraint \"column_unique_index\"")))
 }
+
+func TestCleanupPostgresJsonStringBytes(t *testing.T) {
+	assert.Equal(
+		t,
+		string(U.CleanupUnsupportedCharOnStringBytes([]byte("🌎💧🍃🌾🏭🔬🚽🚿🇯🇵  Environmental Bioengineering Lab. led by Akihiko Terada and Shohei Riya at Dep. Chem. Eng. in Tokyo Univ. Agri. & Tech., "))),
+		"  Environmental Bioengineering Lab. led by Akihiko Terada and Shohei Riya at Dep. Chem. Eng. in Tokyo Univ. Agri. & Tech., ",
+	)
+}
