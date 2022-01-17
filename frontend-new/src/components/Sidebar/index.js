f3d484842e167cdcd1be597f787dbb1e7ec1c9c4
Junaid Khan <junaid@factors.ai>
2022-01-17 18:17:44 +0530

    Minor Fix

--- frontend-new/src/components/Sidebar/index.js
+++ frontend-new/src/components/Sidebar/index.js
@@ -38,7 +38,7 @@ function Sidebar(props) {
 
   useEffect(() => {
     let agent = props.agents?.filter(agent => agent.email === props.currentAgent.email);
-    if(!agent && !agent[0]?.invited_by) {
+    if(!agent || !agent[0]?.invited_by) {
       if (props.currentAgent.is_onboarding_flow_seen) {
           setShowProjectModal(false);
       } else {
