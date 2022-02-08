e3fd675f52350fc220bec45b472b3184f9d85adf
kartheek-factors <94624403+kartheek-factors@users.noreply.github.com>
2022-02-08 16:55:08 +0530

    Removing order of groupbys, adding google_ads and changing kpi profiles (#4041)
    * Removng order of groupbys, adding google_ads
    
    * Changing kpi profiles to support.

--- backend/src/factors/model/store/postgres/linkedin_document.go
+++ backend/src/factors/model/store/postgres/linkedin_document.go
@@ -638,32 +638,9 @@ func getLinkedinSpecificFilters(requestFilters []model.ChannelFilterV1) ([]model
 
 // @Kark TODO v1
 func getLinkedinSpecificGroupBy(requestGroupBys []model.ChannelGroupBy) ([]model.ChannelGroupBy, error) {
-	sortedGroupBys := make([]model.ChannelGroupBy, 0)
-	for _, groupBy := range requestGroupBys {
-		if groupBy.Object == CAFilterCampaign {
-			sortedGroupBys = append(sortedGroupBys, groupBy)
-		}
-	}
 
-	for _, groupBy := range requestGroupBys {
-		if groupBy.Object == CAFilterAdGroup {
-			sortedGroupBys = append(sortedGroupBys, groupBy)
-		}
-	}
-
-	for _, groupBy := range requestGroupBys {
-		if groupBy.Object == CAFilterAd {
-			sortedGroupBys = append(sortedGroupBys, groupBy)
-		}
-	}
-
-	for _, groupBy := range requestGroupBys {
-		if groupBy.Object == CAFilterChannel {
-			sortedGroupBys = append(sortedGroupBys, groupBy)
-		}
-	}
 	resultGroupBys := make([]model.ChannelGroupBy, 0, 0)
-	for _, requestGroupBy := range sortedGroupBys {
+	for _, requestGroupBy := range requestGroupBys {
 		var resultGroupBy model.ChannelGroupBy
 		groupByObject, isPresent := model.LinkedinExternalRepresentationToInternalRepresentation[requestGroupBy.Object]
 		if !isPresent {
