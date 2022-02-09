6a9e5c08c27e1e8bdb7d05437d92b4d0e27bccee
Vishnu Baliga <68986923+baliga-factors@users.noreply.github.com>
2022-02-09 10:53:34 +0530

    handling start case in kpi dropdowns (#4051)
    Co-authored-by: Vishnu Baliga <baliga-factors@users.noreply.github.com>

--- frontend-new/src/components/KPIComposer/GroupSelect2/index.js
+++ frontend-new/src/components/KPIComposer/GroupSelect2/index.js
@@ -9,7 +9,8 @@ function GroupSelect2({
     groupedProperties, placeholder,
     optionClick, onClickOutside, extraClass,
     allowEmpty = false,
-    hideTitle=false
+    hideTitle=false,
+    textStartCase=false,
 }) {
     const [groupCollapseState, setGroupCollapseState] = useState({});
     const [searchTerm, setSearchTerm] = useState('');
@@ -81,7 +82,7 @@ function GroupSelect2({
                                             <div key={i} title={val[0]} className={`fa-select-group-select--options`}
                                                 onClick={() => optionClick(group.label, val, group?.category)} >
                                                 {searchTerm.length > 0}
-                                                <Text level={7} type={'title'} extraClass={'m-0'} weight={'thin'}>{val[0]}</Text>
+                                                <Text level={7} type={'title'} extraClass={'m-0'} weight={'thin'}>{textStartCase ? _.startCase(val[0]) : val[0]}</Text>
                                             </div>
                                         );
                                     }
