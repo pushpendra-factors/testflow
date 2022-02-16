02f028f9c1815ad8706812624168847b591f67d3
janani-somaskanda <68629545+janani-somaskanda@users.noreply.github.com>
2022-02-16 10:11:12 +0530

    secret configs for bingads job (#4131)
    * adding bingads secret configs
    
    * adding the migration scripts to schema file

--- backend/src/factors/migrations/db/memsql/1_create_schema.sql
+++ backend/src/factors/migrations/db/memsql/1_create_schema.sql
@@ -828,4 +828,34 @@ CREATE TABLE leadgen_settings (
 -- DOWN
 -- DROP TABLE IF EXISTS leadgen_settings;
 
+CREATE ROWSTORE TABLE IF NOT EXISTS fivetran_mappings(
+    project_id bigint NOT NULL,
+    id text NOT NULL,
+    integration text NOT NULL,
+    connector_id text NOT NULL,
+    schema_id text NOT NULL,
+    status boolean,
+    created_at timestamp(6) NOT NULL,
+    updated_at timestamp(6) NOT NULL,
+    KEY (updated_at),
+    SHARD KEY (project_id),
+    PRIMARY KEY (project_id, id)
+);
+
+CREATE TABLE IF NOT EXISTS integration_documents (
+    document_id text,
+    project_id bigint,
+    customer_account_id text,
+    document_type int,
+    timestamp bigint,
+    source text,
+    value JSON COLLATE utf8_bin OPTION 'SeekableLZ4',
+    created_at timestamp(6) NOT NULL,
+    updated_at timestamp(6) NOT NULL,
+    SHARD KEY (project_id, document_id),
+    KEY (updated_at) USING HASH,
+    KEY (project_id, customer_account_id, document_id, document_type, source, timestamp)  USING CLUSTERED COLUMNSTORE
+);
+
+
 -- DROP DATABASE factors;
