84ee33a997154c82af6dc7a3731f2d6a0a7a825e
Jitesh Kriplani <68528023+jk0693@users.noreply.github.com>
2022-02-28 14:14:13 +0530

    import fix (#4216)
    * import fix
    
    * import fix

--- frontend-new/src/Views/CoreQuery/AnalysisResultsPage/ReportsLayout.js
+++ frontend-new/src/Views/CoreQuery/AnalysisResultsPage/ReportsLayout.js
@@ -289,6 +289,7 @@ function ReportsLayout({
                   campaignState={campaignState}
                   savedQueryId={savedQueryId}
                   handleChartTypeChange={handleChartTypeChange}
+                  queryOptions={queryOptions}
                   {...rest}
                 />
               )}
