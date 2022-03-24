513eb162b268662d78a0fbf403fa7dec3e824694
Junaid Khan <92861240+junaidfactors@users.noreply.github.com>
2022-03-24 11:18:20 +0530

    Email validation fix (#4382)

--- frontend-new/src/Views/Pages/SignUp.js
+++ frontend-new/src/Views/Pages/SignUp.js
@@ -278,7 +278,7 @@ function SignUp({ signup, createHubspotContact, getHubspotContact }) {
                                                     },
                                                     ({ getFieldValue }) => ({
                                                         validator(rule, value) { 
-                                                          if (!value || value.match(/^([\w-\.]+@(?!gmail)(?!yahoo)(?!hotmail)(?!hey)(?!aol)(?!abc)(?!xyz)(?!pqr)(?!rediffmail)(?!live)(?!outlook)(?!me)(?!msn)(?!ymail)([\w-]+\.)+[\w-]{2,})?$/)) {
+                                                          if (!value || value.match(/^([\w-\.]+@(?!gmail.com)(?!yahoo.com)(?!hotmail.com)(?!yahoo.co.in)(?!hey.com)(?!icloud.com)(?!me.com)(?!mac.com)(?!aol.com)(?!abc.com)(?!xyz.com)(?!pqr.com)(?!rediffmail.com)(?!live.com)(?!outlook.com)(?!msn.com)(?!ymail.com)([\w-]+\.)+[\w-]{2,})?$/)) {
                                                             return Promise.resolve();
                                                           }
                                                           return Promise.reject(new Error('Please enter your business email address.'));
