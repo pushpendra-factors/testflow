801dc1731c8429deb506314aa6c29a0ef8f24d96
sathviktumoju07 <97498570+sathviktumoju07@users.noreply.github.com>
2022-04-11 11:27:33 +0530

    fixes (#4593)

--- backend/src/factors/scripts/run_project_events/run_project_events.go
+++ backend/src/factors/scripts/run_project_events/run_project_events.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	C "factors/config"
 	"factors/filestore"
+	"factors/model/store"
 	P "factors/pattern"
 	serviceDisk "factors/services/disk"
 	serviceGCS "factors/services/gcstorage"
@@ -11,6 +13,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"reflect"
 	"sort"
@@ -39,26 +42,215 @@ type validationRule struct {
 
 var validations = make(map[string]map[string]validationRule)
 
-func openFile(fileName string) (*os.File, error) {
-	file, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal("failed to open file : ", fileName, err)
+func main() {
+
+	bucketName := flag.String("bucket_name", "/usr/local/var/factors/cloud_storage", "")
+	envFlag := flag.String("env", "development", "environment")
+	var cloudManager filestore.FileManager
+
+	//Taking flag inputs
+	date := time.Now().AddDate(0, 0, -7)
+	for date.Weekday() != time.Sunday {
+		date = date.AddDate(0, 0, -1)
 	}
-	return file, err
-}
-func closeFile(fileName *os.File) error {
-	err := fileName.Close()
-	if err != nil {
-		log.Fatal("failed to close file : ", fileName, err)
+	fromDate := flag.String("date", date.Format(U.DATETIME_FORMAT_YYYYMMDD), "start date of events")
+	projectIdFlag := flag.String("project_ids", "", "Optional: Project Id. A comma separated list of project Ids and supports '*' for all projects. ex: 1,2,6,9")
+
+	modelType := flag.String("modelType", "w", "Model Type of events")
+	flag.Parse()
+
+	allProjects, projectIdsToRun, _ := C.GetProjectsFromListWithAllProjectSupport(*projectIdFlag, "")
+	if allProjects {
+		projectIDs, errCode := store.GetStore().GetAllProjectIDs()
+		if errCode != http.StatusFound {
+			log.Fatal("Failed to get all projects and project_ids set to '*'.")
+		}
+
+		projectIdsToRun = make(map[uint64]bool, 0)
+		for _, projectID := range projectIDs {
+			projectIdsToRun[projectID] = true
+		}
 	}
-	return err
-}
-func createFile(fileName string) (*os.File, error) {
-	f, err := os.Create(fileName)
-	if err != nil {
-		log.Fatal("Failed to create OS file : ", fileName, err)
+
+	projectIdsArray := make([]uint64, 0)
+	for projectId := range projectIdsToRun {
+		projectIdsArray = append(projectIdsArray, projectId)
 	}
-	return f, err
+	if *envFlag == "development" {
+		cloudManager = serviceDisk.New(*bucketName)
+	} else {
+		var err error
+		cloudManager, err = serviceGCS.New(*bucketName)
+		if err != nil {
+			log.Error("Failed to init New GCS Client")
+			panic(err)
+		}
+	}
+
+	fromTime, err := time.Parse(U.DATETIME_FORMAT_YYYYMMDD, *fromDate)
+	if err != nil {
+		log.Fatal("Failed in parsing date. Error = ", err)
+	}
+	if fromTime.Format(U.DATETIME_FORMAT_YYYYMMDD) > time.Now().AddDate(0, 0, -7).Format(U.DATETIME_FORMAT_YYYYMMDD) {
+		log.Fatal("Could not run for given date range.")
+	} else {
+		for fromTime.Weekday() != time.Sunday {
+			fromTime = fromTime.AddDate(0, 0, -1)
+		}
+		*fromDate = fromTime.Format(U.DATETIME_FORMAT_YYYYMMDD)
+	}
+	toDate := fromTime.AddDate(0, 0, 6).Format(U.DATETIME_FORMAT_YYYYMMDD)
+
+	//fileDir and fileName
+	startTimeStamp := fromTime.Unix()
+	for _, project_id := range projectIdsArray {
+
+		fileDir, fileName := (cloudManager).GetModelEventsFilePathAndName(uint64(project_id), startTimeStamp, *modelType)
+		reader, err := (cloudManager).Get(fileDir, fileName)
+		if err != nil {
+			log.Fatal("Failed to get data from file path. error = ", err)
+		}
+		dataFile, _ := createFile(fileName)
+		//downloading file
+		if _, err := io.Copy(dataFile, reader); err != nil {
+			log.Fatal("Failed to download data at io.Copy. error = ", err)
+		}
+		_ = closeFile(dataFile)
+		log.Info("data download sucessful...")
+		//initizalizing validator and validationMap
+		file, _ := openFile(fileName)
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanLines)
+
+		validate := validator.New()
+		initialize()
+		var reportMap = make(map[string]map[string]bool)
+		var daterange = make(map[string]map[string]bool)
+		var dates = make([]string, 0, 10)
+		All_Events := []string{Session, Hubspot_created, Hubspot_updated, Sf_created, Sf_updated}
+		//Scanning the file row by row
+		for scanner.Scan() {
+			row := []byte(scanner.Text())
+			var data P.CounterEventFormat
+			json.Unmarshal(row, &data)
+			time := time.Unix(data.EventTimestamp, 0)
+			weekday := time.Weekday().String()[0:3]
+			dateKey := time.AddDate(0, 0, 0).Format(U.DATETIME_FORMAT_YYYYMMDD)
+			if dateKey > toDate {
+				continue
+			}
+			mapKey := fmt.Sprintf("Date:%s, Day:%s ", dateKey, weekday)
+			if daterange[mapKey] == nil {
+				daterange[mapKey] = make(map[string]bool)
+				for _, event := range All_Events {
+					daterange[mapKey][event] = false
+				}
+				dates = append(dates, mapKey)
+			}
+			eventName := data.EventName
+			if validations[eventName] == nil {
+				eventName = "NoEvent"
+			} else {
+				daterange[mapKey][eventName] = true
+			}
+			r := reflect.ValueOf(data)
+			for key, value := range validations[eventName] {
+				if reportMap[key] == nil {
+					reportMap[key] = make(map[string]bool)
+				}
+				var f reflect.Value
+				if value.property == "" {
+					f = reflect.Indirect(r).FieldByName(value.feild)
+				} else if value.property == "UserProperties" || value.property == "EventProperties" {
+					f = reflect.Indirect(r).FieldByName(value.property).MapIndex(reflect.ValueOf(value.feild))
+				} else {
+					log.Info("invalid property")
+					continue
+				}
+				if (!f.IsValid() && value.required) || (f.IsValid() && validate.Var(f.Interface(), value.validate) != nil) {
+					reportMap[key][data.UserId] = true
+				}
+			}
+		}
+		//end of data file
+
+		// Creating Reports
+		var report, detailed_report *os.File
+		report, _ = createFile(ReportName)
+		detailed_report, _ = createFile(DetailedReportName)
+		report.WriteString(fmt.Sprintf("Report from %s to %s.\n\n", *fromDate, toDate))
+		detailed_report.WriteString(fmt.Sprintf("Report from %s to %s.\n\n", *fromDate, toDate))
+
+		for key, value := range reportMap {
+			report.WriteString(fmt.Sprintf("Total invalid %s : %s \n", key, strconv.Itoa(int(len(value)))))
+			if len(value) != 0 {
+				detailed_report.WriteString("Invalid " + key + ":\n")
+				for uid := range value {
+					detailed_report.WriteString(uid + "\n")
+				}
+				detailed_report.WriteString("\n")
+			}
+		}
+		sort.Strings(dates)
+		report.WriteString("\nEvents for Date Ranges :\n")
+		for _, key := range dates {
+			report.WriteString(key + " : ")
+			value := daterange[key]
+			s := ""
+			flag := false
+			for event, present := range value {
+				if present {
+					flag = true
+					s += event
+					s += ", "
+				}
+			}
+			if !flag {
+				report.WriteString("None\n")
+				continue
+			}
+			report.WriteString(s[0:len(s)-2] + "\n")
+		}
+		report.WriteString("\nMissing events for Date Ranges :\n")
+		for _, key := range dates {
+			report.WriteString(key + " : ")
+			value := daterange[key]
+			s := ""
+			flag := false
+			for event, present := range value {
+				if !present {
+					flag = true
+					s += event
+					s += ", "
+				}
+			}
+			if !flag {
+				report.WriteString("None\n")
+				continue
+			}
+			report.WriteString(s[0:len(s)-2] + "\n")
+		}
+
+		//uploading reports to cloud
+		_ = closeFile(report)
+		_ = closeFile(detailed_report)
+		report, _ = openFile(ReportName)
+		detailed_report, _ = openFile(DetailedReportName)
+		err = (cloudManager).Create(fileDir, ReportName, report)
+		if err != nil {
+			log.Fatal("Failed to upload report in cloud. error = ", err)
+		}
+		err = (cloudManager).Create(fileDir, DetailedReportName, detailed_report)
+		if err != nil {
+			log.Fatal("Failed to upload detailed_report in cloud. error = ", err)
+		}
+		//reports uploaded
+		//closing reports
+		_ = closeFile(report)
+		_ = closeFile(detailed_report)
+		log.Info(fmt.Sprintf("Report written successfully from %s to %s.", *fromDate, toDate))
+		//sucess log
+	}
 }
 
 func initialize() {
@@ -123,191 +315,25 @@ func initializeSalesforce_Updated() {
 	validations[Sf_updated]["EventTimestamp"] = validationRule{"EventTimestamp", "", true, "required,gt=0"}
 
 }
-func main() {
 
-	bucketName := flag.String("bucket_name", "/usr/local/var/factors/cloud_storage", "")
-	envFlag := flag.String("env", "development", "environment")
-	var cloudManager filestore.FileManager
-
-	//Taking flag inputs
-	date := time.Now().AddDate(0, 0, -7)
-	for date.Weekday() != time.Sunday {
-		date = date.AddDate(0, 0, -1)
-	}
-	fromDate := flag.String("date", date.Format(U.DATETIME_FORMAT_YYYYMMDD), "start date of events")
-	projectId := flag.Int("projectId", 0, "Project Id of events")
-	modelType := flag.String("modelType", "w", "Model Type of events")
-	flag.Parse()
-
-	if *envFlag == "development" {
-		cloudManager = serviceDisk.New(*bucketName)
-	} else {
-		var err error
-		cloudManager, err = serviceGCS.New(*bucketName)
-		if err != nil {
-			log.Error("Failed to init New GCS Client")
-			panic(err)
-		}
-	}
-	fromTime, err := time.Parse(U.DATETIME_FORMAT_YYYYMMDD, *fromDate)
+func openFile(fileName string) (*os.File, error) {
+	file, err := os.Open(fileName)
 	if err != nil {
-		log.Fatal("Failed in parsing date. Error = ", err)
+		log.Fatal("failed to open file : ", fileName, err)
 	}
-	if fromTime.Format(U.DATETIME_FORMAT_YYYYMMDD) > time.Now().AddDate(0, 0, -7).Format(U.DATETIME_FORMAT_YYYYMMDD) {
-		log.Fatal("Could not run for given date range.")
-	} else {
-		for fromTime.Weekday() != time.Sunday {
-			fromTime = fromTime.AddDate(0, 0, -1)
-		}
-		*fromDate = fromTime.Format(U.DATETIME_FORMAT_YYYYMMDD)
-	}
-	toDate := fromTime.AddDate(0, 0, 6).Format(U.DATETIME_FORMAT_YYYYMMDD)
-
-	//fileDir and fileName
-	startTimeStamp := fromTime.Unix()
-	fileDir, fileName := (cloudManager).GetModelEventsFilePathAndName(uint64(*projectId), startTimeStamp, *modelType)
-	reader, err := (cloudManager).Get(fileDir, fileName)
+	return file, err
+}
+func closeFile(fileName *os.File) error {
+	err := fileName.Close()
 	if err != nil {
-		log.Fatal("Failed to get data from file path. error = ", err)
+		log.Fatal("failed to close file : ", fileName, err)
 	}
-	dataFile, _ := createFile(fileName)
-	//downloading file
-	if _, err := io.Copy(dataFile, reader); err != nil {
-		log.Fatal("Failed to download data at io.Copy. error = ", err)
-	}
-	_ = closeFile(dataFile)
-	log.Info("data download sucessful...")
-	//initizalizing validator and validationMap
-	file, _ := openFile(fileName)
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-
-	validate := validator.New()
-	initialize()
-	var reportMap = make(map[string]map[string]bool)
-	var daterange = make(map[string]map[string]bool)
-	var dates = make([]string, 0, 10)
-	All_Events := []string{Session, Hubspot_created, Hubspot_updated, Sf_created, Sf_updated}
-	//Scanning the file row by row
-	for scanner.Scan() {
-		row := []byte(scanner.Text())
-		var data P.CounterEventFormat
-		json.Unmarshal(row, &data)
-		time := time.Unix(data.EventTimestamp, 0)
-		weekday := time.Weekday().String()[0:3]
-		dateKey := time.AddDate(0, 0, 0).Format(U.DATETIME_FORMAT_YYYYMMDD)
-		if dateKey > toDate {
-			continue
-		}
-		mapKey := fmt.Sprintf("Date:%s, Day:%s ", dateKey, weekday)
-		if daterange[mapKey] == nil {
-			daterange[mapKey] = make(map[string]bool)
-			for _, event := range All_Events {
-				daterange[mapKey][event] = false
-			}
-			dates = append(dates, mapKey)
-		}
-		eventName := data.EventName
-		if validations[eventName] == nil {
-			eventName = "NoEvent"
-		} else {
-			daterange[mapKey][eventName] = true
-		}
-		r := reflect.ValueOf(data)
-		for key, value := range validations[eventName] {
-			var f reflect.Value
-			if value.property == "" {
-				f = reflect.Indirect(r).FieldByName(value.feild)
-			} else if value.property == "UserProperties" || value.property == "EventProperties" {
-				f = reflect.Indirect(r).FieldByName(value.property).MapIndex(reflect.ValueOf(value.feild))
-			} else {
-				log.Info("invalid property")
-				continue
-			}
-			if (!f.IsValid() && value.required) || (f.IsValid() && validate.Var(f.Interface(), value.validate) != nil) {
-				if reportMap[key] == nil {
-					reportMap[key] = make(map[string]bool)
-				}
-				reportMap[key][data.UserId] = true
-			}
-		}
-	}
-	//end of data file
-
-	// Creating Reports
-	var report, detailed_report *os.File
-	report, _ = createFile(ReportName)
-	detailed_report, _ = createFile(DetailedReportName)
-	report.WriteString(fmt.Sprintf("Report from %s to %s.\n\n", *fromDate, toDate))
-	detailed_report.WriteString(fmt.Sprintf("Report from %s to %s.\n\n", *fromDate, toDate))
-
-	for key, value := range reportMap {
-		if len(value) != 0 {
-			report.WriteString(fmt.Sprintf("Total invalid %s : %s \n", key, strconv.Itoa(int(len(value)))))
-			detailed_report.WriteString("Invalid " + key + ":\n")
-			for uid := range value {
-				detailed_report.WriteString(uid + "\n")
-			}
-			detailed_report.WriteString("\n")
-		}
-	}
-	sort.Strings(dates)
-	report.WriteString("\nEvents for Date Ranges :\n")
-	for _, key := range dates {
-		report.WriteString(key + " : ")
-		value := daterange[key]
-		s := ""
-		flag := false
-		for event, present := range value {
-			if present {
-				flag = true
-				s += event
-				s += ", "
-			}
-		}
-		if !flag {
-			report.WriteString("None\n")
-			continue
-		}
-		report.WriteString(s[0:len(s)-2] + "\n")
-	}
-	report.WriteString("\nMissing events for Date Ranges :\n")
-	for _, key := range dates {
-		report.WriteString(key + " : ")
-		value := daterange[key]
-		s := ""
-		flag := false
-		for event, present := range value {
-			if !present {
-				flag = true
-				s += event
-				s += ", "
-			}
-		}
-		if !flag {
-			report.WriteString("None\n")
-			continue
-		}
-		report.WriteString(s[0:len(s)-2] + "\n")
-	}
-
-	//uploading reports to cloud
-	_ = closeFile(report)
-	_ = closeFile(detailed_report)
-	report, _ = openFile(ReportName)
-	detailed_report, _ = openFile(DetailedReportName)
-	err = (cloudManager).Create(fileDir, ReportName, report)
+	return err
+}
+func createFile(fileName string) (*os.File, error) {
+	f, err := os.Create(fileName)
 	if err != nil {
-		log.Fatal("Failed to upload report in cloud. error = ", err)
+		log.Fatal("Failed to create OS file : ", fileName, err)
 	}
-	err = (cloudManager).Create(fileDir, DetailedReportName, detailed_report)
-	if err != nil {
-		log.Fatal("Failed to upload detailed_report in cloud. error = ", err)
-	}
-	//reports uploaded
-	//closing reports
-	_ = closeFile(report)
-	_ = closeFile(detailed_report)
-	log.Info(fmt.Sprintf("Report written successfully from %s to %s.", *fromDate, toDate))
-	//sucess log
+	return f, err
 }
