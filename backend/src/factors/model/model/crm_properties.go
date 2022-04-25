4cb97ee3b58b5b5550c79135906bbe46fb64b0ab
Maisa21 <61135455+Maisa21@users.noreply.github.com>
2022-04-25 13:30:14 +0530

    CRM properties (#4680)
    * add model file
    
    * fix schema for properties
    
    * add crm properties tabel
    
    * add enrich for user and group properties
    
    * add name from activites
    
    * code cleanup
    
    * fix existenc check
    
    * add tests
    
    * fix display names
    
    * code cleanup
    
    * code cleanup
    
    * fix test
    
    * fix type
    
    * remove postgres model
    
    * fix test

---
+++ backend/src/factors/model/model/crm_properties.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	U "factors/util"
+	"time"
+)
+
+// CRMProperty interface for crm_properties table
+type CRMProperty struct {
+	ID               string    `gorm:"primary_key:true;auto_increment:false" json:"id"`
+	ProjectID        uint64    `gorm:"primary_key:true;auto_increment:false" json:"project_id"`
+	Source           CRMSource `gorm:"primary_key:true;auto_increment:false" json:"source"`
+	Type             int       `gorm:"primary_key:true;auto_increment:false" json:"type"`
+	Name             string    `gorm:"primary_key:true;auto_increment:false" json:"name"`
+	Timestamp        int64     `json:"timestamp"`
+	ExternalDataType string    `json:"external_data_type"`
+	MappedDataType   string    `json:"mapped_data_type"`
+	Label            string    `json:"label"`
+	Synced           bool      `json:"synced"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+}
+
+func IsValidCRMMappedDataType(dataType string) bool {
+	if dataType != U.PropertyTypeDateTime && dataType != U.PropertyTypeNumerical {
+		return false
+	}
+
+	return true
+}
---
+++ backend/src/factors/model/store/memsql/crm_properties.go
@@ -0,0 +1,147 @@
+package memsql
+
+import (
+	"errors"
+	C "factors/config"
+	"factors/model/model"
+	U "factors/util"
+	"net/http"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	log "github.com/sirupsen/logrus"
+)
+
+func isCurrentPropertyType(projectID uint64, incomingProperty *model.CRMProperty) (int, error) {
+	logFields := log.Fields{"project_id": projectID, "crm_property": incomingProperty}
+	defer model.LogOnSlowExecutionWithParams(time.Now(), &logFields)
+
+	db := C.GetServices().Db
+	var currentProperty model.CRMProperty
+	err := db.Model(&model.CRMProperty{}).Where("project_id = ? AND source = ? AND type = ? and name = ? ",
+		projectID, incomingProperty.Source, incomingProperty.Type, incomingProperty.Name).Order("timestamp desc").Limit(1).
+		Find(&currentProperty).Error
+
+	if err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return http.StatusNotFound, nil
+		}
+		log.WithFields(log.Fields{"project_id": projectID, "crm_property": currentProperty}).WithError(err).
+			Error("Failed to check for current crm property.")
+		return http.StatusInternalServerError, err
+	}
+
+	if currentProperty.ID == "" ||
+		incomingProperty.ExternalDataType != currentProperty.ExternalDataType {
+		return http.StatusNotFound, nil
+	}
+
+	return http.StatusFound, nil
+}
+
+func (store *MemSQL) CreateCRMProperties(crmProperty *model.CRMProperty) (int, error) {
+	logFields := log.Fields{"crm_properties": crmProperty}
+	logCtx := log.WithFields(logFields)
+
+	if crmProperty.ProjectID == 0 {
+		logCtx.Error("Missing project id.")
+		return http.StatusBadRequest, errors.New("missing project id")
+	}
+
+	if crmProperty.Source == 0 {
+		logCtx.Error("Missing source.")
+		return http.StatusBadRequest, errors.New("missing source")
+	}
+
+	if crmProperty.Type == 0 || crmProperty.Name == "" || crmProperty.ExternalDataType == "" {
+		logCtx.Error("Missing crm property required fields.")
+		return http.StatusBadRequest, errors.New("missing properties required fields type,name,mapped_data_type")
+	}
+
+	if !model.IsValidCRMMappedDataType(crmProperty.MappedDataType) {
+		logCtx.Error("Invalid mapped data type.")
+		return http.StatusBadRequest, errors.New("invalid mapped data type")
+	}
+
+	status, err := isCurrentPropertyType(crmProperty.ProjectID, crmProperty)
+	if err != nil {
+		logCtx.WithError(err).Error("Failed to check for current property type.")
+		return http.StatusInternalServerError, err
+	}
+
+	if status != http.StatusNotFound {
+		if status == http.StatusFound {
+			return http.StatusConflict, nil
+		}
+
+		return http.StatusInternalServerError, errors.New("failed to check for existence of property type")
+	}
+
+	crmProperty.Timestamp = U.TimeNowZ().Unix()
+	crmProperty.ID = U.GetUUID()
+
+	db := C.GetServices().Db
+	if err := db.Create(crmProperty).Error; err != nil {
+		if isDuplicateRecord(err) {
+			return http.StatusConflict, nil
+		}
+		logCtx.WithError(err).Error("Failed to insert crm properties.")
+		return http.StatusInternalServerError, err
+	}
+
+	return http.StatusCreated, nil
+}
+
+func (store *MemSQL) GetCRMPropertiesForSync(projectID uint64) ([]model.CRMProperty, int) {
+	logFields := log.Fields{"project_id": projectID}
+	defer model.LogOnSlowExecutionWithParams(time.Now(), &logFields)
+
+	logCtx := log.WithFields(logFields)
+	if projectID == 0 {
+		logCtx.Error("Invalid project id.")
+	}
+
+	db := C.GetServices().Db
+
+	var properties []model.CRMProperty
+	err := db.Model(model.CRMProperty{}).Where("project_id = ? AND synced = false", projectID).
+		Order("timestamp").Find(&properties).Error
+	if err != nil {
+		logCtx.WithError(err).Error("Failed to get crm properties for sync.")
+		return nil, http.StatusInternalServerError
+	}
+
+	if len(properties) == 0 {
+		return nil, http.StatusNotFound
+	}
+
+	return properties, http.StatusFound
+}
+
+func (store *MemSQL) UpdateCRMProperyAsSynced(projectID uint64, source model.CRMSource, crmProperty *model.CRMProperty) (*model.CRMProperty, int) {
+
+	logFields := log.Fields{"crm_properties": crmProperty, "source": source, "project_id": projectID}
+	defer model.LogOnSlowExecutionWithParams(time.Now(), &logFields)
+
+	logCtx := log.WithFields(logFields)
+	if projectID == 0 || source == 0 || crmProperty.ID == "" || crmProperty.Type == 0 ||
+		crmProperty.Name == "" || crmProperty.Timestamp == 0 {
+		logCtx.Error("Invalid parameters")
+		return nil, http.StatusBadRequest
+	}
+
+	updates := make(map[string]interface{})
+	updates["synced"] = true
+
+	db := C.GetServices().Db
+	err := db.Model(&model.CRMProperty{}).Where("project_id = ? AND source = ? AND id = ? AND type= ? AND name = ? AND timestamp= ? ",
+		projectID, source, crmProperty.ID, crmProperty.Type, crmProperty.Name, crmProperty.Timestamp).Updates(updates).Error
+	if err != nil {
+		logCtx.WithError(err).Error("Failed to update crm user as synced.")
+		return nil, http.StatusInternalServerError
+	}
+
+	crmProperty.Synced = true
+
+	return crmProperty, http.StatusAccepted
+}
