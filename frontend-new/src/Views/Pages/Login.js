d7503b4b21eb93fb2afd65cb8bba1fc36d3d060d
Junaid Khan <92861240+junaidfactors@users.noreply.github.com>
2022-06-13 12:22:23 +0530

    Google ads error fix (#5223)
    * Google ads error msg fix
    
    * error text change

--- frontend-new/src/Views/Pages/Login.js
+++ frontend-new/src/Views/Pages/Login.js
@@ -106,7 +106,7 @@ function Login(props) {
                                     <div className={'flex flex-col justify-center items-center mt-8 w-full'} >
                                         <Form.Item label={null}
                                             name="form_username"
-                                            rules={[{ required: true, type: 'email', message: 'Please enter email' }]}
+                                            rules={[{ required: true, type: 'email', message: 'Please enter a valid email' }]}
                                             >
                                             <Input className={'fa-input w-full'} disabled={dataLoading} size={'large'} placeholder="Email" />
                                         </Form.Item>
