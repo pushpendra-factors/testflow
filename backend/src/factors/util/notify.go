c521ed35b628a74b3d8e02d7c07e8e7fefdaa14c
Viknesh R <105622491+R-Viknesh@users.noreply.github.com>
2022-06-16 10:54:50 +0530

    Panic notification on slack (#5224)
    * Send notification to Slack
    
    * Update app.go
    
    * Delete app.go
    
    * Update notify.go
    
    * Update notify.go

--- backend/src/factors/util/notify.go
+++ backend/src/factors/util/notify.go
@@ -60,14 +60,94 @@ func NotifyThroughSNS(source, env, message interface{}) error {
 	return nil
 }
 
+// NotifyThroughSlack - Send slack notification to the team.
+func NotifyThroughSlack(source, env, message interface{}) error {
+	if env != "staging" && env != "production" && env != "development" {
+		return fmt.Errorf("nofitication skipped for env %s", env)
+	}
+
+	body := map[string]interface{}{
+		"blocks": []interface{}{
+			map[string]interface{}{
+				"type": "section",
+				"fields": []interface{}{
+					map[string]interface{}{
+						"type": "mrkdwn",
+						"text": "*Source*: " + fmt.Sprintf("%v", source),
+					},
+					map[string]interface{}{
+						"type": "mrkdwn",
+						"text": "*Environment*: " + fmt.Sprintf("%v", env),
+					},
+				},
+			},
+			map[string]interface{}{
+				"type": "section",
+				"fields": []interface{}{
+					map[string]interface{}{
+						"type": "mrkdwn",
+						"text": "*Error message*\n" + fmt.Sprintf("%v", message),
+					},
+				},
+			},
+		},
+	}
+
+	// pagerduty-high-priority Slack channel
+	url_Slack_TOPIC := "https://hooks.slack.com/services/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"
+
+	if env == "staging" {
+		log.WithFields(log.Fields{"message": message, "source": source}).Info("Notification.")
+		// panic-notification-on-slack Slack channel
+		url_Slack_TOPIC = "https://hooks.slack.com/services/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"
+	}
+
+	jsonBody, err := json.MarshalIndent(body, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	if env == "development" {
+		fmt.Println("-- Notification Template -- \n")
+		fmt.Println(string(jsonBody))
+		// panic-notification-on-slack Slack channel
+		url_Slack_TOPIC = "https://hooks.slack.com/services/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"
+	}
+
+	req, err := http.NewRequest("POST", url_Slack_TOPIC, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	} else if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return fmt.Errorf("slack return non 200 status: %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
 func NotifyOnPanic(taskId, env string) {
+
 	if pe := recover(); pe != nil {
 		if ne := NotifyThroughSNS(taskId, env,
 			map[string]interface{}{"panic_error": pe, "stacktrace": string(debug.Stack())}); ne != nil {
-			log.Fatal(ne, pe) // using fatal to avoid panic loop.
+			log.WithField("stack_trace", string(debug.Stack())).Error(pe)
+			return
 		}
+	}
 
-		log.Fatal(pe) // using fatal to avoid panic loop.
+	if pe := recover(); pe != nil {
+		if ne := NotifyThroughSlack(taskId, env,
+			map[string]interface{}{"panic_error": pe, "stacktrace": string(debug.Stack())}); ne != nil {
+			log.WithField("stack_trace", string(debug.Stack())).Error(pe)
+			return
+		}
 	}
 }
 
@@ -84,6 +164,11 @@ func NotifyOnPanicWithError(env, appName string) {
 		err := NotifyThroughSNS(appName, env, msg)
 		if err != nil {
 			log.WithError(err).Error("failed to send message to sns")
+		}
+
+		err = NotifyThroughSlack(appName, env, msg)
+		if err != nil {
+			log.WithError(err).Error("failed to send message to slack")
 		}
 	}
 }
