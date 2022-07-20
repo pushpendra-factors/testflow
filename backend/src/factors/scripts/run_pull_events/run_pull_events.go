ad0b9bfc5322be0d7d6afec18cf51933716a9859
anshul-factors <85862157+anshul-factors@users.noreply.github.com>
2022-07-20 14:24:06 +0530

    default value and fix (#5543)

--- backend/src/factors/scripts/run_pull_events/run_pull_events.go
+++ backend/src/factors/scripts/run_pull_events/run_pull_events.go
@@ -54,7 +54,7 @@ func main() {
 
 	hardPull := flag.Bool("hard_pull", false, "replace the files already present")
 
-	fileTypesFlag := flag.String("file_types", "",
+	fileTypesFlag := flag.String("file_types", "*",
 		"Optional: file type. A comma separated list of file types and supports '*' for all files. ex: 1,2,6,9") //refer to T.fileType map
 	projectIdFlag := flag.String("project_ids", "",
 		"Optional: Project Id. A comma separated list of project Ids and supports '*' for all projects. ex: 1,2,6,9")
