3ced4c0c1c843ebf0d14cd22e976feb2f3f9fcbd
Jitesh Kriplani <68528023+jk0693@users.noreply.github.com>
2022-08-24 09:58:57 +0530

    format fix for sparkline comparison charts (#5810)

--- frontend-new/src/Views/CoreQuery/EventsAnalytics/eventsAnalytics.helpers.js
+++ frontend-new/src/Views/CoreQuery/EventsAnalytics/eventsAnalytics.helpers.js
@@ -17,8 +17,8 @@ export const getBreakdownDisplayName = ({
     propCategory === 'user'
       ? get(userPropNames, property, property)
       : propCategory === 'event'
-        ? get(eventPropNames, property, property)
-        : property;
+      ? get(eventPropNames, property, property)
+      : property;
 
   if (breakdown.eventIndex && !multipleEvents) {
     return displayTitle + ' (event)';
@@ -26,6 +26,28 @@ export const getBreakdownDisplayName = ({
   return displayTitle;
 };
 
-export const getEventDisplayName = ({ eventNames, event }) => {
+export const getEventDisplayName = ({ eventNames, event, queryType, kpi }) => {
+  if (
+    queryType === QUERY_TYPE_KPI &&
+    kpi != null &&
+    get(kpi, 'category', null) === 'channels'
+  ) {
+    const kpiGroup = get(kpi, 'group', null);
+    if (kpiGroup === 'facebook_metrics') {
+      return 'Facebook ' + kpi.label;
+    }
+    if (kpiGroup === 'google_organic_metrics') {
+      return 'Google ' + kpi.label;
+    }
+    if (kpiGroup === 'all_channels_metrics') {
+      return 'All Channels ' + kpi.label;
+    }
+    if (kpiGroup === 'bingads_metrics') {
+      return 'Bing Ads ' + kpi.label;
+    }
+    if (kpiGroup === 'linkedin_metrics') {
+      return 'Linkedin ' + kpi.label;
+    }
+  }
   return get(eventNames, event, event);
 };
---
+++ frontend-new/src/components/SparkChartWithCount/ChartHeader.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import cx from 'classnames';
+import { Tooltip } from 'antd';
+import { Number as NumFormat, SVG, Text } from '../factorsComponents';
+import { SPARK_LINE_CHART_TITLE_CHAR_COUNT } from '../../constants/charts.constants';
+import { getFormattedKpiValue } from '../../Views/CoreQuery/KPIAnalysis/kpiAnalysis.helpers';
+import LegendsCircle from '../../styles/components/LegendsCircle';
+import ControlledComponent from '../ControlledComponent/ControlledComponent';
+
+function ChartHeader({
+  total,
+  bgColor,
+  smallFont = false,
+  metricType = null,
+  compareTotal,
+  comparisonEnabled,
+  headerTitle
+}) {
+  const percentChange = comparisonEnabled
+    ? ((total - compareTotal) / compareTotal) * 100
+    : 0;
+
+  const changeIcon = comparisonEnabled ? (
+    <SVG
+      color={percentChange > 0 ? '#5ACA89' : '#FF0000'}
+      name={percentChange > 0 ? 'arrowLift' : 'arrowDown'}
+      size={16}
+    />
+  ) : null;
+
+  return (
+    <div className={cx('flex flex-col items-center justify-center row-gap-2')}>
+      <Tooltip title={headerTitle}>
+        <div className={'flex items-center col-gap-1'}>
+          <LegendsCircle color={bgColor} />
+          <Text color="grey-8" type="title" level={7}>
+            {headerTitle.length > SPARK_LINE_CHART_TITLE_CHAR_COUNT
+              ? headerTitle.slice(0, SPARK_LINE_CHART_TITLE_CHAR_COUNT) + '...'
+              : headerTitle}
+          </Text>
+        </div>
+      </Tooltip>
+
+      <ControlledComponent controller={!smallFont}>
+        <Text weight="bold" type="title" level={2} color="grey-2">
+          {metricType ? (
+            getFormattedKpiValue({ value: total, metricType })
+          ) : (
+            <NumFormat shortHand={total > 1000} number={total} />
+          )}
+        </Text>
+      </ControlledComponent>
+
+      <ControlledComponent controller={smallFont}>
+        <Text weight="bold" type="title" level={3} color="grey-2">
+          {metricType ? (
+            getFormattedKpiValue({ value: total, metricType })
+          ) : (
+            <NumFormat shortHand={total > 1000} number={total} />
+          )}
+        </Text>
+      </ControlledComponent>
+
+      {comparisonEnabled && (
+        <div className="flex flex-col row-gap-1 items-center">
+          <div className="flex col-gap-1 items-center">
+            {changeIcon}
+            <Text
+              level={7}
+              type="title"
+              color={percentChange < 0 ? 'red' : 'green'}
+            >
+              <NumFormat number={Math.abs(percentChange)} />%
+            </Text>
+          </div>
+          <Text type="title" level={8} color="grey">
+            {metricType ? (
+              getFormattedKpiValue({ value: compareTotal, metricType })
+            ) : (
+              <NumFormat
+                number={compareTotal}
+                shortHand={compareTotal > 1000}
+              />
+            )}{' '}
+            in prev. period
+          </Text>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ChartHeader;
