7d488d5ba03d9f2dcb36e0477dbe109fef58d453
parth-factors <106651233+parth-factors@users.noreply.github.com>
2022-08-24 13:52:08 +0530

    fixes on staging (#5817)

--- backend/src/factors/util/util.go
+++ backend/src/factors/util/util.go
@@ -78,12 +78,12 @@ var spaceBytes = []byte{0x20}
 
 // TODO Handle all errors for location on all functions later.
 var QueryDateRangePresets = map[string]func(TimeZoneString) (int64, int64, error){
-	DateRangePresetLastMonth:    GetQueryRangePresetLastMonthIn,
+	DateRangePresetToday:        GetQueryRangePresetTodayIn,
+	DateRangePresetYesterday:    GetQueryRangePresetYesterdayIn,
+	DateRangePresetCurrentWeek:  GetQueryRangePresetCurrentWeekIn,
 	DateRangePresetLastWeek:     GetQueryRangePresetLastWeekIn,
 	DateRangePresetCurrentMonth: GetQueryRangePresetCurrentMonthIn,
-	DateRangePresetCurrentWeek:  GetQueryRangePresetCurrentWeekIn,
-	DateRangePresetYesterday:    GetQueryRangePresetYesterdayIn,
-	DateRangePresetToday:        GetQueryRangePresetTodayIn,
+	DateRangePresetLastMonth:    GetQueryRangePresetLastMonthIn,
 }
 
 var PresetLookup = map[string]string{
