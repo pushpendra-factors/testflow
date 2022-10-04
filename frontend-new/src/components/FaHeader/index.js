95a006e1042771b48389914e616d6cdc73fec67a
Pushpendra Vishwakarma <pushpendra@factors.ai>
2022-10-04 13:37:59 +0530

    Hamburger Button on Header, Margin-Right=16px

--- frontend-new/src/components/FaHeader/index.js
+++ frontend-new/src/components/FaHeader/index.js
@@ -18,9 +18,8 @@ function FaHeader(props) {
       <div className={'flex items-center'}>
         <Button
           onClick={onCollapse}
-          className='fa-btn--custom mr-2 ml-4'
+          className={'fa-btn--custom mr-2 ml-4 '+styles.header_hamburgerbtn}
           type='text'
-          style={{marginRight:'16px'}}
         >
           <SVG name={'bars'} />
         </Button>
