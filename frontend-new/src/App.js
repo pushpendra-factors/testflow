55e3e7b6b7b517a48ef2b7c8db9d9d501b8a0c29
Pushpendra Vishwakarma <pushpendra@factors.ai>
2022-10-11 13:31:35 +0530

    Build Config, App.js code removed req for Intercom locally

--- frontend-new/src/App.js
+++ frontend-new/src/App.js
@@ -29,48 +29,7 @@ const AppLayout = lazyWithRetry(() => import("./Views/AppLayout"));
 
 function App({ isAgentLoggedIn, agent_details, active_project, enableBingAdsIntegration, enableMarketoIntegration }) {
   const dispatch = useDispatch();
- //intercom init and passing logged-in user-data 
- var APP_ID = "rvffkuu7";
- window.intercomSettings = {
-   app_id: APP_ID,
-   name: agent_details?.first_name,
-   email: agent_details?.email,
-   user_id: agent_details?.uuid,
- };
-
- (function () {
-   var w = window;
-   var ic = w.Intercom;
-   if (typeof ic === "function") {
-     ic("reattach_activator");
-     ic("update", w.intercomSettings);    
-   } else {
-     var d = document;  
-     var i = function () {
-       i.c(arguments);
-     };
-     i.q = [];
-     i.c = function (args) {
-       i.q.push(args);
-     };
-     w.Intercom = i;
-     var l = function () {
-       var s = d.createElement("script");
-       s.type = "text/javascript";
-       s.async = true;
-       s.src = "https://widget.intercom.io/widget/" + APP_ID;
-       var x = d.getElementsByTagName("script")[0];
-       x.parentNode.insertBefore(s, x);
-     };
-     if (document.readyState === "complete") {
-       l();
-     } else if (w.attachEvent) {
-       w.attachEvent("onload", l);
-     } else {
-       w.addEventListener("load", l, false);
-     }
-   }
- })();
+  
   const ssoLogin = () => {
     if (window.location.href.indexOf("?error=") > -1) {
       let searchParams = new URLSearchParams(window.location.search);
