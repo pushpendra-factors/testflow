f55c5d5506e48afae2a2ff6237f2350b360571e1
Jitesh Kriplani <68528023+jk0693@users.noreply.github.com>
2022-10-19 11:18:10 +0530

    attribution event based filters (#6341)

--- frontend-new/src/Views/CoreQuery/AttributionsResult/index.js
+++ frontend-new/src/Views/CoreQuery/AttributionsResult/index.js
@@ -16,8 +16,9 @@ function AttributionsResult({
     touchpoint,
     models,
     linkedEvents,
-    attr_dimensions,
-    content_groups,
+    attr_dimensions: attrDimensions,
+    content_groups: contentGroups,
+    attrQueries
   } = attributionsState;
 
   return (
@@ -33,12 +34,13 @@ function AttributionsResult({
       attribution_method={models[0]}
       attribution_method_compare={models[1]}
       section={section}
-      attr_dimensions={attr_dimensions}
-      content_groups={content_groups}
+      attr_dimensions={attrDimensions}
+      content_groups={contentGroups}
       currMetricsValue={currMetricsValue}
       ref={renderedCompRef}
       chartType={chartType}
       queryOptions={queryOptions}
+      attrQueries={attrQueries}
     />
   );
 }
