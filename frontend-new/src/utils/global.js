137982fa7c84a546a95929870e7df70b94db45fe
Jitesh Kriplani <68528023+jk0693@users.noreply.github.com>
2022-11-30 10:25:35 +0530

    changed isNumeric fn definition (#6735)

--- frontend-new/src/utils/global.js
+++ frontend-new/src/utils/global.js
@@ -33,12 +33,8 @@ export const toCapitalCase = (str) => {
   return lower.charAt(0).toUpperCase() + lower.slice(1);
 };
 
-export function isNumeric(str) {
-  if (typeof str !== 'string') return false; // we only process strings!
-  return (
-    !Number.isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-    !Number.isNaN(parseFloat(str))
-  ); // ...and ensure strings of whitespace fail
+export function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 export const abbreviateNumber = (n) => {
