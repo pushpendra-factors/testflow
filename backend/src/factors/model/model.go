40d6df06631766d46cc08a93c45173a32d3e4b0a
janani <janani@factors.ai>
2022-12-07 14:40:35 +0530

    leadsquared new documents

--- backend/src/factors/model/model.go
+++ backend/src/factors/model/model.go
@@ -791,5 +791,5 @@ type Model interface {
 
 	// leadsquaredmarker
 	CreateLeadSquaredMarker(marker model.LeadsquaredMarker) int
-	GetLeadSquaredMarker(ProjectID int64, Delta int64, Document string, Tag string) (int, int)
+	GetLeadSquaredMarker(ProjectID int64, Delta int64, Document string, Tag string) (int, int, bool)
 }
