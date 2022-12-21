5c7ce17c1428decf0ad5225ad2c8cd361e0b6bfd
Mohammad Ashhar Ansari <60336936+ansari-ashhar@users.noreply.github.com>
2022-12-21 16:00:00 +0530

    Fix for breakdown by channel (#6951)

--- backend/src/factors/model/store/memsql/analytics.go
+++ backend/src/factors/model/store/memsql/analytics.go
@@ -2285,7 +2285,7 @@ func getChannelGroupKeyIndexesForSlicing(cols []string) (int, int, error) {
 
 	index := 0
 	for _, col := range cols {
-		if strings.HasPrefix(col, "campaign_") || strings.HasPrefix(col, "ad_group_") || strings.HasPrefix(col, "keyword_") {
+		if strings.HasPrefix(col, "campaign_") || strings.HasPrefix(col, "ad_group_") || strings.HasPrefix(col, "keyword_") || strings.HasPrefix(col, "channel_") {
 			if start == -1 {
 				start = index
 			} else {
