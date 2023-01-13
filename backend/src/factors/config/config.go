ad5ea81ff5b0665cdf34a83f0bd30cc7c032eac2
Dineshprabu S <44255030+dineshprabu-slashbit@users.noreply.github.com>
2023-01-13 16:52:51 +0530

    Fix db2 service. (#7214)

--- backend/src/factors/config/config.go
+++ backend/src/factors/config/config.go
@@ -732,10 +732,10 @@ func InitDBWithMaxIdleAndMaxOpenConn(config Configuration,
 	maxOpenConns, maxIdleConns int) error {
 	if UseMemSQLDatabaseStore() {
 		if IsDBConnectionPool2Enabled() {
-			return InitMemSQLDBWithMaxIdleAndMaxOpenConn(config.MemSQL2Info, maxOpenConns, maxIdleConns)
-		} else {
-			return InitMemSQLDBWithMaxIdleAndMaxOpenConn(config.MemSQLInfo, maxOpenConns, maxIdleConns)
+			return InitMemSQLDBWithMaxIdleAndMaxOpenConn(config.MemSQL2Info, maxOpenConns, maxIdleConns, true)
 		}
+
+		return InitMemSQLDBWithMaxIdleAndMaxOpenConn(config.MemSQLInfo, maxOpenConns, maxIdleConns, false)
 	}
 	return InitPostgresDBWithMaxIdleAndMaxOpenConn(config.DBInfo, maxOpenConns, maxIdleConns)
 }
@@ -881,7 +881,7 @@ func isValidMemSQLResourcePool(resourcePool string) bool {
 	return exists
 }
 
-func InitMemSQLDBWithMaxIdleAndMaxOpenConn(dbConf DBConf, maxOpenConns, maxIdleConns int) error {
+func InitMemSQLDBWithMaxIdleAndMaxOpenConn(dbConf DBConf, maxOpenConns, maxIdleConns int, isDb2 bool) error {
 	if services == nil {
 		services = &Services{}
 	}
@@ -942,7 +942,13 @@ func InitMemSQLDBWithMaxIdleAndMaxOpenConn(dbConf DBConf, maxOpenConns, maxIdleC
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 
-	services.Db = memSQLDB
+	// initiates corresponding service.
+	if isDb2 {
+		services.Db2 = memSQLDB
+	} else {
+		services.Db = memSQLDB
+	}
+
 	configuration.DBInfo = dbConf
 	services.DBContext = &ctx
 	services.DBContextCancel = &cancel
