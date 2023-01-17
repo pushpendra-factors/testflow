bc2354a3a5b54f6df6f6aca9a2b167a4d5e5f325
Junaid Khan <92861240+junaidfactors@users.noreply.github.com>
2023-01-17 18:13:20 +0530

    Fix Event based trigger (#7242)
    * fix edit
    
    * add state into alert utils file
    
    * Fix create and view alert

--- frontend-new/src/Views/Settings/ProjectSettings/Alerts/utils.js
+++ frontend-new/src/Views/Settings/ProjectSettings/Alerts/utils.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { formatFilterDate } from 'Utils/dataFormatter';
+import { groupFilters } from 'Utils/global';
 import { FILTER_TYPES } from '../../../CoreQuery/constants';
 import { convertDateTimeObjectValuesToMilliSeconds, reverseDateOperatorMap, reverseOperatorMap } from '../../../CoreQuery/utils';
 
@@ -20,6 +21,94 @@ const operatorMap = {
     'not in the current': 'notInCurrent',
     before: 'before',
     since: 'since',
+  };
+
+  export const getEventsWithProperties = (queries) => {
+    const ewps = [];
+    queries.forEach((ev) => {
+      const filterProps = [];
+  
+      const filtersGroupedByRef = Object.values(groupFilters(ev.filters, 'ref'));
+      filtersGroupedByRef.forEach((filtersGr) => {
+        if (filtersGr.length === 1) {
+          const fil = filtersGr[0];
+          if (Array.isArray(fil.values)) {
+            fil.values.forEach((val, index) => {
+              filterProps.push({
+                en: fil.props[2] === 'group' ? 'user' : fil.props[2],
+                lop: !index ? 'AND' : 'OR',
+                op: operatorMap[fil.operator],
+                pr: fil.props[0],
+                ty: fil.props[1],
+                va: fil.props[1] === 'datetime' ? val : val
+              });
+            });
+          } else {
+            filterProps.push({
+              en: fil.props[2] === 'group' ? 'user' : fil.props[2],
+              lop: 'AND',
+              op: operatorMap[fil.operator],
+              pr: fil.props[0],
+              ty: fil.props[1],
+              va: fil.props[1] === 'datetime' ? fil.values : fil.values
+            });
+          }
+        } else {
+          let fil = filtersGr[0];
+          if (Array.isArray(fil.values)) {
+            fil.values.forEach((val, index) => {
+              filterProps.push({
+                en: fil.props[2] === 'group' ? 'user' : fil.props[2],
+                lop: !index ? 'AND' : 'OR',
+                op: operatorMap[fil.operator],
+                pr: fil.props[0],
+                ty: fil.props[1],
+                va: fil.props[1] === 'datetime' ? val : val
+              });
+            });
+          } else {
+            filterProps.push({
+              en: fil.props[2] === 'group' ? 'user' : fil.props[2],
+              lop: 'AND',
+              op: operatorMap[fil.operator],
+              pr: fil.props[0],
+              ty: fil.props[1],
+              va: fil.props[1] === 'datetime' ? fil.values : fil.values
+            });
+          }
+          fil = filtersGr[1];
+          if (Array.isArray(fil.values)) {
+            fil.values.forEach((val) => {
+              filterProps.push({
+                en: fil.props[2] === 'group' ? 'user' : fil.props[2],
+                lop: 'OR',
+                op: operatorMap[fil.operator],
+                pr: fil.props[0],
+                ty: fil.props[1],
+                va: fil.props[1] === 'datetime' ? val : val
+              });
+            });
+          } else {
+            filterProps.push({
+              en: fil.props[2] === 'group' ? 'user' : fil.props[2],
+              lop: 'OR',
+              op: operatorMap[fil.operator],
+              pr: fil.props[0],
+              ty: fil.props[1],
+              va: fil.props[1] === 'datetime' ? fil.values : fil.values
+            });
+          }
+        }
+      });
+  
+      ewps.push({
+        an: ev.alias,
+        na: ev.label,
+        grpa: ev.group,
+        pr: filterProps
+      });
+    });
+    return ewps[0]?.pr;
   };
 
 export const getEventsWithPropertiesKPI = (filters, category) => {
@@ -85,6 +174,25 @@ export const getEventsWithPropertiesKPI = (filters, category) => {
           extra: [DNa, pr.prNa, pr.prDaTy],
         });
       } else if (pr.prDaTy === FILTER_TYPES.CATEGORICAL) {
+        filters[filters.length - 1].values.push(pr.va);
+      }
+    });
+    return filters;
+  };
+
+  export const getStateFromFiltersEvent = (rawFilters = []) => {
+    const filters = [];
+    rawFilters.forEach((pr) => {
+      if (pr.lop === 'AND') {
+        filters.push({
+          operator:
+            pr.ty === 'datetime'
+              ? reverseDateOperatorMap[pr.op]
+              : reverseOperatorMap[pr.op],
+          props: [pr.pr, pr.ty, pr.en],
+          values: [pr.va]
+        });
+      } else {
         filters[filters.length - 1].values.push(pr.va);
       }
     });
