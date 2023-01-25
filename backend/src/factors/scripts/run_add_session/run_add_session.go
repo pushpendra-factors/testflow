f0e55b95f7be8bb843c822aad756d5e0183501f4
janani-somaskanda <68629545+janani-somaskanda@users.noreply.github.com>
2023-01-25 18:44:10 +0530

    event triggers flow for session and integration request worker (#7324)

--- backend/src/factors/scripts/run_add_session/run_add_session.go
+++ backend/src/factors/scripts/run_add_session/run_add_session.go
@@ -69,6 +69,8 @@ func main() {
 	enableUserLevelEventPullForAddSessionByProjectID := flag.String("enable_user_level_pull", "", "List of projectIds where user level event pull is enabled for AddSession job")
 	eventsPullMaxLimit := flag.Int("max_limit_for_events_pull", 50000, "Maximum limit for pulling events in V2") // Default is 50000
 	batchRangeInSeconds := flag.Int64("batch_range_in_seconds", 0, "Batch size for Add Session job")
+	eventTriggerEnabled := flag.Bool("event_trigger_enabled", false, "")
+	eventTriggerEnabledProjectIDs := flag.String("event_trigger_enabled_project_ids", "", "")
 
 	flag.Parse()
 
@@ -121,6 +123,8 @@ func main() {
 		IngestionTimezoneEnabledProjectIDs:  C.GetTokensFromStringListAsString(*IngestionTimezoneEnabledProjectIDs),
 		EnableUserLevelEventPullForAddSessionByProjectID: *enableUserLevelEventPullForAddSessionByProjectID,
 		EventsPullMaxLimit: *eventsPullMaxLimit,
+		EventTriggerEnabled:           *eventTriggerEnabled,
+		EventTriggerEnabledProjectIDs: *eventTriggerEnabledProjectIDs,
 	}
 
 	C.InitConf(config)
