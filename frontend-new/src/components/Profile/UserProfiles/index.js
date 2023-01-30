d6a771cc9c9e0c5ae8c6e8870cf35d573930698d
Vikas Kumar <85861073+vikasatfactors@users.noreply.github.com>
2023-01-30 16:58:57 +0530

    field name change (#7367)

--- frontend-new/src/components/Profile/UserProfiles/index.js
+++ frontend-new/src/components/Profile/UserProfiles/index.js
@@ -405,8 +405,8 @@ function UserProfiles({
   const segmentInfo = () => {
     const cardContent = [];
     if (activeSegment.query) {
-      if (activeSegment.query.gp) {
-        const filters = formatPayloadForFilters(activeSegment.query.gp);
+      if (activeSegment.query.gup) {
+        const filters = formatPayloadForFilters(activeSegment.query.gup);
         cardContent.push(
           <div className='pointer-events-none mt-3'>
             <PropertyFilter
