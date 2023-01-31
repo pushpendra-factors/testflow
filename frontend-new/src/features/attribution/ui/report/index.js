affffe70563f04676478e502aa1b9df5cac04250
akhil-nair-factors <68314466+akhil-nair-factors@users.noreply.github.com>
2023-01-31 20:23:21 +0530

    group analysis fix report page fix (#7390)

--- frontend-new/src/features/attribution/ui/report/index.js
+++ frontend-new/src/features/attribution/ui/report/index.js
@@ -878,7 +878,7 @@ function CoreQuery({
                     campaignState={campaignState}
                     savedQueryId={savedQueryId}
                     handleChartTypeChange={handleChartTypeChange}
-                    queryOptions={queryOptions}
+                    queryOptions={{...queryOptions, group_analysis: 'all'}}
                     resultState={resultState}
                     queries={appliedQueries}
                     handleDurationChange={handleDurationChange}
