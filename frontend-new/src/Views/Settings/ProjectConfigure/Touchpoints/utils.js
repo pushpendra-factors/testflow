c06209089f5700567ab2b1959a570965963cd82f
Raghavendra <97873444+raghavendra184@users.noreply.github.com>
2023-02-14 13:34:48 +0530

    Hotfix:- OTP Salesforce, setting Rule Type. (#7568)

--- frontend-new/src/Views/Settings/ProjectConfigure/Touchpoints/utils.js
+++ frontend-new/src/Views/Settings/ProjectConfigure/Touchpoints/utils.js
@@ -5,20 +5,21 @@ export const RULE_TYPE_HS_CALLS = 'hs_calls';
 export const RULE_TYPE_HS_MEETINGS = 'hs_meetings';
 export const RULE_TYPE_HS_LISTS = 'hs_contact_list';
 
+export const RULE_TYPE_SF_CONTACT = 'sf_contact';
 
 export const Extra_PROP_SHOW_OPTIONS = [
-    ['Campaign Id', null, 'campaign_id'],
-    ['Adgroup', null, 'adgroup'],
-    ['Adgroup ID', null, 'adgroup_id'],
-    ['Page URL', null, 'page_url']
-  ];
-  
+  ['Campaign Id', null, 'campaign_id'],
+  ['Adgroup', null, 'adgroup'],
+  ['Adgroup ID', null, 'adgroup_id'],
+  ['Page URL', null, 'page_url']
+];
+
 export const SEARCHSOURCE = {
-    source: '',
-    campaign: '',
-    channel: '',
-    campaign_id: '',
-    adgroup: '',
-    adgroup_id: '',
-    page_url: ''
+  source: '',
+  campaign: '',
+  channel: '',
+  campaign_id: '',
+  adgroup: '',
+  adgroup_id: '',
+  page_url: ''
 };
