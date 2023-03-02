30136b91aff88ce27aca48026eda2f88b8b4876f
Sumit <83937876+sumit-factors-ai@users.noreply.github.com>
2023-03-02 14:22:25 +0530

    revert comment changes for editing alert rule (#7737)
    * make alert rule editable
    
    * add nil check in interface conversion
    
    * add return statement
    
    * fix
    
    * add explicit nil check
    
    * make alert rule editable
    
    Signed-off-by: sumit <sumit@factors.ai>
    
    * add nil check in interface conversion
    
    * add return statement
    
    * fix
    
    * add explicit nil check
    
    * revert comment changes
    
    ---------
    
    Signed-off-by: sumit <sumit@factors.ai>

--- backend/src/factors/model/store/memsql/alerts.go
+++ backend/src/factors/model/store/memsql/alerts.go
@@ -305,12 +305,12 @@ func (store *MemSQL) UpdateAlert(projectID int64, alertID string, alert model.Al
 	updatedFields := map[string]interface{}{
 		"alert_name":          alert.AlertName,
 		"alert_configuration": alert.AlertConfiguration,
-		//	"alert_description":   alert.AlertDescription,
-		"updated_at": time.Now().UTC(),
+		"alert_description":   alert.AlertDescription,
+		"updated_at":          time.Now().UTC(),
 	}
 	alert.ID = alertID
 	// validating alert config
-	isValid, status, errMsg := store.validateAlertBody(projectID, alert, true)
+	isValid, status, errMsg := store.validateAlertBody(projectID, alert)
 	if !isValid {
 		return model.Alert{}, status, errMsg
 	}
@@ -352,7 +352,7 @@ func (store *MemSQL) CreateAlert(projectID int64, alert model.Alert) (model.Aler
 		CreatedAt:          time.Now().UTC(),
 		UpdatedAt:          time.Now().UTC(),
 	}
-	isValid, status, errMsg := store.validateAlertBody(projectID, alertRecord, false)
+	isValid, status, errMsg := store.validateAlertBody(projectID, alertRecord)
 	if !isValid {
 		return model.Alert{}, status, errMsg
 	}
@@ -367,7 +367,7 @@ func (store *MemSQL) CreateAlert(projectID int64, alert model.Alert) (model.Aler
 
 }
 
-func (store *MemSQL) validateAlertBody(projectID int64, alert model.Alert, skipDescription bool) (bool, int, string) {
+func (store *MemSQL) validateAlertBody(projectID int64, alert model.Alert) (bool, int, string) {
 	logFields := log.Fields{
 		"project_id": projectID,
 		"alert":      alert,
@@ -411,14 +411,11 @@ func (store *MemSQL) validateAlertBody(projectID int64, alert model.Alert, skipD
 		logCtx.WithError(err).Error("Failed to create alert")
 		return false, http.StatusBadRequest, err.Error()
 	}
-	if skipDescription {
-		return true, http.StatusOK, ""
-	}
 	err = U.DecodePostgresJsonbToStructType(alert.AlertDescription, &alertDescription)
 	if err != nil {
 		return false, http.StatusInternalServerError, "failed to decode jsonb to alert description"
 	}
-	
+
 	if alertDescription.Name == "" && alert.AlertType != model.ALERT_TYPE_QUERY_SHARING {
 		logCtx.Error("Invalid alert name")
 		return false, http.StatusBadRequest, "Invalid alert name"
