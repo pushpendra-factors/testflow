60cfccb5e17e2272c03a1e1f5b84cb9bc58924ca
kartheek-factors <94624403+kartheek-factors@users.noreply.github.com>
2023-03-04 09:37:43 +0530

    Adwords etl v13 upgrade (#7747)
    * Fixing to upgrade adwords api version to v13.
    
    * Remove dependency of unusued route - get_customer_accounts and internal adwords code.
    
    * Fixing last sync and next sync infos.
    
    * Small fixes.
    
    * Fixing last sync and next sync infos.

--- python_backend/lib/adwords/oauth_service/fetch_service.py
+++ python_backend/lib/adwords/oauth_service/fetch_service.py
@@ -1,4 +1,4 @@
-from googleads import oauth2, adwords
+from googleads import oauth2
 
 from google.ads.googleads.client import GoogleAdsClient
 
@@ -8,31 +8,13 @@ ADWORDS_CLIENT_USER_AGENT = "FactorsAI (https://www.factors.ai)"
 class FetchService:
 
     # New Version
-    NEW_VERSION = "v10"
-
+    NEW_VERSION = "v13"
 
     # Old Version
     VERSION = "v201809"
 
     def __init__(self, config):
         self.CONFIG = config
-
-    def get_customer_accounts(self, refresh_token):
-        oauth_client = oauth2.GoogleRefreshTokenClient(self.CONFIG.client_id, self.CONFIG.client_secret, refresh_token)
-        adwords_client = adwords.AdWordsClient(self.CONFIG.developer_token, oauth_client, ADWORDS_CLIENT_USER_AGENT)
-        return adwords_client.GetService("CustomerService", version=self.VERSION)
-
-    def get_service(self, service_name, refresh_token, customer_acc_id):
-        oauth_client = oauth2.GoogleRefreshTokenClient(self.CONFIG.client_id, self.CONFIG.client_secret, refresh_token)
-        adwords_client = adwords.AdWordsClient(self.CONFIG.developer_token, oauth_client, ADWORDS_CLIENT_USER_AGENT)
-        adwords_client.SetClientCustomerId(customer_acc_id)
-        return adwords_client.GetService(service_name, version=self.VERSION)
-
-    def get_report_downloader(self, refresh_token, customer_acc_id):
-        oauth_client = oauth2.GoogleRefreshTokenClient(self.CONFIG.client_id, self.CONFIG.client_secret, refresh_token)
-        adwords_client = adwords.AdWordsClient(self.CONFIG.developer_token, oauth_client, ADWORDS_CLIENT_USER_AGENT)
-        adwords_client.SetClientCustomerId(customer_acc_id)
-        return adwords_client.GetReportDownloader(version=self.VERSION)
 
     def new_get_service(self, service_name, refresh_token, login_customer_id=None):
         # GoogleAdsClient will read the credentials dictionary
