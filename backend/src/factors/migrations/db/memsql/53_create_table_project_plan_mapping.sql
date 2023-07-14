519d0e0c1d60363d1452a3c145b544ba875eb75f
Sumit <83937876+sumit-factors-ai@users.noreply.github.com>
2023-07-14 12:30:06 +0530

    fix schema (#9265)

--- backend/src/factors/migrations/db/memsql/53_create_table_project_plan_mapping.sql
+++ backend/src/factors/migrations/db/memsql/53_create_table_project_plan_mapping.sql
@@ -4,6 +4,5 @@ CREATE TABLE IF NOT EXISTS project_plan_mapping (
     add_ons json,
     last_renewed_on timestamp(6),
     PRIMARY KEY (project_id),
-    SHARD KEY ( id)
-    FOREIGN KEY (plan_id) REFERENCES plan_details(id)
+    SHARD KEY (project_id)
 );
