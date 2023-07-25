aa1c3e42c323bd0a1ea2033ade6204d2f300f6be
Jitesh Kriplani <68528023+jk0693@users.noreply.github.com>
2023-07-25 11:45:17 +0530

    fixed metric type issue for kpis (#9349)

--- frontend-new/src/Views/CoreQuery/index.js
+++ frontend-new/src/Views/CoreQuery/index.js
@@ -620,7 +620,18 @@ function CoreQuery({
         updateAppliedBreakdown();
       }
       if (queryType === QUERY_TYPE_KPI) {
-        setAppliedQueries(queriesA);
+        setAppliedQueries(
+          queriesA.map((q) => {
+            const category = KPI_config.find(
+              (elem) => elem.category === q.category
+            );
+            const metric = category?.metrics.find((m) => m.name === q.metric);
+            return {
+              ...q,
+              metricType: metric?.type != null ? metric.type : q.metricType
+            };
+          })
+        );
         updateAppliedBreakdown();
       }
       if (queryType === QUERY_TYPE_PROFILE) {
@@ -642,7 +653,8 @@ function CoreQuery({
       savedQueries,
       updateChartTypes,
       updateAppliedBreakdown,
-      profileQueries
+      profileQueries,
+      KPI_config
     ]
   );
 
