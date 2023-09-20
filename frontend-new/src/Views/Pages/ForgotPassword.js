11f0a966726fef3c3b046530fcaeb229250b3b78
deepak bisht <deepak@factors.ai>
2023-09-20 16:09:00 +0530

    sqashed signup changes commits

--- frontend-new/src/Views/Pages/ForgotPassword.js
+++ frontend-new/src/Views/Pages/ForgotPassword.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import {
-  Row, Col, Button, Input, Form
-} from 'antd';
-import { Text, SVG } from 'factorsComponents';
-import { useHistory } from 'react-router-dom';
+import { Row, Col, Button, Input, Form, Divider, message } from 'antd';
+import { Text } from 'factorsComponents';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { forgotPassword } from 'Reducers/agentActions';
+import LoggedOutScreenHeader from 'Components/GenericComponents/LoggedOutScreenHeader';
+import ForgotPasswordIllustration from '../../assets/images/forgot_password_illustration.png';
+import ForgotPasswordSuccessIllustration from '../../assets/images/forgot_password_success.png';
+import styles from './index.module.scss';
 
 function ForgotPassword({ forgotPassword }) {
   const [form] = Form.useForm();
@@ -13,31 +15,32 @@ function ForgotPassword({ forgotPassword }) {
   const [errorInfo, seterrorInfo] = useState(null);
   const [userEmail, setUserEmail] = useState(null);
 
-  const history = useHistory();
-  const routeChange = (url) => {
-    history.push(url);
-  };
-
   const SendResetLink = () => {
     setDataLoading(true);
-    form.validateFields().then((value) => {
-      setDataLoading(true);
-      setTimeout(() => {
-        forgotPassword(value.email)
-          .then(() => {
-            setUserEmail(value.email);
-            // history.push('/');
-          }).catch((err) => {
-            setDataLoading(false);
-            form.resetFields();
-            seterrorInfo(err);
-          });
-      }, 200);
-    }).catch((info) => {
-      setDataLoading(false);
-      form.resetFields();
-      seterrorInfo(info);
-    });
+    form
+      .validateFields()
+      .then((value) => {
+        setDataLoading(true);
+        setTimeout(() => {
+          forgotPassword(userEmail ? userEmail : value.email)
+            .then(() => {
+              if (!userEmail) setUserEmail(value.email);
+              else message.success('Email resent successfully');
+              setDataLoading(false);
+              // history.push('/');
+            })
+            .catch((err) => {
+              setDataLoading(false);
+              form.resetFields();
+              seterrorInfo(err);
+            });
+        }, 200);
+      })
+      .catch((info) => {
+        setDataLoading(false);
+        form.resetFields();
+        seterrorInfo(info);
+      });
   };
 
   const onChange = () => {
@@ -47,91 +50,202 @@ function ForgotPassword({ forgotPassword }) {
   return (
     <>
       <div className={'fa-container'}>
-            <Row justify={'center'}>
-                <Col span={12} >
-                    <div className={'flex flex-col justify-center items-center login-container'}>
-                    <Form
-                        form={form}
-                        name="login"
-                        validateTrigger
-                        initialValues={{ remember: false }}
-                        onFinish={SendResetLink}
-                        onChange={onChange}
+        <Row justify={'center'}>
+          <Col span={24}>
+            <LoggedOutScreenHeader />
+          </Col>
+          <Col span={24}>
+            <div className='w-full flex items-center justify-center mt-6'>
+              <div
+                className='flex flex-col justify-center items-center'
+                style={{
+                  width: 400,
+                  padding: '40px 48px',
+                  borderRadius: 8,
+                  border: '1px solid  #D9D9D9'
+                }}
+              >
+                <div className='py-4'>
+                  <img
+                    src={
+                      userEmail
+                        ? ForgotPasswordSuccessIllustration
+                        : ForgotPasswordIllustration
+                    }
+                    alt='illustration'
+                    className={styles.forgotPasswordIllustration}
+                  />
+                </div>
+                <div className={'flex justify-center items-center mt-4'}>
+                  <Text
+                    type={'title'}
+                    level={3}
+                    extraClass={'m-0'}
+                    weight={'bold'}
+                    color='character-title'
+                  >
+                    {userEmail ? 'Please check your email' : 'Forgot password?'}
+                  </Text>
+                </div>
+                <Form
+                  form={form}
+                  name='login'
+                  validateTrigger
+                  initialValues={{ remember: false }}
+                  onFinish={SendResetLink}
+                  onChange={onChange}
+                >
+                  {!userEmail && (
+                    <>
+                      <div
+                        className={
+                          'flex justify-center items-center text-center mt-4'
+                        }
+                      >
+                        <Text
+                          type={'title'}
+                          level={7}
+                          color={'character-primary'}
+                          extraClass={'m-0 desc-text'}
                         >
-                            <Row>
-                                <Col span={24} >
-                                    <div className={'flex justify-center items-center mb-5'} >
-                                        <SVG name={'BrandFull'} size={40} color="white"/>
-                                    </div>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col span={24}>
-                                    <div className={'flex justify-center items-center mt-10'} >
-                                        <Text type={'title'} level={6} extraClass={'m-0'} weight={'bold'}>Forgot password?</Text>
-                                    </div>
-                                </Col>
-                                {!userEmail && <>
-                                <Col span={24}>
-                                    <div className={'flex justify-center items-center mt-10'} >
-                                        <Text type={'title'} level={7} color={'grey'} extraClass={'m-0 desc-text'}>Please enter the email address. We will send an email with a reset link and instructions to reset your password</Text>
-                                    </div>
-                                </Col>
-                                <Col span={24}>
-                                    <div className={'flex flex-col justify-center items-center mt-10'} >
-                                    <Form.Item label={null}
-                                            name="email"
-                                            rules={[{ required: true, type: 'email', message: 'Please enter your email' }]}
-                                            >
-                                            <Input className={'fa-input w-full'} loading={dataLoading} size={'large'} placeholder="Enter your email" />
-                                        </Form.Item>
-
-                                    </div>
-                                </Col>
-                                <Col span={24}>
-                                    <div className={'flex flex-col justify-center items-center mt-5'} >
-                                        {/* <Button type={'primary'} className={'fa-button-50'} size={'large'} onClick={() => routeChange('/resetpassword')}>Send Reset Link</Button> */}
-                                        <Form.Item className={'m-0'} loading={dataLoading}>
-                                            <Button htmlType="submit" loading={dataLoading} type={'primary'} size={'large'} className={'w-full'}>Send Reset Link</Button>
-                                        </Form.Item>
-                                    </div>
-                                </Col>
-                                </>
-                                }
-                                {userEmail && <Col span={24}>
-                                    <div className={'flex flex-col justify-center items-center my-6'} >
-                                        <Text type={'title'} size={'6'} color={'grey'} align={'center'} extraClass={'m-0 desc-text'}>{`An email has been sent to ${userEmail}. Please follow the link in the email to reset your password.`}</Text>
-                                    </div>
-                                </Col>
-                                }
-                                {errorInfo && <Col span={24}>
-                                    <div className={'flex flex-col justify-center items-center mt-1'} >
-                                        <Text type={'title'} color={'red'} size={'7'} className={'m-0'}>{errorInfo}</Text>
-                                    </div>
-                                </Col>
-                                }
-
-                                <Col span={24}>
-                                    <div className={'flex flex-col justify-center items-center mt-10'} >
-                                        <a type={'text'} size={'large'} onClick={() => routeChange('/login')}>Go back to login</a>
-                                    </div>
-                                </Col>
-                                <Col span={24}>
-                                    <div className={'flex flex-col justify-center items-center mt-5'} >
-                                        {/* <Text type={'paragraph'} mini color={'grey'}>Don’t have an account? <a onClick={() => routeChange('/signup')}>Sign Up</a></Text> */}
-                                        <Text type={'paragraph'} mini color={'grey'}>Want to try out Factors.AI? <a href={'https://www.factors.ai/schedule-a-demo'} target="_blank">Request A Demo</a></Text>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Form>
+                          Please enter the email address. We will send an email
+                          with a reset link and instructions to reset your
+                          password
+                        </Text>
+                      </div>
+                      <div
+                        className={
+                          'flex flex-col justify-center items-center mt-6'
+                        }
+                      >
+                        <Form.Item
+                          label={null}
+                          name='email'
+                          rules={[
+                            {
+                              required: true,
+                              type: 'email',
+                              message: 'Please enter your email'
+                            }
+                          ]}
+                          className='w-full'
+                        >
+                          <Input
+                            className={'fa-input w-full'}
+                            loading={dataLoading}
+                            size={'large'}
+                            placeholder='Enter your email'
+                          />
+                        </Form.Item>
+                      </div>
+                      <div
+                        className={
+                          'flex flex-col justify-center items-center mt-6'
+                        }
+                      >
+                        <Form.Item
+                          className={'m-0 w-full'}
+                          loading={dataLoading}
+                        >
+                          <Button
+                            htmlType='submit'
+                            loading={dataLoading}
+                            type={'primary'}
+                            size={'large'}
+                            className={'w-full'}
+                          >
+                            Send Reset Link
+                          </Button>
+                        </Form.Item>
+                      </div>
+                    </>
+                  )}
+                  {userEmail && (
+                    <div
+                      className={
+                        'flex flex-col justify-center items-center my-4 text-center'
+                      }
+                    >
+                      <Text
+                        type={'title'}
+                        size={'6'}
+                        color={'character-secondary'}
+                        align={'center'}
+                        extraClass={'m-0 desc-text'}
+                      >
+                        An email has been sent from{' '}
+                        <span style={{ fontWeight: 600 }}>
+                          support@factors.ai
+                        </span>
+                        . Please follow the link in the email to reset your
+                        password
+                      </Text>
+                      <Text
+                        type={'title'}
+                        size={'6'}
+                        color={'character-primary'}
+                        align={'center'}
+                        weight={'bold'}
+                        extraClass={'m-0 desc-text mt-4 mb-3'}
+                      >
+                        {userEmail}
+                      </Text>
+                      <Divider />
+                      <div className='flex justify-center items-center gap-2'>
+                        <Text
+                          type={'title'}
+                          level={7}
+                          color='character-primary'
+                          extraClass='m-0'
+                        >
+                          Didn’t get it?
+                        </Text>
+                        <Button
+                          htmlType='submit'
+                          loading={dataLoading}
+                          className={styles.resendButton}
+                        >
+                          Resend Email
+                        </Button>
+                      </div>
                     </div>
-                </Col>
-            </Row>
-            <SVG name={'singlePages'} extraClass={'fa-single-screen--illustration'} />
+                  )}
+                  {errorInfo && (
+                    <Col span={24}>
+                      <div
+                        className={
+                          'flex flex-col justify-center items-center mt-1'
+                        }
+                      >
+                        <Text
+                          type={'title'}
+                          color={'red'}
+                          size={'7'}
+                          className={'m-0'}
+                        >
+                          {errorInfo}
+                        </Text>
+                      </div>
+                    </Col>
+                  )}
+                </Form>
+              </div>
+            </div>
+          </Col>
+          <Col span={24}>
+            <div className={'w-full flex  justify-center items-center mt-8'}>
+              <Link
+                to={{
+                  pathname: '/login'
+                }}
+              >
+                Go back to login
+              </Link>
+            </div>
+          </Col>
+        </Row>
       </div>
-
     </>
-
   );
 }
 
