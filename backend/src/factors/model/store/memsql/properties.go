dcaf056dad4700fc1c376f2661f6ea54485a4a60
roshankumar <roshan@factors.ai>
2023-10-06 11:57:23 +0530

    clearbit prop display check.

--- backend/src/factors/model/store/memsql/properties.go
+++ backend/src/factors/model/store/memsql/properties.go
@@ -11,7 +11,7 @@ func (store *MemSQL) GetStandardUserPropertiesBasedOnIntegration(projectID int64
 
 	finalStandardUserProperties := make(map[string]string)
 	for property, propertyDisplayName := range U.STANDARD_USER_PROPERTIES_DISPLAY_NAMES {
-		if !strings.HasPrefix(property, U.ENRICHED_PROPERTIES_PREFIX) && !strings.HasPrefix(property, U.SIX_SIGNAL_PROPERTIES_PREFIX) && !strings.HasPrefix(property, U.G2_PROPERTIES_PREFIX) {
+		if !strings.HasPrefix(property, U.ENRICHED_PROPERTIES_PREFIX) && !strings.HasPrefix(property, U.SIX_SIGNAL_PROPERTIES_PREFIX) && !strings.HasPrefix(property, U.CLR_PROPERTIES_PREFIX) && !strings.HasPrefix(property, U.G2_PROPERTIES_PREFIX) {
 			finalStandardUserProperties[property] = propertyDisplayName
 		}
 	}
