526b72b22e1cc654eeec607222032f023e22f03d
kartheek-factors <kartheek+1@factors.ai>
2023-10-17 11:27:34 +0530

    Fixing when result has no rows - Predefined dashboard

--- backend/src/factors/tests/model_predefined_website_aggregation_test.go
+++ backend/src/factors/tests/model_predefined_website_aggregation_test.go
@@ -60,7 +60,7 @@ func TestSampleWebsiteAggregation(t *testing.T) {
 			GroupByTimestamp:  "",
 			Timezone:          "UTC",
 			From:              1669856400,
-			To:                1675213200,
+			To: 			   1675213200,
 			InternalEventType: model.PredefEventTypeSession,
 			WidgetInternalID:  2,
 			WidgetName:        model.PredefWidGtmParams,
@@ -131,5 +131,93 @@ func TestSampleWebsiteAggregation(t *testing.T) {
 		assert.Equal(t, 63, len(result[1].Rows))
 		assert.Equal(t, float64(3), result[1].Rows[31][2].(float64))
 
+	})
+
+	t.Run("test predefined website aggregation for empty rows with group by", func(t *testing.T) {
+
+		query1 := model.PredefWebsiteAggregationQuery{
+			Metrics: []model.PredefinedMetric{
+				{Name: model.PredefTotalSessions, DisplayName: model.PredefDispTotalSessions},
+			},
+			GroupBy: model.PredefinedGroupBy{
+				Name: model.PredefPropCity, DisplayName: model.PredefPropDispCity,
+			},
+			Filters: []model.PredefinedFilter{
+				{PropertyName: model.PredefPropCity, PropertyDataType: "categorical", Condition: "equals", Value: "AB", LogicalOp: ""},
+			},
+			GroupByTimestamp:  	"",
+			Timezone:          	"UTC",
+			From:              	1669856400,
+			To: 				1669856400 + 90400,
+			InternalEventType: model.PredefEventTypeSession,
+			WidgetInternalID:  2,
+			WidgetName:        model.PredefWidGtmParams,
+		}
+
+		query2 := model.PredefWebsiteAggregationQuery{}
+		U.DeepCopy(&query1, &query2)
+
+		query2.GroupByTimestamp = "date"
+
+		queries := []model.PredefWebsiteAggregationQuery{query1, query2}
+		result, statusCode, _ := store.GetStore().ExecuteQueryGroupForPredefinedWebsiteAggregation(project.ID, model.PredefWebsiteAggregationQueryGroup{
+			Class:   "predefined_dashboard",
+			Queries: queries,
+		})
+		assert.Equal(t, http.StatusOK, statusCode)
+		log.WithField("result", result).Warn("kark2")
+
+		assert.Equal(t, 2, len(result[0].Headers))
+		assert.Equal(t, 1, len(result[0].Rows))
+		assert.Equal(t, 0, result[0].Rows[0][1].(int))
+
+		assert.Equal(t, 3, len(result[1].Headers))
+		assert.Equal(t, 2, len(result[1].Rows))
+		assert.Equal(t, 0, result[1].Rows[0][2].(int))
+		assert.Equal(t, 0, result[1].Rows[1][2].(int))
+
+	})
+
+	t.Run("test predefined website aggregation for empty rows without group by", func(t *testing.T) {
+
+		query1 := model.PredefWebsiteAggregationQuery{
+			Metrics: []model.PredefinedMetric{
+				{Name: model.PredefTotalSessions, DisplayName: model.PredefDispTotalSessions},
+			},
+			GroupBy: model.PredefinedGroupBy{
+			},
+			Filters: []model.PredefinedFilter{
+				{PropertyName: model.PredefPropCity, PropertyDataType: "categorical", Condition: "equals", Value: "AB", LogicalOp: ""},
+			},
+			GroupByTimestamp:  "",
+			Timezone:          "UTC",
+			From:              1669856400,
+			To: 				1669856400 + 90400,
+			InternalEventType: model.PredefEventTypeSession,
+			WidgetInternalID:  1,
+			WidgetName:        model.PredefWidGtmParams,
+		}
+
+		query2 := model.PredefWebsiteAggregationQuery{}
+		U.DeepCopy(&query1, &query2)
+
+		query2.GroupByTimestamp = "date"
+
+		queries := []model.PredefWebsiteAggregationQuery{query1, query2}
+		result, statusCode, _ := store.GetStore().ExecuteQueryGroupForPredefinedWebsiteAggregation(project.ID, model.PredefWebsiteAggregationQueryGroup{
+			Class:   "predefined_dashboard",
+			Queries: queries,
+		})
+		assert.Equal(t, http.StatusOK, statusCode)
+		log.WithField("result", result).Warn("kark2")
+
+		assert.Equal(t, 1, len(result[0].Headers))
+		assert.Equal(t, 1, len(result[0].Rows))
+		assert.Equal(t, 0, result[0].Rows[0][0].(int))
+
+		assert.Equal(t, 2, len(result[1].Headers))
+		assert.Equal(t, 2, len(result[1].Rows))
+		assert.Equal(t, 0, result[1].Rows[0][1].(int))
+		assert.Equal(t, 0, result[1].Rows[1][1].(int))
 	})
 }
