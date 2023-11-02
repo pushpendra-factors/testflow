e9e054b52bbac0357e68d8a80d33f4a6c564617e
parth-factors <parthg@factors.ai>
2023-11-02 16:51:51 +0530

    fix for wrong projid

--- backend/src/factors/handler/v1/project_analytics.go
+++ backend/src/factors/handler/v1/project_analytics.go
@@ -18,11 +18,7 @@ func GetFactorsAnalyticsHandler(c *gin.Context) {
 	noOfDays := int(7)
 	noOfDaysParam := c.Query("days")
 	isHtmlRequired := c.Query("html")
-	projectID := c.Query("projectID")
-	monthString := c.Query("month")
 	var err error
-
-	agentUUID := U.GetScopeByKeyAsString(c, mid.SCOPE_LOGGEDIN_AGENT_UUID)
 
 	if noOfDaysParam != "" {
 		noOfDays, err = strconv.Atoi(noOfDaysParam)
@@ -34,54 +30,17 @@ func GetFactorsAnalyticsHandler(c *gin.Context) {
 
 	var analytics map[string][]*model.ProjectAnalytics
 
-	if projectID != "" {
+	analytics, err = store.GetStore().GetEventUserCountsOfAllProjects(noOfDays)
 
-		log.WithFields(log.Fields{"projectId": projectID}).Info("GetFactorsAnalyticsHandler-before")
-
-		projId, err := strconv.ParseInt(projectID, 10, 64)
-		if err != nil {
-			return
-		}
-
-		log.WithFields(log.Fields{"projectId": projId}).Info("GetFactorsAnalyticsHandler-after")
-
-		analytics, err = store.GetStore().GetEventUserCountsByProjectID(projId, noOfDays)
-		if err != nil {
-			log.WithError(err).Error("GetEventUserCountsByProjectID")
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-
-		data, err := store.GetStore().GetGlobalProjectAnalyticsDataByProjectId(projId, monthString, agentUUID)
-		if err != nil {
-			log.WithError(err).Error("GetGlobalProjectAnalyticsDataByProjectId")
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-
-		var settings *model.ProjectSetting
-		var errCode int
-		settings, errCode = store.GetStore().GetProjectSetting(projId)
-		if errCode != http.StatusFound {
-			log.WithError(err).Error("project settings not found")
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-
-		integrationList := store.GetStore().GetIntegrationStatusesCount(*settings, projId, agentUUID)
-
-		project, _ := store.GetStore().GetProject(projId)
-
-		globalData := make(map[string][]map[string]interface{})
-		integrations := make(map[string][]map[string]interface{})
-
-		globalData["metrics"] = data
-		integrations["integrations"] = integrationList
+	if err != nil {
+		log.WithError(err).Error("GetEventUserCountsOfAllProjects")
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	if isHtmlRequired == "true" {
 
 		resultJson, err := json.Marshal(analytics)
 		if err != nil {
-			log.WithError(err).Error("failed to parse data")
-			c.AbortWithStatus(http.StatusInternalServerError)
 			return
 		}
 
@@ -92,46 +51,93 @@ func GetFactorsAnalyticsHandler(c *gin.Context) {
 			return
 		}
 
-		if isHtmlRequired == "true" {
-
-			U.ReturnReadableHtmlFromMaps(c, globalData, model.GlobalDataProjectAnalyticsColumnsName, model.GlobalDataProjectAnalyticsColumnsNameToJsonKeys, fmt.Sprintf("project : %s (%d)", project.Name, project.ID))
-			U.ReturnReadableHtmlFromList(c, integrations, model.GlobalDataIntegrationListColumnsName, model.GlobalDataIntegrationListColumnsNameToJsonKeys, "")
-			U.ReturnReadableHtmlFromMaps(c, resultMap, model.ProjectAnalyticsColumnsName, model.ProjectAnalyticsColumnsNameToJsonKeys, "remove")
-
-			return
-		}
-
-		resultMap["metrics"] = append(resultMap["metrics"], data...)
-		resultMap["metrics"] = append(resultMap["metrics"], integrationList...)
-		c.JSON(http.StatusOK, resultMap)
-
+		U.ReturnReadableHtmlFromMaps(c, resultMap, model.AllProjectAnalyticsColumnsName, model.ProjectAnalyticsColumnsNameToJsonKeys, "")
 		return
-	} else {
-		analytics, err = store.GetStore().GetEventUserCountsOfAllProjects(noOfDays)
+	}
 
+	c.JSON(http.StatusOK, gin.H{"analytics": analytics})
+}
+
+func GetFactorsAnalyticsByprojectIDHandler(c *gin.Context) {
+	noOfDays := int(7)
+	noOfDaysParam := c.Query("days")
+	isHtmlRequired := c.Query("html")
+	monthString := c.Query("month")
+	var err error
+
+	agentUUID := U.GetScopeByKeyAsString(c, mid.SCOPE_LOGGEDIN_AGENT_UUID)
+	projectID := U.GetScopeByKeyAsInt64(c, mid.SCOPE_PROJECT_ID)
+
+	if noOfDaysParam != "" {
+		noOfDays, err = strconv.Atoi(noOfDaysParam)
 		if err != nil {
-			log.WithError(err).Error("GetEventUserCountsOfAllProjects")
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-		if isHtmlRequired == "true" {
-
-			resultJson, err := json.Marshal(analytics)
-			if err != nil {
-				return
-			}
-
-			var resultMap map[string][]map[string]interface{}
-			err = json.Unmarshal(resultJson, &resultMap)
-			if err != nil {
-				log.Error(err)
-				return
-			}
-
-			U.ReturnReadableHtmlFromMaps(c, resultMap, model.AllProjectAnalyticsColumnsName, model.ProjectAnalyticsColumnsNameToJsonKeys, "")
+			c.AbortWithStatus(http.StatusBadRequest)
 			return
 		}
 	}
 
-	c.JSON(http.StatusOK, gin.H{"analytics": analytics})
+	var analytics map[string][]*model.ProjectAnalytics
+
+	analytics, err = store.GetStore().GetEventUserCountsByProjectID(projectID, noOfDays)
+	if err != nil {
+		log.WithError(err).Error("GetEventUserCountsByProjectID")
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	data, err := store.GetStore().GetGlobalProjectAnalyticsDataByProjectId(projectID, monthString, agentUUID)
+	if err != nil {
+		log.WithError(err).Error("GetGlobalProjectAnalyticsDataByProjectId")
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	var settings *model.ProjectSetting
+	var errCode int
+	settings, errCode = store.GetStore().GetProjectSetting(projectID)
+	if errCode != http.StatusFound {
+		log.WithError(err).Error("project settings not found")
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	integrationList := store.GetStore().GetIntegrationStatusesCount(*settings, projectID, agentUUID)
+
+	project, _ := store.GetStore().GetProject(projectID)
+
+	globalData := make(map[string][]map[string]interface{})
+	integrations := make(map[string][]map[string]interface{})
+
+	globalData["metrics"] = data
+	integrations["integrations"] = integrationList
+
+	resultJson, err := json.Marshal(analytics)
+	if err != nil {
+		log.WithError(err).Error("failed to parse data")
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	var resultMap map[string][]map[string]interface{}
+	err = json.Unmarshal(resultJson, &resultMap)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	if isHtmlRequired == "true" {
+
+		U.ReturnReadableHtmlFromMaps(c, globalData, model.GlobalDataProjectAnalyticsColumnsName, model.GlobalDataProjectAnalyticsColumnsNameToJsonKeys, fmt.Sprintf("project : %s (%d)", project.Name, project.ID))
+		U.ReturnReadableHtmlFromList(c, integrations, model.GlobalDataIntegrationListColumnsName, model.GlobalDataIntegrationListColumnsNameToJsonKeys, "")
+		U.ReturnReadableHtmlFromMaps(c, resultMap, model.ProjectAnalyticsColumnsName, model.ProjectAnalyticsColumnsNameToJsonKeys, "remove")
+
+		return
+	}
+
+	resultMap["metrics"] = append(resultMap["metrics"], data...)
+	resultMap["metrics"] = append(resultMap["metrics"], integrationList...)
+	c.JSON(http.StatusOK, resultMap)
+
+	return
+
 }
