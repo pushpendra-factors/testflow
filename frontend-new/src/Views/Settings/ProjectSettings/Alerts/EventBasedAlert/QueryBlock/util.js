af0a8e2f527f8fc90d9d0079105a0882bb1314f2
Vishnu Baliga <baliga-factors@users.noreply.github.com>
2023-12-07 15:22:10 +0530

    6signal duplicate value fix, cooltime fix

--- frontend-new/src/Views/Settings/ProjectSettings/Alerts/EventBasedAlert/QueryBlock/util.js
+++ frontend-new/src/Views/Settings/ProjectSettings/Alerts/EventBasedAlert/QueryBlock/util.js
@@ -1,21 +1,23 @@
+const Company_identification = "Company identification";
 
 export const  removeDuplicateAndEmptyKeys = (obj) => {
-    const uniqueKeys = {};
-    //blacklisted groups
-    let removeGroupList = ["Company identification"];
+    const uniqueKeys = {}; 
     for (const key in obj) {
       //remove duplicate keys
-      if (!uniqueKeys.hasOwnProperty(key)) {
-        //remove blacklisted keys
-        if(!key.includes(removeGroupList)){
+      if (!uniqueKeys.hasOwnProperty(key)) { 
           //remove empty keys
           if(!_.isEmpty(obj[key])){
           uniqueKeys[key] = obj[key]; 
-          }
-        }
-        
+          } 
       }     
     }
+    //remove 'Company identification' from user properties since the same duplicate properties is available in $6_signal
+    if(uniqueKeys.hasOwnProperty('user')){
+      if(uniqueKeys?.user?.hasOwnProperty(Company_identification)){
+        delete uniqueKeys?.user[Company_identification];
+      }
+    }
+
     return uniqueKeys;
   }
 
