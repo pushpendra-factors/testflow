00b14d009abc1ab8311b8be932d80dc7c65c75a6
Rahul Gnagwar <146810380+rahul895424@users.noreply.github.com>
2024-01-18 15:40:57 +0530

    Squashed commit of the add-new-workflow:
    commit 84a42efd346abe0d9110be4a551c8f5fc82d20f1
    Author: Rahul Gnagwar <146810380+rahul895424@users.noreply.github.com>
    Date:   Thu Jan 18 15:37:13 2024 +0530
    
        Update commit_count.yml file
    
    commit 90c5bceb07414d8891bdc62f4ee15e496d07f3e4
    Author: Rahul Gnagwar <146810380+rahul895424@users.noreply.github.com>
    Date:   Thu Jan 18 15:18:56 2024 +0530
    
        update commit_count.yml
    
    commit ad0a946c1f74480ec22e2b015a00687a03e207d6
    Author: Rahul Gnagwar <146810380+rahul895424@users.noreply.github.com>
    Date:   Thu Jan 18 15:01:25 2024 +0530
    
        Squashed commit of check_commits chnages:
    
        commit 24cfb43c4e1ee33168e218846eeb9f8c89fa0972
        Author: Rahul Gnagwar <146810380+rahul895424@users.noreply.github.com>
        Date:   Thu Jan 18 14:01:49 2024 +0530
    
            added check_commits.sh file and update test.yml

---
+++ check_commits.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#constants
+master="origin/master"
+staging="origin/staging"
+release="origin/release"
+curbranch="origin/${GITHUB_HEAD_REF#refs/heads/}"
+basebranch="origin/${GITHUB_BASE_REF#refs/heads/}"
+
+
+# turning off hints
+git config --global advice.resolveConflict false
+git config --global advice.commitBeforeMerge false
+
+# check out current branch
+git config --global user.name "Github Actions"
+git config --global user.email "actions@github.com"
+git fetch --unshallow -q
+git checkout -q $curbranch
+
+
+# Count number of commits on current branch
+total_commits=$(git rev-list --count HEAD ^$basebranch)
+
+# To check conflicts when number of commits more than 1 
+if [[ "$curbranch" != "$master" && "$curbranch" != "$release" && "$curbranch" != "$staging" ]]; then
+    if [[ "$total_commits" -gt 1 ]]; then
+        printf "Merge failed! Your $curbranch branch has more than 1 commit; please squash all commits.\n"
+        exit -1
+    fi
+fi
