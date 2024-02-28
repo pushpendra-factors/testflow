2842113bac01ebf54a7d99cf5ed3e5db9c973ac7
Vikas Kumar <85861073+vikasatfactors@users.noreply.github.com>
2024-02-28 18:11:57 +0530

    Source Engagement Signals from Properties

--- backend/src/factors/model/model/tl_profile.go
+++ backend/src/factors/model/model/tl_profile.go
@@ -163,13 +163,6 @@ var HostNameProps = []string{
 	U.G2_DOMAIN,
 }
 
-var DomainProperties = []string{
-	U.DP_ENGAGEMENT_LEVEL,
-	U.DP_ENGAGEMENT_SCORE,
-	U.DP_TOTAL_ENGAGEMENT_SCORE,
-	U.DP_DOMAIN_NAME,
-}
-
 // Hover Events Property Map
 var TIMELINE_EVENT_PROPERTIES_CONFIG = map[string][]string{
 	U.EVENT_NAME_SESSION: {
