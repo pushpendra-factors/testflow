cd99b0b994ac1a4bacb5df651869fe66e97fede2
Pushpendra Vishwakarma <pushpendra@factors.ai>
2024-03-01 18:04:16 +0530

    Hotfix: Last activtiy column on download as CSV should come in order after reselecting other columns

--- frontend-new/src/components/Profile/AccountProfiles/index.js
+++ frontend-new/src/components/Profile/AccountProfiles/index.js
@@ -1065,18 +1065,19 @@ function AccountProfiles({
       csvRows.push(headers.join(','));
 
       data.forEach((d) => {
-        const values = selectedOptions
-          .filter((elem) => elem !== 'last_activity')
-          .map((elem) =>
-            d.table_props[elem] != null ? `"${d.table_props[elem]}"` : '-'
-          );
+        const values = selectedOptions.map((elem) =>
+          elem === 'last_activity'
+            ? d.last_activity?.replace('T', ' ').replace('Z', '')
+            : d.table_props[elem] != null
+              ? `"${d.table_props[elem]}"`
+              : '-'
+        );
         values.unshift(
           d.name,
           d.engagement != null ? d.engagement : '-',
           d.score != null ? formatCount(d.score) : '-'
         );
-        if (headers.find((item) => item === 'Last Activity'))
-          values.push(d.last_activity?.replace('T', ' ').replace('Z', ''));
+
         csvRows.push(values);
       });
 
