896d2059bb57fa4ddfd0bbdce9cdcd29e29b9091
Vishnu Baliga <baliga-factors@users.noreply.github.com>
2024-03-19 12:53:48 +0530

    linkedin scope shanges

--- frontend-new/src/Views/Settings/ProjectSettings/IntegrationSettings/LinkedIn/index.js
+++ frontend-new/src/Views/Settings/ProjectSettings/IntegrationSettings/LinkedIn/index.js
@@ -13,6 +13,7 @@ import { ErrorBoundary } from 'react-error-boundary';
 import factorsai from 'factorsai';
 import { sendSlackNotification } from '../../../../../utils/slack';
 import _ from 'lodash';
+import {linkedInScope_rw_ads, linkedInScope_rw_conversions} from './constants';
 
 const LinkedInIntegration = ({
   fetchProjectSettings,
@@ -43,18 +44,6 @@ const LinkedInIntegration = ({
     // return isDevelopment() ? BUILD_CONFIG.adwords_service_host : BUILD_CONFIG.backend_host;
     return BUILD_CONFIG.backend_host;
   };
-
-  const linkedInWritePermissionArr = [
-    '12384898978000017', // Traceable
-    '1000005', // Akeyless
-    '12384898978000004', // Spotdraft
-    '6000320', // Sprinklr
-    '6000366', // Moengage
-    '568', // Clevertap
-    '6000356',// Freshworks 
-    '399', // Chargebee
-    '2', // FactorsAI Solutions
-  ]
 
   useEffect(() => {
     if (currentProjectSettings?.int_linkedin_ad_account) {
@@ -139,8 +128,11 @@ const LinkedInIntegration = ({
       if (port === undefined || port === '') {
         redirect_uri = protocol + '//' + hostname;
       }
-      let adsWritePermission = linkedInWritePermissionArr.includes(activeProject?.id);
-      let href = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${BUILD_CONFIG.linkedin_client_id}&redirect_uri=${redirect_uri}&state=factors&scope=r_basicprofile%20r_liteprofile%20r_ads_reporting%20${adsWritePermission?'rw_ads':'r_ads'}`;
+      //linkedIn scope check for accounts (defined in ./constants file).
+      let scope_rw_ads = linkedInScope_rw_ads.includes(activeProject?.id);
+      let scope_rw_conversions = linkedInScope_rw_conversions.includes(activeProject?.id);
+      //linkedIn oauth url gets updated based on the scopes assigned to project.
+      let href = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${BUILD_CONFIG.linkedin_client_id}&redirect_uri=${redirect_uri}&state=factors&scope=r_basicprofile%20r_liteprofile%20r_ads_reporting${scope_rw_ads?'%20rw_ads':'%20r_ads'}${scope_rw_conversions?'%20rw_conversions':''}`;
       return (
         <a href={href} className='ant-btn ant-btn-primary'>
           {' '}
