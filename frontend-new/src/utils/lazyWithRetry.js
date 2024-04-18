bafba1c63a11bd2c154a7c2164a4fcc99cf2874d
deepak bisht <deepak@factors.ai>
2024-04-18 16:58:18 +0530

    Fixed multiple reload for error component

--- frontend-new/src/utils/lazyWithRetry.js
+++ frontend-new/src/utils/lazyWithRetry.js
@@ -2,31 +2,26 @@ import { lazy } from 'react';
 
 const lazyWithRetry = (componentImport) =>
   lazy(async () => {
-    // console.log("lazyWithRetry", componentImport)
-    const pageHasAlreadyBeenForceRefreshed = JSON.parse(
-      window.localStorage.getItem(
-        'page-has-been-force-refreshed'
-      ) || 'false'
-    );
-
     try {
       const component = await componentImport();
 
-      window.localStorage.setItem(
-        'page-has-been-force-refreshed',
-        'false'
-      );
-
       return component;
     } catch (error) {
-      if (!pageHasAlreadyBeenForceRefreshed) {
+      const lastReloadedTime = window.localStorage.getItem(
+        'page-error-reloaded-last-time'
+      );
+      if (
+        !lastReloadedTime ||
+        Date.now() - Number(lastReloadedTime) > 60 * 1 * 1000
+      ) {
         // Assuming that the user is not on the latest version of the application.
         // Let's refresh the page immediately.
         window.localStorage.setItem(
-          'page-has-been-force-refreshed',
-          'true'
+          'page-error-reloaded-last-time',
+          Date.now()
         );
-        return window.location.reload();
+        if (process.env.NODE_ENV !== 'development')
+          return window.location.reload();
       }
 
       // The page has already been reloaded
@@ -36,5 +31,4 @@ const lazyWithRetry = (componentImport) =>
     }
   });
 
-
-  export default lazyWithRetry
+export default lazyWithRetry;
