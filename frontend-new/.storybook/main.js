b19c37b2ec99ad513df3f485da47fa6f983a20a1
Vishnu Baliga <baliga-factors@users.noreply.github.com>
2024-05-13 19:37:21 +0530

    storybook addon fixes

--- frontend-new/.storybook/main.js
+++ frontend-new/.storybook/main.js
@@ -2,7 +2,7 @@ const custom = require('../webpack.config.js');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-actions'],
   webpackFinal: async (config) => {
     return {
       ...config,
