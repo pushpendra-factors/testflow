d28b6d0f7866c09d488860ef7d3df8a0a469117f
dineshprabu-slashbit <dinesh@factors.ai>
2024-06-11 14:49:23 +0530

    Concurrency for set batch queries.

--- backend/src/factors/tests/cache_persistent_test.go
+++ backend/src/factors/tests/cache_persistent_test.go
@@ -17,9 +17,11 @@ func TestCacheDBSetBatch(t *testing.T) {
 
 	cacheKey1, _ := model.GetValuesByEventPropertyRollUpCacheKey(2, "$session", U.RandomLowerAphaNumString(5), "20240524")
 	cacheKey2, _ := model.GetValuesByEventPropertyRollUpCacheKey(2, "$session", U.RandomLowerAphaNumString(5), "20240524")
+	cacheKey3, _ := model.GetValuesByEventPropertyRollUpCacheKey(2, "$session", U.RandomLowerAphaNumString(5), "20240524")
 
 	records[cacheKey1] = "value1"
 	records[cacheKey2] = "value2"
+	records[cacheKey3] = "value3"
 
 	err := cacheDB.SetBatch(records, 7200)
 	assert.Nil(t, err)
@@ -30,13 +32,18 @@ func TestCacheDBSetBatch(t *testing.T) {
 	v2, err := cacheDB.Get(cacheKey2)
 	assert.Nil(t, err)
 
+	v3, err := cacheDB.Get(cacheKey3)
+	assert.Nil(t, err)
+
 	assert.Equal(t, "value1", v1)
 	assert.Equal(t, "value2", v2)
+	assert.Equal(t, "value3", v3)
 
 	// Update same records and it should be updated without duplicate error.
 
-	records[cacheKey1] = "value3"
-	records[cacheKey2] = "value4"
+	records[cacheKey1] = "value11"
+	records[cacheKey2] = "value22"
+	records[cacheKey3] = "value33"
 
 	err = cacheDB.SetBatch(records, 7200)
 	assert.Nil(t, err)
@@ -47,7 +54,10 @@ func TestCacheDBSetBatch(t *testing.T) {
 	v2, err = cacheDB.Get(cacheKey2)
 	assert.Nil(t, err)
 
-	assert.Equal(t, "value3", v1)
-	assert.Equal(t, "value4", v2)
+	v3, err = cacheDB.Get(cacheKey3)
+	assert.Nil(t, err)
 
+	assert.Equal(t, "value11", v1)
+	assert.Equal(t, "value22", v2)
+	assert.Equal(t, "value33", v3)
 }
