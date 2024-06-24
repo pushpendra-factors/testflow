8c79e1b1b93c51ba6425aabab9b4e6640c4052e9
Jitesh Kriplani <68528023+jk0693@users.noreply.github.com>
2024-06-24 08:54:25 +0530

    fixed dashboard ui issue

--- frontend-new/src/components/NoDataInTimeRange/index.js
+++ frontend-new/src/components/NoDataInTimeRange/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import { Text } from 'factorsComponents';
 
-const NoDataInTimeRange = ({ message }) => {
-  return (
-    <div className={'flex flex-col items-center fa-no-data--img'}>
-      <img
-        alt='no-data'
-        src='https://s3.amazonaws.com/www.factors.ai/assets/img/product/no-data-charts.png'
-        className={'mb-2'}
-      />
-      <Text
-        type={'title'}
-        level={8}
-        weight={'thin'}
-        color={'grey'}
-        extraClass={'m-0'}
-      >
-        {message}
-      </Text>
-    </div>
-  );
-};
+const NoDataInTimeRange = ({ message }) => (
+  <div className='flex flex-col items-center gap-y-2 text-center fa-no-data--img p-4'>
+    <img
+      alt='no-data'
+      src='https://s3.amazonaws.com/www.factors.ai/assets/img/product/no-data-charts.png'
+    />
+    <Text type='title' level={8} weight='thin' color='grey' extraClass='m-0'>
+      {message}
+    </Text>
+  </div>
+);
 
 export default NoDataInTimeRange;
